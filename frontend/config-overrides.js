2b816fd8fac240280e3b2a21ecc8b8c84757dd9a
Rikimaru <rikimaru0345@users.noreply.github.com>
2019-10-01 22:24:36 +0200

    prepare for eventual dark theme

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,20 +1,27 @@
 
-const { override, fixBabelImports } = require('customize-cra');
-
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const darkTheme = require('@ant-design/dark-theme')
 
 module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config;
+    // do stuff with the webpack config...
+    return config;
 };
 
 
 console.log('config override running...')
 
 module.exports = override(
-  
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true,
-  }),
+
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    // todo: to really get the dark theme working correctly we'll need
+    // to fix a lot of stuff related to styling...
+    // move inline styles out, invert stuff like #fff, ...
+    // addLessLoader({
+    //     javascriptEnabled: true,
+    //     modifyVars: darkTheme.default
+    // }),
 );
