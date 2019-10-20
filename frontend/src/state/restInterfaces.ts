8cfaadb8c062fb73c678ed2cd7ca8bcbb5f7ee12
Rikimaru <rikimaru0345@users.noreply.github.com>
2019-10-21 00:21:44 +0200

    show value type of messages; display binary data as hex

--- frontend/src/state/restInterfaces.ts
+++ frontend/src/state/restInterfaces.ts
@@ -36,9 +36,12 @@ export interface TopicMessage {
     partitionID: number,
     key: string, // base64 encoded key of the message
     value: any, // json representation of the message value (xml, avro, etc will get converted)
+    valueType: 'json' | 'xml' | 'avro' | 'text' | 'binary', // actual format the message (before being converted to json)
     size: number, // size in bytes of the kafka message
 
-    valueJson: string, // a helper prop we populate after receiving the response; not part of the response
+    // Added by the frontend (sometimes)
+    valueJson: string,
+    valueBinHexPreview: string,
 }
 
 export interface ListMessageResponse {
