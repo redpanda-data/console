d24afea3c2b030656cf0161baf24bb4ed35ec141
Rikimaru <rikimaru0345@users.noreply.github.com>
2019-11-02 16:27:19 +0100

    allow hooks to configure chi routers; rename env variables;

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/kafka-owl/kafka-owl/pkg/api"
+	"github.com/kafka-owl/kafka-owl/pkg/common/flagext"
 )
 
 func main() {
-	build, _ := api.NewAPIBuilder()
+
+	cfg := &api.Config{}
+	flagext.RegisterFlags(cfg)
+	flag.Parse()
+
+	build := api.NewAPIBuilder(cfg)
 	api := build.Build()
 	api.Start()
 }
