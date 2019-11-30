2b48fe3bc54504de94873424c279228fee90eef6
unknown <weeco91@gmail.com>
2019-11-30 19:36:46 +0100

    Put KeepAlive into it's own function

--- backend/pkg/kafka/service.go
+++ backend/pkg/kafka/service.go
@@ -18,53 +18,55 @@ type Service struct {
 func (s *Service) Start() {
 	// Custom keep alive for Kafka, because: https://github.com/Shopify/sarama/issues/1487
 	// The KeepAlive property in sarama doesn't work either, because of golang's buggy net module: https://github.com/golang/go/issues/31490
-	go func() {
-		log := s.Logger
-		wasHealthy := false
-		warningLimit := rate.NewLimiter(rate.Every(30*time.Second), 1)
-		for {
-			// Normal keepalive interval is 3 seconds
-			time.Sleep(3 * time.Second)
+	go s.keepAlive()
+}
 
-			brokers := s.Client.Brokers()
-			//log.Info("Kafka Keepalive", zap.Int("brokers", len(brokers)))
-			connectedCount := 0
+func (s *Service) keepAlive() {
+	log := s.Logger
+	wasHealthy := false
+	warningLimit := rate.NewLimiter(rate.Every(30*time.Second), 1)
+	for {
+		// Normal keepalive interval is 3 seconds
+		time.Sleep(3 * time.Second)
 
-			for _, b := range brokers {
+		brokers := s.Client.Brokers()
+		//log.Info("Kafka Keepalive", zap.Int("brokers", len(brokers)))
+		connectedCount := 0
 
-				connected, _ := b.Connected()
-				if !connected {
-					err := b.Open(s.Client.Config())
-					if err != nil {
-						log.Warn("could not open connection to broker", zap.String("broker", b.Addr()), zap.Error(err))
-					} else {
-						log.Info("connecting to broker", zap.String("broker", b.Addr()))
-					}
-					continue
-				}
-				connectedCount++
+		for _, b := range brokers {
 
-				_, err := b.GetMetadata(&sarama.MetadataRequest{})
-
+			connected, _ := b.Connected()
+			if !connected {
+				err := b.Open(s.Client.Config())
 				if err != nil {
-					log.Warn("heartbeat to broker has errored", zap.Error(err), zap.String("broker", b.Addr()), zap.Int32("id", b.ID()))
-
-					b.Close()
-					b.Open(s.Client.Config())
+					log.Warn("could not open connection to broker", zap.String("broker", b.Addr()), zap.Error(err))
+				} else {
+					log.Info("connecting to broker", zap.String("broker", b.Addr()))
 				}
+				continue
 			}
+			connectedCount++
 
-			if connectedCount == len(brokers) {
-				if !wasHealthy {
-					log.Info("connection to all brokers healthy", zap.Int("brokers", connectedCount))
-				}
-				wasHealthy = true
-			} else {
-				if warningLimit.Allow() {
-					log.Info("not connected to all brokers", zap.Int("connected", connectedCount), zap.Int("brokers", len(brokers)))
-				}
-				wasHealthy = false
+			_, err := b.GetMetadata(&sarama.MetadataRequest{})
+
+			if err != nil {
+				log.Warn("heartbeat to broker has errored", zap.Error(err), zap.String("broker", b.Addr()), zap.Int32("id", b.ID()))
+
+				b.Close()
+				b.Open(s.Client.Config())
 			}
 		}
-	}()
+
+		if connectedCount == len(brokers) {
+			if !wasHealthy {
+				log.Info("connection to all brokers healthy", zap.Int("brokers", connectedCount))
+			}
+			wasHealthy = true
+		} else {
+			if warningLimit.Allow() {
+				log.Info("not connected to all brokers", zap.Int("connected", connectedCount), zap.Int("brokers", len(brokers)))
+			}
+			wasHealthy = false
+		}
+	}
 }
