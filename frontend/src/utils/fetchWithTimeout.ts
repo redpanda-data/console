02adbab2f1722bedfb971cb94fa1fea1267b8810
Rikimaru <rikimaru0345@users.noreply.github.com>
2020-02-02 11:25:36 +0100

    slightly increase message timeout (related #25)

--- frontend/src/utils/fetchWithTimeout.ts
+++ frontend/src/utils/fetchWithTimeout.ts
@@ -1,20 +1,20 @@
 
 
 export class RestTimeoutError extends Error {
-	constructor(m: string) {
-		super(m);
-		Object.setPrototypeOf(this, RestTimeoutError.prototype);
-	}
+    constructor(m: string) {
+        super(m);
+        Object.setPrototypeOf(this, RestTimeoutError.prototype);
+    }
 }
 
 export default function (url: RequestInfo, timeoutMs: number, options?: RequestInit): Promise<Response> {
 
-	const requestPromise = fetch(url, options);
-	const timeoutPromise = new Promise<Response>((_, reject) => {
-		setTimeout(() => {
-			reject(new RestTimeoutError("Request '" + url + "' timed out after " + (timeoutMs / 1000).toFixed(1) + " sec"))
-		}, timeoutMs);
-	});
+    const requestPromise = fetch(url, options);
+    const timeoutPromise = new Promise<Response>((_, reject) => {
+        setTimeout(() => {
+            reject(new RestTimeoutError("Request timed out after " + (timeoutMs / 1000).toFixed(1) + " sec: " + url))
+        }, timeoutMs);
+    });
 
-	return Promise.race([requestPromise, timeoutPromise]);
+    return Promise.race([requestPromise, timeoutPromise]);
 }
