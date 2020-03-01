fdd6ef9707287aecdcf5e0306e6cec25eac8e6e5
Rikimaru <rikimaru0345@users.noreply.github.com>
2020-03-01 14:36:11 +0100

    save search params and quickSearch in query

--- frontend/src/utils/queryHelper.ts
+++ frontend/src/utils/queryHelper.ts
@@ -22,7 +22,7 @@ export const objToQuery = (obj: { [key: string]: any; }) => '?' + queryString.st
 
 // edit the current search query,
 // IFF you make any changes inside editFunction, it returns the stringified version of the search query
-export function editQuery(editFunction: (queryObject: ParsedQuery<string | number>) => void) {
+export function editQuery(editFunction: (queryObject: ParsedQuery<string>) => void) {
     const urlParams = queryString.parse(window.location.search);
     editFunction(urlParams);
     const query = queryString.stringify(urlParams);
