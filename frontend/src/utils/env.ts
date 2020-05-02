ed83d59c539d760f67a7b19a4b47e5143b5049cd
Rikimaru <rikimaru0345@users.noreply.github.com>
2020-05-02 23:01:45 +0200

    print env vars

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,9 +1,16 @@
+import { ToJson } from "./utils";
+import { isBusinessVersion } from "..";
+import { IsDevelopment } from "./isProd";
 
 
 const envNames = [
     'REACT_APP_KOWL_GIT_SHA',
     'REACT_APP_KOWL_GIT_REF', // 'master' or 'v1.2.3'
-    'REACT_APP_KOWL_TIMESTAMP'
+    'REACT_APP_KOWL_TIMESTAMP',
+    'REACT_APP_KOWL_BUSINESS_GIT_SHA',
+    'REACT_APP_KOWL_BUSINESS_GIT_REF',
+    'REACT_APP_KOWL_BUSINESS_TIMESTAMP',
+
 ] as const;
 
 type Environment = { [key in typeof envNames[number]]: string };
@@ -13,12 +20,22 @@ const env = {} as Environment;
 for (let k of envNames)
     env[k] = process.env[k] || '';
 
+const versionObject = JSON.parse(ToJson(env));
+versionObject['REACT_APP_BUSINESS'] = process.env.REACT_APP_BUSINESS ?? false;
+versionObject['NODE_ENV'] = process.env.NODE_ENV;
+console.log(ToJson(versionObject));
+
 export default env;
 
 export function getBuildDate(): string {
-    const timestamp = +env.REACT_APP_KOWL_TIMESTAMP
+    let timestamp = +env.REACT_APP_KOWL_TIMESTAMP
     if (timestamp == 0) {
         return '??';
+    }
+
+    const timestampBusiness = +env.REACT_APP_KOWL_BUSINESS_TIMESTAMP
+    if (timestampBusiness && timestampBusiness > timestamp) {
+        timestamp = timestampBusiness;
     }
 
     return new Date(timestamp * 1000).toLocaleDateString()
