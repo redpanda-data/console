3e56dce80151ef138779c751afe2f33da59da03d
Martin Schneppenheim <weeco91@gmail.com>
2020-05-02 17:15:02 +0200

    Use logging for startup

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"go.uber.org/zap"
 	"os"
 	"strconv"
 	"time"
@@ -12,7 +12,8 @@ import (
 )
 
 func main() {
-	printVersion()
+	startupLogger := zap.NewExample()
+	printVersion(startupLogger)
 
 	cfg := &api.Config{}
 	cfg.SetDefaults()
@@ -21,41 +22,41 @@ func main() {
 
 	// This can not be part of the Config's validate() method because we haven't decoded the YAML config at this point
 	if cfg.ConfigFilepath == "" {
-		fmt.Fprint(os.Stderr, "you must specify the path to the config filepath using the --config.filepath flag")
-		os.Exit(1)
+		startupLogger.Fatal("you must specify the path to the config filepath using the --config.filepath flag")
 	}
 
 	err := api.LoadConfig(cfg.ConfigFilepath, cfg)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to load yaml config: %s", err)
-		os.Exit(1)
+		startupLogger.Fatal("failed to load yaml config", zap.Error(err))
 	}
 	err = cfg.Validate()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to validate config: %s", err)
-		os.Exit(1)
+		startupLogger.Fatal("failed to validate config", zap.Error(err))
 	}
 
 	a := api.New(cfg)
 	a.Start()
 }
 
-func printVersion() {
+func printVersion(logger *zap.Logger) {
 	sha1 := os.Getenv("REACT_APP_KOWL_GIT_SHA")
 	ref1 := os.Getenv("REACT_APP_KOWL_GIT_REF")
 	timestamp1 := os.Getenv("REACT_APP_KOWL_TIMESTAMP")
 
 	if len(sha1) == 0 {
-		fmt.Printf("KOWL (dev)\n")
+		logger.Info("started Kowl", zap.String("version", "dev"))
 	} else {
 		t1, err := strconv.ParseInt(timestamp1, 10, 64)
 		var timeStr1 string
 		if err != nil {
-			fmt.Printf("Timestamp1 %v cannot be parsed as int64: %v", timestamp1, err.Error())
+			logger.Warn("failed to parse timestamp as int64", zap.String("timestamp", timeStr1), zap.Error(err))
 			timeStr1 = "(parsing error)"
 		} else {
 			timeStr1 = time.Unix(t1, 0).Format(time.RFC3339)
 		}
-		fmt.Printf("KOWL (Version: %v) (Built: %v) (Commit: %v)\n", ref1, timeStr1, sha1)
+		logger.Info("started Kowl",
+			zap.String("version", ref1),
+			zap.String("built", timeStr1),
+			zap.String("git_sha", sha1))
 	}
 }
