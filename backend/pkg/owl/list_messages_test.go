c622d8307effab926e93ead200534a936ff9f8a0
Martin Schneppenheim <weeco91@gmail.com>
2020-05-24 21:43:56 +0200

    Fix partition consumer for edge case with high watermarks

--- backend/pkg/owl/list_messages_test.go
+++ backend/pkg/owl/list_messages_test.go
@@ -13,9 +13,6 @@ func TestCalculateConsumeRequests_AllPartitions_FewNewestMessages(t *testing.T) 
 		0: {PartitionID: 0, Low: 0, High: 300},
 		1: {PartitionID: 1, Low: 0, High: 10},
 		2: {PartitionID: 2, Low: 10, High: 30},
-		3: {PartitionID: 3, Low: 0, High: 300},
-		4: {PartitionID: 4, Low: 0, High: 400},
-		5: {PartitionID: 5, Low: 0, High: 500},
 	}
 
 	req := &ListMessageRequest{
@@ -26,10 +23,10 @@ func TestCalculateConsumeRequests_AllPartitions_FewNewestMessages(t *testing.T) 
 	}
 
 	// Expected result should be able to return all 100 requested messages as evenly distributed as possible
-	expected := map[int32]*kafka.partitionConsumeRequest{
-		0: {PartitionID: 0, IsDrained: false, StartOffset: marks[0].High - 1, EndOffset: marks[0].High, MaxMessageCount: 1, LowWaterMark: marks[0].Low, HighWaterMark: marks[0].High},
-		1: {PartitionID: 0, IsDrained: false, StartOffset: marks[1].High - 1, EndOffset: marks[1].High, MaxMessageCount: 1, LowWaterMark: marks[1].Low, HighWaterMark: marks[1].High},
-		2: {PartitionID: 0, IsDrained: false, StartOffset: marks[2].High - 1, EndOffset: marks[2].High, MaxMessageCount: 1, LowWaterMark: marks[2].Low, HighWaterMark: marks[2].High},
+	expected := map[int32]*kafka.PartitionConsumeRequest{
+		0: {PartitionID: 0, IsDrained: false, StartOffset: marks[0].High - 1, EndOffset: marks[0].High - 1, MaxMessageCount: 1, LowWaterMark: marks[0].Low, HighWaterMark: marks[0].High},
+		1: {PartitionID: 1, IsDrained: false, StartOffset: marks[1].High - 1, EndOffset: marks[1].High - 1, MaxMessageCount: 1, LowWaterMark: marks[1].Low, HighWaterMark: marks[1].High},
+		2: {PartitionID: 2, IsDrained: false, StartOffset: marks[2].High - 1, EndOffset: marks[2].High - 1, MaxMessageCount: 1, LowWaterMark: marks[2].Low, HighWaterMark: marks[2].High},
 	}
 	actual := calculateConsumeRequests(req, marks)
 
@@ -40,8 +37,8 @@ func TestCalculateConsumeRequests_AllPartitions_Unbalanced(t *testing.T) {
 	// Unbalanced message distribution across 3 partitions
 	marks := map[int32]*kafka.WaterMark{
 		0: {PartitionID: 0, Low: 0, High: 300},
-		1: {PartitionID: 1, Low: 0, High: 10},
-		2: {PartitionID: 2, Low: 10, High: 30},
+		1: {PartitionID: 1, Low: 0, High: 11},
+		2: {PartitionID: 2, Low: 10, High: 31},
 	}
 
 	req := &ListMessageRequest{
@@ -52,10 +49,10 @@ func TestCalculateConsumeRequests_AllPartitions_Unbalanced(t *testing.T) {
 	}
 
 	// Expected result should be able to return all 100 requested messages as evenly distributed as possible
-	expected := map[int32]*kafka.partitionConsumeRequest{
-		0: {PartitionID: 0, IsDrained: false, LowWaterMark: marks[0].Low, HighWaterMark: marks[0].High, StartOffset: 0, EndOffset: marks[0].High, MaxMessageCount: 70},
-		1: {PartitionID: 1, IsDrained: true, LowWaterMark: marks[1].Low, HighWaterMark: marks[1].High, StartOffset: 0, EndOffset: marks[1].High, MaxMessageCount: 10},
-		2: {PartitionID: 2, IsDrained: true, LowWaterMark: marks[2].Low, HighWaterMark: marks[2].High, StartOffset: 10, EndOffset: marks[2].High, MaxMessageCount: 20},
+	expected := map[int32]*kafka.PartitionConsumeRequest{
+		0: {PartitionID: 0, IsDrained: false, LowWaterMark: marks[0].Low, HighWaterMark: marks[0].High, StartOffset: 0, EndOffset: marks[0].High - 1, MaxMessageCount: 70},
+		1: {PartitionID: 1, IsDrained: true, LowWaterMark: marks[1].Low, HighWaterMark: marks[1].High, StartOffset: 0, EndOffset: marks[1].High - 1, MaxMessageCount: 10},
+		2: {PartitionID: 2, IsDrained: true, LowWaterMark: marks[2].Low, HighWaterMark: marks[2].High, StartOffset: 10, EndOffset: marks[2].High - 1, MaxMessageCount: 20},
 	}
 	actual := calculateConsumeRequests(req, marks)
 
@@ -64,60 +61,60 @@ func TestCalculateConsumeRequests_AllPartitions_Unbalanced(t *testing.T) {
 
 func TestCalculateConsumeRequests_SinglePartition(t *testing.T) {
 	marks := map[int32]*kafka.WaterMark{
-		14: {PartitionID: 14, Low: 100, High: 300},
+		14: {PartitionID: 14, Low: 100, High: 301},
 	}
 	lowMark := marks[14].Low
 	highMark := marks[14].High
 
 	tt := []struct {
 		req      *ListMessageRequest
-		expected map[int32]*kafka.partitionConsumeRequest
+		expected map[int32]*kafka.PartitionConsumeRequest
 	}{
 		// Newest 100 messages
 		{
 			&ListMessageRequest{TopicName: "test", PartitionID: 14, StartOffset: sarama.OffsetNewest, MessageCount: 100},
-			map[int32]*kafka.partitionConsumeRequest{
-				14: {PartitionID: 14, IsDrained: false, StartOffset: highMark - 100, EndOffset: highMark, MaxMessageCount: 100, LowWaterMark: lowMark, HighWaterMark: highMark},
+			map[int32]*kafka.PartitionConsumeRequest{
+				14: {PartitionID: 14, IsDrained: false, StartOffset: highMark - 100, EndOffset: highMark - 1, MaxMessageCount: 100, LowWaterMark: lowMark, HighWaterMark: highMark},
 			},
 		},
 
 		// Oldest 40 messages
 		{
 			&ListMessageRequest{TopicName: "test", PartitionID: 14, StartOffset: sarama.OffsetOldest, MessageCount: 40},
-			map[int32]*kafka.partitionConsumeRequest{
-				14: {PartitionID: 14, IsDrained: false, StartOffset: lowMark, EndOffset: highMark, MaxMessageCount: 40, LowWaterMark: lowMark, HighWaterMark: highMark},
+			map[int32]*kafka.PartitionConsumeRequest{
+				14: {PartitionID: 14, IsDrained: false, StartOffset: lowMark, EndOffset: highMark - 1, MaxMessageCount: 40, LowWaterMark: lowMark, HighWaterMark: highMark},
 			},
 		},
 
 		// Custom start offset with drained - 50 messages
 		{
 			&ListMessageRequest{TopicName: "test", PartitionID: 14, StartOffset: 250, MessageCount: 200},
-			map[int32]*kafka.partitionConsumeRequest{
-				14: {PartitionID: 14, IsDrained: true, StartOffset: 250, EndOffset: highMark, MaxMessageCount: 50, LowWaterMark: lowMark, HighWaterMark: highMark},
+			map[int32]*kafka.PartitionConsumeRequest{
+				14: {PartitionID: 14, IsDrained: true, StartOffset: 250, EndOffset: highMark - 1, MaxMessageCount: 50, LowWaterMark: lowMark, HighWaterMark: highMark},
 			},
 		},
 
 		// Custom out of bounds start offset - 50 messages
 		{
 			&ListMessageRequest{TopicName: "test", PartitionID: 14, StartOffset: 15, MessageCount: 50},
-			map[int32]*kafka.partitionConsumeRequest{
-				14: {PartitionID: 14, IsDrained: false, StartOffset: lowMark, EndOffset: highMark, MaxMessageCount: 50, LowWaterMark: lowMark, HighWaterMark: highMark},
+			map[int32]*kafka.PartitionConsumeRequest{
+				14: {PartitionID: 14, IsDrained: false, StartOffset: lowMark, EndOffset: highMark - 1, MaxMessageCount: 50, LowWaterMark: lowMark, HighWaterMark: highMark},
 			},
 		},
 
 		// Newest 500 messages with drained
 		{
 			&ListMessageRequest{TopicName: "test", PartitionID: 14, StartOffset: sarama.OffsetNewest, MessageCount: 500},
-			map[int32]*kafka.partitionConsumeRequest{
-				14: {PartitionID: 14, IsDrained: true, StartOffset: lowMark, EndOffset: highMark, MaxMessageCount: highMark - lowMark, LowWaterMark: lowMark, HighWaterMark: highMark},
+			map[int32]*kafka.PartitionConsumeRequest{
+				14: {PartitionID: 14, IsDrained: true, StartOffset: lowMark, EndOffset: highMark - 1, MaxMessageCount: 201, LowWaterMark: lowMark, HighWaterMark: highMark},
 			},
 		},
 
 		// Oldest 500 messages with drained
 		{
 			&ListMessageRequest{TopicName: "test", PartitionID: 14, StartOffset: sarama.OffsetOldest, MessageCount: 500},
-			map[int32]*kafka.partitionConsumeRequest{
-				14: {PartitionID: 14, IsDrained: true, StartOffset: lowMark, EndOffset: highMark, MaxMessageCount: highMark - lowMark, LowWaterMark: lowMark, HighWaterMark: highMark},
+			map[int32]*kafka.PartitionConsumeRequest{
+				14: {PartitionID: 14, IsDrained: true, StartOffset: lowMark, EndOffset: highMark - 1, MaxMessageCount: 200, LowWaterMark: lowMark, HighWaterMark: highMark},
 			},
 		},
 	}
