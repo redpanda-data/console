1a0f86d49241643c28574b3f748529cf5401f4f9
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2020-05-29 15:03:14 +0200

    enhance: reduce size of statistics on small screens

--- frontend/src/components/pages/admin/Admin.RoleBindings.tsx
+++ frontend/src/components/pages/admin/Admin.RoleBindings.tsx
@@ -13,7 +13,7 @@ import topicConfigInfo from '../../../assets/topicConfigInfo.json'
 import { sortField, range, makePaginationConfig, Spacer } from "../../misc/common";
 import { motion, AnimatePresence } from "framer-motion";
 import { observable, computed, transaction } from "mobx";
-import { findElementDeep, cullText, getAllKeys, ToJson } from "../../../utils/utils";
+import { ToJson } from "../../../utils/utils";
 import { animProps, MotionAlways, MotionDiv } from "../../../utils/animationProps";
 import Paragraph from "antd/lib/typography/Paragraph";
 import { ColumnProps } from "antd/lib/table";
