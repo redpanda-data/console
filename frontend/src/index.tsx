a5c281cbd0186d91bb7cf1e12d8ca8a386c088eb
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2020-08-08 16:11:52 +0200

    use scss; fix some css errors

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {
-  BrowserRouter,
-  withRouter,
-  RouteComponentProps
+    BrowserRouter,
+    withRouter,
+    RouteComponentProps
 } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import "antd/dist/antd.css";
-import "./index.css";
+import "./index.scss";
 import 'mobx-react-lite/batchingForReactDom'
 
 import App from "./components/App";
 import { appGlobal } from "./state/appGlobal";
 
 const HistorySetter = withRouter((p: RouteComponentProps) => {
-  appGlobal.history = p.history;
-  return <></>;
+    appGlobal.history = p.history;
+    return <></>;
 });
 
 ReactDOM.render(
-  <BrowserRouter>
-    <HistorySetter />
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <HistorySetter />
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
 serviceWorker.unregister();
