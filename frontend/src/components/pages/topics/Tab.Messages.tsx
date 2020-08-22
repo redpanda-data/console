eb56fe5c97f5cdc9bf4a5ca22a6b603425dfe46f
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2020-08-22 18:28:03 +0200

    var -> let

--- frontend/src/components/pages/topics/Tab.Messages.tsx
+++ frontend/src/components/pages/topics/Tab.Messages.tsx
@@ -778,8 +778,8 @@ function RenderExpandedMessage(msg: TopicMessage, shouldExpand?: ((x: CollapsedF
             }
             else if (mode == 'hex') {
                 const str = msg.value as string;
-                var hex = '';
-                for (var i = 0; i < str.length; i++) {
+                let hex = '';
+                for (let i = 0; i < str.length; i++) {
                     let n = str.charCodeAt(i).toString(16);
                     if (n.length == 1) n = '0' + n;
                     hex += n + ' ';
@@ -789,9 +789,9 @@ function RenderExpandedMessage(msg: TopicMessage, shouldExpand?: ((x: CollapsedF
             }
             else {
                 const str = msg.value as string;
-                var result = '';
+                let result = '';
                 const isPrintable = /[\x20-\x7E]/;
-                for (var i = 0; i < str.length; i++) {
+                for (let i = 0; i < str.length; i++) {
                     let ch = String.fromCharCode(str.charCodeAt(i)); // str.charAt(i);
                     ch = isPrintable.test(ch) ? ch : '. ';
                     result += ch + ' ';
