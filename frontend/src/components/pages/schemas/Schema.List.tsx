3a4956482067df049458efd923a7abf0c18415c4
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2020-09-15 22:36:01 +0200

    use cards and add schema overview stats

--- frontend/src/components/pages/schemas/Schema.List.tsx
+++ frontend/src/components/pages/schemas/Schema.List.tsx
@@ -1,3 +1,9 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import { PageComponent, PageInitHelper } from '../Page';
+import { api } from '../../../state/backendApi';
+import { Row, Statistic, Table } from 'antd';
+import Card from '../../misc/Card';
 import { appGlobal } from '../../../state/appGlobal';
 
 @observer
@@ -12,17 +18,32 @@ class SchemaList extends PageComponent<{}> {
         api.refreshSchemaOverview(force);
     }
     render() {
-        console.dir({overview: api.SchemaOverview})
-        return (<Table
-        /*  TODO: requestError */
-            columns={[
-                { title: 'Name', dataIndex: 'name' },
-                { title: 'Compatibility Level', dataIndex: 'compatibilityLevel' },
-                { title: 'Versions', dataIndex: 'versionsCount' },
-                { title: 'Latest Version', dataIndex: 'latestVersion' },
-            ]}
-            dataSource={api.SchemaOverview?.subjects}
-        ></Table>)
+        const { mode, compatibilityLevel, subjects} = {...api.SchemaOverview}
+        return (
+            <>
+                <Card>
+                    <Row>
+                        <Statistic title="Mode" value={mode}></Statistic>
+                        <Statistic title="Compatibility Level" value={compatibilityLevel}></Statistic>
+                    </Row>
+                </Card>
+                <Card>
+                    <Table
+                        /* TODO: display any request errors */
+                        /* TODO: page for and links to subject details */
+                        columns={[
+                            { title: 'Name', dataIndex: 'name' },
+                            { title: 'Compatibility Level', dataIndex: 'compatibilityLevel' },
+                            { title: 'Versions', dataIndex: 'versionsCount' },
+                            { title: 'Latest Version', dataIndex: 'latestVersion' },
+                        ]}
+                        dataSource={subjects}
+                        // TODO: Useful pagination settings
+                        pagination={false}
+                    ></Table>
+                </Card>
+            </>
+        );
     }
 }
 
