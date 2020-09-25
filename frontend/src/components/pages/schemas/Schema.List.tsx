0e3b5219d5993d99c5e542b1876d9776681dc8d6
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2020-09-26 01:18:00 +0200

    show occuring request errors on schema overview

--- frontend/src/components/pages/schemas/Schema.List.tsx
+++ frontend/src/components/pages/schemas/Schema.List.tsx
@@ -2,13 +2,30 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import { PageComponent, PageInitHelper } from '../Page';
 import { api } from '../../../state/backendApi';
-import { Empty, Row, Statistic, Table } from 'antd';
+import { Alert, Empty, Row, Statistic, Table } from 'antd';
 import Card from '../../misc/Card';
 import { appGlobal } from '../../../state/appGlobal';
 import { motion } from 'framer-motion';
 import { animProps } from '../../../utils/animationProps';
 import { sortField } from '../../misc/common';
 import { DefaultSkeleton } from '../../../utils/tsxUtils';
+import { SchemaOverviewRequestError } from '../../../state/restInterfaces';
+
+import './Schema.List.scss'
+
+function renderRequestErrors(requestErrors?: SchemaOverviewRequestError[]) {
+    if (!requestErrors || requestErrors.length === 0) {
+        return null;
+    }
+
+    return (
+        <Card className="SchemaList__error-card">
+            {requestErrors.map(({ errorMessage, requestDescription }) => (
+                <Alert type="error" message={errorMessage} description={requestDescription} closable className="SchemaList__alert" />
+            ))}
+        </Card>
+    );
+}
 
 @observer
 class SchemaList extends PageComponent<{}> {
@@ -16,7 +33,7 @@ class SchemaList extends PageComponent<{}> {
         p.title = 'Schema Registry';
         p.addBreadcrumb('Schema Registry', '/schema-registry');
         this.refreshData(false);
-        appGlobal.onRefresh = () => this.refreshData(true)
+        appGlobal.onRefresh = () => this.refreshData(true);
     }
     refreshData(force?: boolean) {
         api.refreshSchemaOverview(force);
@@ -25,6 +42,7 @@ class SchemaList extends PageComponent<{}> {
         if (!api.SchemaOverview) return DefaultSkeleton;
 
         const { mode, compatibilityLevel, subjects, requestErrors } = { ...api.SchemaOverview };
+
         return (
             <motion.div {...animProps} key={'b'} style={{ margin: '0 1rem' }}>
                 <Card>
@@ -33,6 +51,7 @@ class SchemaList extends PageComponent<{}> {
                         <Statistic title="Compatibility Level" value={compatibilityLevel}></Statistic>
                     </Row>
                 </Card>
+                {renderRequestErrors(requestErrors)}
                 <Card>
                     {subjects.length === 0 ? (
                         <Empty />
