002d876ed7404d09d6dfcdc37903bfc4bdbaf2ea
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2020-11-10 18:03:49 +0100

    add 'includes' to string prototype

--- backend/pkg/interpreter/find_function.go
+++ backend/pkg/interpreter/find_function.go
@@ -157,5 +157,16 @@ function findObject(ctx, obj) {
     return false;
 }
 
+
+if (!String.prototype.includes) {
+	String.prototype.includes = function(search, start) {
+	  if (search instanceof RegExp) {
+		throw TypeError('first argument must not be a RegExp');
+	  }
+	  if (start === undefined) { start = 0; }
+	  return this.indexOf(search, start) !== -1;
+	};
+  }
+
 `
 )
