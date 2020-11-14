4ab0c5d9620a097f5b47cba4c441f62493a35750
Martin Schneppenheim <weeco91@gmail.com>
2020-11-14 12:48:03 +0100

    Update health and startup probes

--- backend/pkg/kafka/health_check.go
+++ backend/pkg/kafka/health_check.go
@@ -1,8 +1,28 @@
 package kafka
 
+import (
+	"context"
+	"fmt"
+	"github.com/twmb/franz-go/pkg/kmsg"
+	"go.uber.org/zap"
+)
+
 // IsHealthy checks whether it can communicate with the Kafka cluster or not
-func (s *Service) IsHealthy() error {
-	// TODO: Implement health check
+func (s *Service) IsHealthy(ctx context.Context) error {
+	req := kmsg.MetadataRequest{
+		Topics:                             []kmsg.MetadataRequestTopic{},
+		AllowAutoTopicCreation:             false,
+		IncludeClusterAuthorizedOperations: true,
+		IncludeTopicAuthorizedOperations:   false,
+	}
+	kres, err := req.RequestWith(ctx, s.KafkaClient)
+	if err != nil {
+		s.Logger.Error("failed to request metadata in health check", zap.Error(err))
+		return fmt.Errorf("failed to request metadata: %w", err)
+	}
+	s.Logger.Debug("kafka cluster health check succeeded",
+		zap.Int("broker_count", len(kres.Brokers)),
+	)
 
 	return nil
 }
