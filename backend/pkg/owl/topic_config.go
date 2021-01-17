a7391c3b83a6db87eacd580764ee60a42c627e79
Martin Schneppenheim <weeco91@gmail.com>
2021-01-17 22:17:49 +0100

    Rename TopicConfig struct to singular version

--- backend/pkg/owl/topic_config.go
+++ backend/pkg/owl/topic_config.go
@@ -6,8 +6,8 @@ import (
 	"go.uber.org/zap"
 )
 
-// TopicConfigs is a TopicName along with all it's config entries
-type TopicConfigs struct {
+// TopicConfig is a TopicName along with all it's config entries
+type TopicConfig struct {
 	TopicName     string              `json:"topicName"`
 	ConfigEntries []*TopicConfigEntry `json:"configEntries"`
 }
@@ -15,14 +15,14 @@ type TopicConfigs struct {
 // TopicConfigEntry is a key value pair of a config property with it's value
 type TopicConfigEntry struct {
 	Name        string  `json:"name"`
-	Value       *string `json:"value"` // If value is sensitiv this will be nil
+	Value       *string `json:"value"` // If value is sensitive this will be nil
 	IsDefault   bool    `json:"isDefault"`
 	IsSensitive bool    `json:"isSensitive"`
 }
 
 // GetConfigEntryByName returns the TopicConfigEntry for a given config name (e. g. "cleanup.policy") or nil if
 // no config with that name could be found.
-func (t *TopicConfigs) GetConfigEntryByName(configName string) *TopicConfigEntry {
+func (t *TopicConfig) GetConfigEntryByName(configName string) *TopicConfigEntry {
 	for _, entry := range t.ConfigEntries {
 		if entry.Name != configName {
 			continue
@@ -33,7 +33,7 @@ func (t *TopicConfigs) GetConfigEntryByName(configName string) *TopicConfigEntry
 }
 
 // GetTopicConfigs calls GetTopicsConfigs for a single Topic and returns a single response
-func (s *Service) GetTopicConfigs(ctx context.Context, topicName string, configNames []string) (*TopicConfigs, error) {
+func (s *Service) GetTopicConfigs(ctx context.Context, topicName string, configNames []string) (*TopicConfig, error) {
 	response, err := s.GetTopicsConfigs(ctx, []string{topicName}, configNames)
 	if err != nil {
 		return nil, err
@@ -44,14 +44,14 @@ func (s *Service) GetTopicConfigs(ctx context.Context, topicName string, configN
 
 // GetTopicsConfigs fetches all topic config options for the given set of topic names and config names and converts
 // that information so that it is handy to use. Provide an empty array for configNames to describe all config entries.
-func (s *Service) GetTopicsConfigs(ctx context.Context, topicNames []string, configNames []string) (map[string]*TopicConfigs, error) {
+func (s *Service) GetTopicsConfigs(ctx context.Context, topicNames []string, configNames []string) (map[string]*TopicConfig, error) {
 	response, err := s.kafkaSvc.DescribeTopicsConfigs(ctx, topicNames, configNames)
 	if err != nil {
 		return nil, err
 	}
 
 	// 3. Iterate through response's config entries and convert them into our desired format
-	converted := make(map[string]*TopicConfigs, len(topicNames))
+	converted := make(map[string]*TopicConfig, len(topicNames))
 	for _, res := range response.Resources {
 		err := kerr.ErrorForCode(res.ErrorCode)
 		if err != nil {
@@ -69,7 +69,7 @@ func (s *Service) GetTopicsConfigs(ctx context.Context, topicNames []string, con
 			}
 		}
 
-		converted[res.ResourceName] = &TopicConfigs{
+		converted[res.ResourceName] = &TopicConfig{
 			TopicName:     res.ResourceName,
 			ConfigEntries: entries,
 		}
