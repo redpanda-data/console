d6a8c27c7204e4138a9fba9a72984e1dac77a5ab
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2021-02-09 16:11:10 +0100

    reassign partitions: steps, fix intermediate state for nested checkboxes

--- frontend/src/components/pages/brokers/ReassignPartitions.tsx
+++ frontend/src/components/pages/brokers/ReassignPartitions.tsx
@@ -1,64 +1,51 @@
 import React, { ReactNode, Component } from "react";
 import { observer } from "mobx-react";
-import { Empty, Table, Statistic, Row, Skeleton, Checkbox, Tooltip, Space, Descriptions } from "antd";
+import { Empty, Table, Statistic, Row, Skeleton, Checkbox, Steps, Button } from "antd";
 import { ColumnProps } from "antd/lib/table";
 import { PageComponent, PageInitHelper } from "../Page";
 import { api } from "../../../state/backendApi";
 import { uiSettings } from "../../../state/ui";
 import { makePaginationConfig, sortField } from "../../misc/common";
-import { Broker, BrokerConfigEntry, Partition, TopicDetail } from "../../../state/restInterfaces";
+import { Broker, BrokerConfigEntry, Partition, TopicAction, TopicDetail } from "../../../state/restInterfaces";
 import { motion } from "framer-motion";
 import { animProps } from "../../../utils/animationProps";
 import { observable, computed, autorun, IReactionDisposer } from "mobx";
-import { prettyBytesOrNA } from "../../../utils/utils";
+import { prettyBytesOrNA, toJson } from "../../../utils/utils";
 import { appGlobal } from "../../../state/appGlobal";
 import Card from "../../misc/Card";
-import Icon, { CrownOutlined } from '@ant-design/icons';
+import Icon, { CheckCircleOutlined, CheckSquareOutlined, CrownOutlined, HddOutlined, UnorderedListOutlined } from '@ant-design/icons';
 import { DefaultSkeleton, OptionGroup } from "../../../utils/tsxUtils";
-import { DataValue } from "../topics/Tab.Config";
-import { stringify } from "query-string";
+import { ChevronLeftIcon, ChevronRightIcon } from "@primer/octicons-v2-react";
+const { Step } = Steps;
 
-
-interface PartitionReassignment {
-    topicPartitions: {
-        [topicName: string]: number[] // topicName -> array of partitionIds
-    },
-    targetBrokers: number[],
+interface PartitionSelection { // Which partitions are selected?
+    [topicName: string]: number[] // topicName -> array of partitionIds
+}
+interface PartitionTargets { // Where should the partitions be moved to?
+    [topicName: string]: { partitionId: number, targetBrokerId?: number };
 }
 
-interface TopicPartitions {
-    topicName: string,
-    replicationFactor: number,
-    partitions: {
-        id: number,
-        brokerIds: number[],
-        size: number,
-    }[] | undefined,
-    brokersSum: number,
-    sizeSum: number,
-}
+const steps = [
+    { step: 0, title: 'Select Partitions', icon: <UnorderedListOutlined /> },
+    { step: 1, title: 'Assign to Brokers', icon: <HddOutlined /> },
+    { step: 2, title: 'Review and Confirm', icon: <CheckCircleOutlined /> },
+];
+
 
 @observer
 class ReassignPartitions extends PageComponent {
-
     pageConfig = makePaginationConfig(100, true);
-
-    @observable partitionSelection: {
-        [topicName: string]: number[] // topicName -> array of partitionIds
-    } = {};
-
     autorunHandle: IReactionDisposer | undefined = undefined;
+
+    @observable partitionSelection: PartitionSelection = {};
+    @observable currentStep = 0;
+
 
     initPage(p: PageInitHelper): void {
         p.title = 'Reassign Partitions';
         p.addBreadcrumb('Reassign Partitions', '/reassign-partitions');
 
         appGlobal.onRefresh = () => this.refreshData(true);
-
-        this.setSelection = this.setSelection.bind(this);
-        this.isSelected = this.isSelected.bind(this);
-        this.getSelectedPartitions = this.getSelectedPartitions.bind(this);
-
         this.refreshData(true);
     }
 
@@ -83,20 +70,99 @@ class ReassignPartitions extends PageComponent {
 
     render() {
         if (!api.topics) return DefaultSkeleton;
-
-
         if (api.topicPartitions.size == 0) return <Empty />
 
-        const topicPartitions: (TopicDetail & {
-            partitions: Partition[] | null | undefined,
-        })[] = api.topics.map(topic => {
+        return <>
+            <motion.div {...animProps} style={{ margin: '2px', marginBottom: '16px' }}>
+                <Card>
+                    <Row> {/* type="flex" */}
+                        <Statistic title='Total Partitions' value={123} />
+                        <Statistic title='Leader Partitions' value={123} />
+                        <Statistic title='Replica Partitions' value={123} />
+                        <Statistic title='Broker Count' value={123} />
+                    </Row>
+                </Card>
+
+                <Card>
+                    {/* Steps */}
+                    <div style={{ margin: '0 1em 1em 1em' }}>
+                        <Steps current={this.currentStep}>
+                            {steps.map(item => <Step key={item.title} {...item} />)}
+                        </Steps>
+                    </div>
+
+                    {/* Content */}
+                    {(() => {
+                        switch (this.currentStep) {
+                            case 0: return <StepSelectPartitions partitionSelection={this.partitionSelection} />;
+                            case 1: return "assign to brokers";
+                            case 2: return "review";
+                        }
+                    })()}
+
+                    {/* Navigation */}
+                    <div style={{ marginTop: '1em' }}>
+                        <Button onClick={() => this.currentStep--}
+                            style={{ width: '12em' }}>
+                            <span><ChevronLeftIcon /></span>
+                            <span>Back</span>
+                        </Button>
+                        <Button onClick={() => this.currentStep++} type='primary'
+                            style={{ width: '12em' }}
+                        >
+                            <span>Next Step</span>
+                            <span><ChevronRightIcon /></span>
+                        </Button>
+                    </div>
+
+                    {/* Debug */}
+                    <div>
+                        <h2>Current Selection</h2>
+                        <div className='codeBox'>{toJson(this.partitionSelection, 4)}</div>
+                    </div>
+
+                </Card>
+            </motion.div>
+        </>
+    }
+}
+
+type TopicWithPartitions = TopicDetail & { partitions: Partition[] };
+
+@observer
+class StepSelectPartitions extends Component<{ partitionSelection: PartitionSelection }> {
+    pageConfig = makePaginationConfig(100, true);
+    autorunHandle: IReactionDisposer | undefined = undefined;
+
+    topicPartitions: TopicWithPartitions[] = [];
+
+    constructor(props: any) {
+        super(props);
+        this.setSelection = this.setSelection.bind(this);
+        this.isSelected = this.isSelected.bind(this);
+        this.getSelectedPartitions = this.getSelectedPartitions.bind(this);
+        this.getTopicCheckState = this.getTopicCheckState.bind(this);
+    }
+
+    componentWillUnmount() {
+        if (this.autorunHandle) {
+            this.autorunHandle();
+            this.autorunHandle = undefined;
+        }
+    }
+
+    render() {
+        if (!api.topics) return DefaultSkeleton;
+        if (api.topicPartitions.size == 0) return <Empty />
+
+        this.topicPartitions = api.topics.map(topic => {
             return {
                 ...topic,
-                partitions: api.topicPartitions.get(topic.topicName)
+                partitions: api.topicPartitions.get(topic.topicName)!
             }
         });
 
-        const columns: ColumnProps<typeof topicPartitions[0]>[] = [
+        const columns: ColumnProps<TopicWithPartitions>[] = [
             { width: 'auto', title: 'Topic', dataIndex: 'topicName' },
             { width: 'auto', title: 'Partitions', dataIndex: 'partitionCount' },
             { width: 'auto', title: 'Replication Factor', dataIndex: 'replicationFactor' },
@@ -108,48 +174,59 @@ class ReassignPartitions extends PageComponent {
         ]
 
         return <>
-            <motion.div {...animProps} style={{ margin: '0 1rem' }}>
-                <Card>
-                    <Row> {/* type="flex" */}
-                        <Statistic title='Total Partitions' value={123} />
-                        <Statistic title='Leader Partitions' value={123} />
-                        <Statistic title='Replica Partitions' value={123} />
-                        <Statistic title='Broker Count' value={123} />
-                    </Row>
-                </Card>
+            <Table
+                style={{ margin: '0', }} size={'middle'}
+                pagination={this.pageConfig}
+                dataSource={this.topicPartitions}
+                rowKey={r => r.topicName}
+                rowClassName={() => 'pureDisplayRow'}
+                rowSelection={{
+                    type: 'checkbox',
+                    columnTitle: <></>, // don't show "select all" checkbox
+                    renderCell: (value: boolean, record, index: number, originNode: React.ReactNode) => {
+                        return <IndeterminateCheckbox originalCheckbox={originNode} getCheckState={() => this.getTopicCheckState(record.topicName)} />
 
-                <Card>
-                    <Table
-                        style={{ margin: '0', }} size={'middle'}
-                        pagination={this.pageConfig}
-                        dataSource={topicPartitions}
-                        rowKey={r => r.topicName}
-                        rowClassName={() => 'pureDisplayRow'}
-                        rowSelection={{
-                            type: 'checkbox',
-                        }}
-                        columns={columns}
-                        expandable={{
-                            expandIconColumnIndex: 1,
-                            expandedRowRender: topic => topic.partitions
-                                ? <PartitionTable
-                                    topic={topic}
-                                    topicPartitions={topic.partitions}
-                                    isSelected={this.isSelected}
-                                    setSelection={this.setSelection}
-                                    getSelectedPartitions={() => this.getSelectedPartitions(topic.topicName)}
-                                />
-                                : <>Error loading partitions</>,
-                            // expandedRowClassName: r => 'noPadding',
-                        }}
-                    />
-                </Card>
-            </motion.div>
+                    },
+                    onSelect: (record, selected: boolean, selectedRows) => {
+                        // Select all partitions in this topic
+                        if (record.partitions)
+                            for (const p of record.partitions)
+                                this.setSelection(record.topicName, p.id, selected);
+                    },
+                    getCheckboxProps: record => {
+                        if (record.partitions) {
+                            const selectedPartitions = this.props.partitionSelection[record.topicName];
+                            let hasPartialSelection = false;
+                            if (selectedPartitions && selectedPartitions.length != 0 && selectedPartitions.length < record.partitionCount)
+                                hasPartialSelection = true;
+
+                            return { indeterminate: hasPartialSelection };
+                        }
+                        return {};
+                    },
+                    // todo: in setSelected() call, keep track of what the 'topic checkbox state' should be (selected, indeterminate)
+                    // selectedRowKeys:
+                }}
+                columns={columns}
+                expandable={{
+                    expandIconColumnIndex: 1,
+                    expandedRowRender: topic => topic.partitions
+                        ? <PartitionTable
+                            topic={topic}
+                            topicPartitions={topic.partitions}
+                            isSelected={this.isSelected}
+                            setSelection={this.setSelection}
+                            getSelectedPartitions={() => this.getSelectedPartitions(topic.topicName)}
+                        />
+                        : <>Error loading partitions</>,
+                    // expandedRowClassName: r => 'noPadding',
+                }}
+            />
         </>
     }
 
     setSelection(topic: string, partition: number, isSelected: boolean) {
-        const partitions = this.partitionSelection[topic] ?? [];
+        const partitions = this.props.partitionSelection[topic] ?? [];
 
         if (isSelected) {
             partitions.pushDistinct(partition);
@@ -157,21 +234,51 @@ class ReassignPartitions extends PageComponent {
             partitions.remove(partition);
         }
 
-        this.partitionSelection[topic] = partitions;
+        this.props.partitionSelection[topic] = partitions;
     }
 
     getSelectedPartitions(topic: string) {
-        const partitions = this.partitionSelection[topic];
+        const partitions = this.props.partitionSelection[topic];
         if (!partitions) return [];
         return partitions;
     }
 
     isSelected(topic: string, partition: number) {
-        const partitions = this.partitionSelection[topic];
+        const partitions = this.props.partitionSelection[topic];
         if (!partitions) return false;
         return partitions.includes(partition);
     }
 
+    getTopicCheckState(topicName: string): { checked: boolean, indeterminate: boolean } {
+        const tp = this.topicPartitions.first(t => t.topicName == topicName);
+        if (!tp) return { checked: false, indeterminate: false };
+
+        const selected = this.props.partitionSelection[topicName];
+        if (!selected) return { checked: false, indeterminate: false };
+
+        if (selected.length == 0)
+            return { checked: false, indeterminate: false };
+
+        if (selected.length == tp.partitionCount)
+            return { checked: true, indeterminate: false };
+
+        return { checked: false, indeterminate: true };
+    }
+
+}
+
+@observer
+class IndeterminateCheckbox extends Component<{ originalCheckbox: React.ReactNode, getCheckState: () => { checked: boolean, indeterminate: boolean } }> {
+
+    render() {
+        const state = this.props.getCheckState();
+        // console.log(`checkbox${index} props: ${(originNode as any).props?.indeterminate}`)
+        const clone = React.cloneElement(this.props.originalCheckbox as any, {
+            checked: state.checked,
+            indeterminate: state.indeterminate,
+        });
+        return clone;
+    }
 }
 
 @observer
@@ -198,12 +305,12 @@ class PartitionTable extends Component<{
                     // onChange: (selected) => console.log('onChange selected=', selected),
                     selectedRowKeys: this.props.getSelectedPartitions().slice(),
                     onSelect: (record, selected: boolean, selectedRows) => {
+                        // console.log("SetSelection, now selected: ", { getSelectedPartitions: this.props.getSelectedPartitions() });
                         this.props.setSelection(this.props.topic.topicName, record.id, selected);
-                        console.log("SetSelection, now selected: ", this.props.getSelectedPartitions());
                     },
                 }}
                 columns={[
-                    { width: 100, title: 'ID', dataIndex: 'id', sortOrder: 'ascend', sorter: (a, b) => a.id - b.id },
+                    { width: 100, title: 'Partition', dataIndex: 'id', sortOrder: 'ascend', sorter: (a, b) => a.id - b.id },
                     { width: 160, title: 'Leading Broker', dataIndex: 'leader' },
                     { width: undefined, title: 'Brokers', render: (v, record,) => record.replicas.slice().sort((a, b) => a - b).join(", ") },
                 ]}
