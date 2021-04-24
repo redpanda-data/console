a0b47d7ea78c7e7db49c9cfd8a9d64df6a7b2d9e
Martin Schneppenheim <weeco91@gmail.com>
2021-04-24 15:24:38 +0200

    Remove support for encrypted TLS key as it's insecure by design

--- backend/pkg/schema/config_tls.go
+++ backend/pkg/schema/config_tls.go
@@ -1,18 +1,10 @@
 package schema
 
-import "flag"
-
 // TLSConfig to connect to Schema via TLS
 type TLSConfig struct {
 	Enabled               bool   `yaml:"enabled"`
 	CaFilepath            string `yaml:"caFilepath"`
 	CertFilepath          string `yaml:"certFilepath"`
 	KeyFilepath           string `yaml:"keyFilepath"`
-	Passphrase            string `yaml:"passphrase"`
 	InsecureSkipTLSVerify bool   `yaml:"insecureSkipTlsVerify"`
-}
-
-// RegisterFlags for all sensitive Schema TLS configs
-func (c *TLSConfig) RegisterFlags(f *flag.FlagSet) {
-	f.StringVar(&c.Passphrase, "kafka.schemaRegistry.tls.passphrase", "", "Passphrase to optionally decrypt the private key")
 }
