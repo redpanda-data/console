45705de072d89f787e8c2c934a444f3bf617ee8e
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2021-05-14 16:25:46 +0200

    cleanup, remove debug code, small fixes for avatar image/sizing

--- frontend/src/components/misc/UserButton.tsx
+++ frontend/src/components/misc/UserButton.tsx
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { observer } from "mobx-react"
-import { Menu, Select, Avatar, Popconfirm, Dropdown } from 'antd';
-import { uiSettings } from '../../state/ui';
-import { RenderTrap, Spacer } from './common';
+import { Select, Avatar, Dropdown } from 'antd';
 import { api } from '../../state/backendApi';
-import Icon, { UserOutlined } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
 import { IsBusiness } from '../../utils/env';
-import { ChevronDownIcon } from '@primer/octicons-v2-react';
 import { makeObservable, observable } from 'mobx';
 import { UserPreferencesDialog } from './UserPreferences';
-import MenuContext from 'antd/lib/menu/MenuContext';
-import Sider, { SiderContext } from 'antd/lib/layout/Sider';
-
-const { Option } = Select;
 
 @observer
 export class UserProfile extends Component {
@@ -33,7 +26,7 @@ export class UserProfile extends Component {
         // luckily antd passes many props to rc-dropdown using the spread operator
         const noExpand = { minOverlayWidthMatchTrigger: false };
 
-        const dropdown = <>
+        return <>
             <Dropdown overlayClassName="avatarDropdown"
                 overlay={() => <UserMenu onOpenPreferences={() => {
                     this.preferencesOpen = true;
@@ -43,19 +36,14 @@ export class UserProfile extends Component {
                 arrow={false}
                 placement="topCenter"
                 visible={this.menuOpen}
-                onVisibleChange={e => {
-                    // if (this.preferencesOpen) {
-                    //     this.menuOpen = false;
-                    //     return;
-                    // }
-                    this.menuOpen = e;
-                }}
+                onVisibleChange={e => this.menuOpen = e}
                 {...noExpand}
             >
                 <div className="profile">
                     <div className="avatar">
-                        {/* <img src="https://cdn.discordapp.com/avatars/372161181080748038/ca238aec10ac4835bccee89e37793701.png?size=128" /> */}
-                        <Avatar icon={<img src={user.meta.avatarUrl} />} />
+                        <Avatar src={user.meta.avatarUrl} alt={user.meta.name} >
+                            <UserOutlined />
+                        </Avatar>
                     </div>
                     <div className="text">
                         <div className="userName">{user.meta.name}</div>
@@ -65,22 +53,12 @@ export class UserProfile extends Component {
             </Dropdown>
 
             <UserPreferencesDialog visible={this.preferencesOpen} onClose={() => this.preferencesOpen = false} />
-        </>;
-
-        return (
-            dropdown
-        );
+        </>
     }
 }
 
 @observer
 export class UserMenu extends Component<{ onOpenPreferences: () => void }> {
-
-    constructor(p: any) {
-        super(p);
-        // makeObservable(this);
-    }
-
     render() {
         const userName = api.userData?.user?.meta?.name ?? 'null';
 
@@ -101,19 +79,7 @@ export class UserMenu extends Component<{ onOpenPreferences: () => void }> {
                     Logout
                 </div>
             </div>
-
-            // <Menu className="avatarMenu" >
-            //     <Menu.Item style={{ pointerEvents: 'none' }} >
-            //         Signed in as<br />
-            //         <span style={{ fontWeight: 'bold' }}>{user.meta.name}</span>
-            //     </Menu.Item>
-            //     <Menu.Divider />
-            //     <Menu.Item key="0" onClick={() => this.preferencesOpen = true}>Preferences</Menu.Item>
-            //     <Menu.Item key="1" onClick={() => { api.logout(); window.location.reload(); }}>Logout</Menu.Item>
-            // </Menu>
         )
     }
 }
-
-
 
