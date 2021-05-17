43029a0e925d4b134965b73201528e6a44d06f42
Martin Schneppenheim <weeco91@gmail.com>
2021-05-17 21:00:39 +0200

    Test kafka connect cluster connectivity concurrently

--- backend/pkg/connect/service.go
+++ backend/pkg/connect/service.go
@@ -4,29 +4,27 @@ import (
 	"context"
 	"crypto/tls"
 	"go.uber.org/zap"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type Service struct {
 	Cfg              Config
 	Logger           *zap.Logger
-	ClientsByCluster map[string]*Client
+	ClientsByCluster map[ConfigCluster]*Client
 }
 
 func NewService(cfg Config, logger *zap.Logger) (*Service, error) {
-	clients := make(map[string]*Client)
+	logger.Info("creating Kafka connect HTTP clients and testing connectivity to all clusters")
 
-	logger.Info("creating Kafka connect service and testing cluster connectivity")
-	clustersWithConnectivity := 0
-
-	shortCtx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
-	defer cancel()
-	// TODO: Check connectivity concurrently?
+	// 1. Create a client for each configured Connect cluster
+	clientsByCluster := make(map[ConfigCluster]*Client)
 	for _, clusterCfg := range cfg.Clusters {
 		// Create dedicated Connect HTTP Client for each cluster
 		childLogger := logger.With(
 			zap.String("cluster_name", clusterCfg.Name),
-			zap.String("address", clusterCfg.URL))
+			zap.String("cluster_address", clusterCfg.URL))
 
 		opts := []ClientOption{WithTimeout(60 * time.Second), WithUserAgent("Kowl")}
 
@@ -51,20 +49,50 @@ func NewService(cfg Config, logger *zap.Logger) (*Service, error) {
 
 		// Create client
 		client := NewClient(opts...)
-		_, err = client.GetRoot(shortCtx)
-		if err != nil {
-			childLogger.Error("failed to check connect cluster HTTP connectivity", zap.Error(err))
-		} else {
-			clustersWithConnectivity++
-		}
+		clientsByCluster[clusterCfg] = client
 	}
-	logger.Info("successfully tested Kafka connect cluster connectivity",
-		zap.Int("connected_clusters", clustersWithConnectivity),
-		zap.Int("failed_clusters", len(cfg.Clusters)-clustersWithConnectivity))
+	svc := &Service{
+		Cfg:              cfg,
+		Logger:           logger,
+		ClientsByCluster: clientsByCluster,
+	}
+
+	// 2. Test connectivity against each cluster concurrently
+	shortCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	svc.TestConnectivity(shortCtx)
+
+	logger.Info("successfully create Kafka connect service")
 
 	return &Service{
 		Cfg:              cfg,
 		Logger:           logger,
-		ClientsByCluster: clients,
+		ClientsByCluster: clientsByCluster,
 	}, nil
+}
+
+// TestConnectivity will send to each Kafka connect client a request to check if it is reachable. If a cluster is not
+// reachable an error log message will be printed.
+func (s *Service) TestConnectivity(ctx context.Context) {
+	var successfulChecks uint32
+	wg := sync.WaitGroup{}
+	for cfg, client := range s.ClientsByCluster {
+		wg.Add(1)
+		go func(cfg ConfigCluster, c *Client) {
+			defer wg.Done()
+			_, err := c.GetRoot(ctx)
+			if err != nil {
+				s.Logger.Warn("connect cluster is not reachable",
+					zap.String("cluster_name", cfg.Name),
+					zap.String("cluster_address", cfg.URL),
+					zap.Error(err))
+				return
+			}
+			atomic.AddUint32(&successfulChecks, 1)
+		}(cfg, client)
+	}
+	wg.Wait()
+	s.Logger.Info("tested Kafka connect cluster connectivity",
+		zap.Uint32("successful_clusters", successfulChecks),
+		zap.Uint32("failed_clusters", uint32(len(s.ClientsByCluster))-successfulChecks))
 }
