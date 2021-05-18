2c6f68cf3d2fdc30ace4edbbeb23fdd6ae8c7c87
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2021-05-18 23:10:08 +0200

    add react testing library for future componen unit tests

---
+++ frontend/src/setupTests.js
@@ -0,0 +1,13 @@
+Object.defineProperty(global, 'matchMedia', {
+    writable: true,
+    value: (query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    }),
+});
