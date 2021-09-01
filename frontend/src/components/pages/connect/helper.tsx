baf5ca4ce2436d88d4d085e9fc79f938f0b5144d
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2021-09-01 16:49:57 +0200

    match kafka connect connector logos by className or prefix; fix extension of cassandra image

--- frontend/src/components/pages/connect/helper.tsx
+++ frontend/src/components/pages/connect/helper.tsx
@@ -1,34 +1,104 @@
 
 
 import { Button, Empty, Popover, Statistic } from 'antd';
-import { motion } from 'framer-motion';
+import { m, motion } from 'framer-motion';
 import { observer } from 'mobx-react';
 import React, { Component, CSSProperties } from 'react';
 
-import DatagenLogo from '../../../assets/connectors/datagen.png';
 import { api } from '../../../state/backendApi';
 import { ClusterConnectorInfo } from '../../../state/restInterfaces';
 import { animProps } from '../../../utils/animationProps';
 import { findPopupContainer, LayoutBypass } from '../../../utils/tsxUtils';
 import Card from '../../misc/Card';
 
+import DatagenLogo from '../../../assets/connectors/datagen.png';
+import MsSqlLogo from '../../../assets/connectors/mssql.png';
+import MongoDBLogo from '../../../assets/connectors/mongodb.png';
+import DebeziumLogo from '../../../assets/connectors/debezium.png';
+
 interface ConnectorMetadata {
-    friendlyName: string;
-    logo?: JSX.Element,
+    readonly className?: string;         // match by exact match
+    readonly classNamePrefix?: string;   // match by prefix
+
+    readonly logo?: JSX.Element,         // img element for the connector
+    readonly friendlyName?: string;      // override display name (instead of just 'className without namespace')
 }
-export const connectorMetadata: {
-    [className: string]: ConnectorMetadata | undefined
-} = {
+export const connectorMetadata: ConnectorMetadata[] = [
+    {
+        className: "io.confluent.kafka.connect.datagen.DatagenConnector",
+        logo: <img src={DatagenLogo} alt='datagen logo' className='connectorLogo' />
+    } as const,
+    {
+        className: "io.debezium.connector.sqlserver.SqlServerConnector",
+        logo: <img src={MsSqlLogo} alt='mssql logo' className='connectorLogo' />
+    } as const,
+    {
+        className: "io.debezium.connector.sqlserver.MongoDBConnector",
+        logo: <img src={MongoDBLogo} alt='mongo db logo' className='connectorLogo' />
+    } as const,
+    {
+        classNamePrefix: "io.debezium.",
+        logo: <img src={DebeziumLogo} alt='debezium logo' className='connectorLogo' />
+    } as const,
+];
 
-    "io.confluent.kafka.connect.datagen.DatagenConnector": {
-        friendlyName: 'DatagenConnector',
-        logo: <img src={DatagenLogo} alt='datagen logo' className='connectorLogo' />,
-    },
 
-} as const;
+export const ConnectorClass = React.memo((props: { connector: ClusterConnectorInfo }) => {
+    const c = props.connector;
+    let meta: ConnectorMetadata | null = null;
 
+    for (const m of connectorMetadata) {
+        if ((m.className && m.className == c.class) ||
+            (m.classNamePrefix && c.class.startsWith(m.classNamePrefix))) {
+            meta = m;
+            break;
+        }
+    }
 
+    const displayName = meta?.friendlyName ?? removeNamespace(c.class);
 
+    let content: JSX.Element;
+    if (!meta) {
+        // No additional information available
+        // Default: simple display
+        content = <span>{displayName}</span>
+    }
+    else {
+        // We found custom metadata
+        // Show connector logo
+        content = <span style={{ display: 'inline-flex', gap: '.5em', alignItems: 'center' }}>
+            {meta.logo &&
+                <LayoutBypass height='0px' width='26px'>
+                    {/* <span style={{ display: 'inline-block', maxHeight: '0px' }}> */}
+                    {meta.logo}
+                    {/* </span> */}
+                </LayoutBypass>
+            }
+            {displayName}
+        </span>
+    }
+
+    return <span>
+        <Popover placement='right' overlayClassName='popoverSmall'
+            getPopupContainer={findPopupContainer}
+            content={<div style={{ maxWidth: '500px', whiteSpace: 'pre-wrap' }}>
+                {c.class}
+            </div>}
+        >
+            {content}
+        </Popover>
+    </span>
+});
+
+function removeNamespace(className: string): string {
+    if (!className) return className;
+
+    const lastDot = className.lastIndexOf('.');
+    if (lastDot >= 0)
+        return className.substr(lastDot + 1, undefined);
+
+    return className;
+}
 
 export const StatisticsCard = observer(() => {
     const totalClusters = api.connectConnectors?.clusters?.length ?? '...';
@@ -45,48 +115,6 @@ export const StatisticsCard = observer(() => {
 
 
 
-
-export const ConnectorClass = React.memo((props: { connector: ClusterConnectorInfo }) => {
-    const c = props.connector;
-    // const meta = connectorMetadata[c.class];
-    const meta = null as any;
-
-    let content: JSX.Element;
-    if (meta) {
-        // Use known connector name (and maybe image)
-        content = <span style={{ display: 'inline-flex', gap: '.5em', alignItems: 'center' }}>
-            {meta.logo &&
-                <LayoutBypass height='0px' width='26px'>
-                    {/* <span style={{ display: 'inline-block', maxHeight: '0px' }}> */}
-                    {meta.logo}
-                    {/* </span> */}
-                </LayoutBypass>
-            }
-            {meta.friendlyName}
-        </span>
-    }
-    else {
-        // Otherwise just remove the namespace of the class and use that
-        let shortName = c.class;
-        const lastDot = shortName.lastIndexOf('.');
-        if (lastDot >= 0)
-            shortName = shortName.substr(lastDot + 1, undefined);
-
-        content = <span>{shortName}</span>
-    }
-
-
-    return <span>
-        <Popover placement='rightTop' overlayClassName='popoverSmall'
-            getPopupContainer={findPopupContainer}
-            content={<div style={{ maxWidth: '500px', whiteSpace: 'pre-wrap' }}>
-                {c.class}
-            </div>}
-        >
-            {content}
-        </Popover>
-    </span>
-});
 
 export function NotConfigured() {
     return (
