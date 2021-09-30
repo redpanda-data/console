6b62f5137813f8ed87024e632e8357c188ebfd8b
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2021-09-30 18:27:06 +0200

    use real validation endpoint

--- frontend/src/components/pages/connect/CreateConnector.tsx
+++ frontend/src/components/pages/connect/CreateConnector.tsx
@@ -1,105 +1,97 @@
-import React, { useState } from 'react';
-import { PageComponent, PageInitHelper } from '../Page';
-import { animProps } from '../../../utils/animationProps';
-import { motion } from 'framer-motion';
-import { ApiOutlined, DatabaseOutlined, SearchOutlined, SettingOutlined } from '@ant-design/icons';
-import { Wizard, WizardStep } from '../../misc/Wizard';
+import React, {useState} from 'react';
+import {PageComponent, PageInitHelper} from '../Page';
+import {animProps} from '../../../utils/animationProps';
+import {motion} from 'framer-motion';
+import {ApiOutlined, DatabaseOutlined, SearchOutlined} from '@ant-design/icons';
+import {Wizard, WizardStep} from '../../misc/Wizard';
 import Card from '../../misc/Card';
-
-import styles from './CreateConnector.module.scss';
 import {observer} from 'mobx-react';
 import {api} from '../../../state/backendApi';
 import {appGlobal} from '../../../state/appGlobal';
-import {ClusterConnectors, ConnectorProperty, ConnectorValidationResult} from '../../../state/restInterfaces';
-import {Alert, Select} from 'antd';
+import {ClusterConnectors, ConnectorValidationResult} from '../../../state/restInterfaces';
+import {Alert, Select, Table} from 'antd';
 import {HiddenRadioList} from '../../misc/HiddenRadioList';
 import {ConnectorBoxCard, ConnectorPlugin} from './ConnectorBoxCard';
-import {DemoPage} from './dynamic-ui/components';
-import { observer } from 'mobx-react';
-import { api } from '../../../state/backendApi';
-import { appGlobal } from '../../../state/appGlobal';
-import { ClusterConnectors } from '../../../state/restInterfaces';
-import { Select } from 'antd';
-import { HiddenRadioList } from '../../misc/HiddenRadioList';
-import { ConnectorBoxCard, ConnectorPlugin } from './ConnectorBoxCard';
-import { ConfigPage } from './dynamic-ui/components';
+import {ConfigPage} from './dynamic-ui/components';
 import KowlEditor from '../../misc/KowlEditor';
 import {useHistory} from 'react-router-dom';
 
-const { Option } = Select;
+import styles from './CreateConnector.module.scss';
+
+const {Option} = Select;
 
 interface ConnectorTypeProps {
-    connectClusters: Array<ClusterConnectors>;
-    activeCluster: string | null;
-    onActiveClusterChange: (clusterName: string | null) => void;
-    selectedPlugin: ConnectorPlugin | null;
-    onPluginSelectionChange: (plugin: ConnectorPlugin | null) => void;
+  connectClusters: Array<ClusterConnectors>;
+  activeCluster: string | null;
+  onActiveClusterChange: (clusterName: string | null) => void;
+  selectedPlugin: ConnectorPlugin | null;
+  onPluginSelectionChange: (plugin: ConnectorPlugin | null) => void;
 }
 
 const ConnectorType = observer(({
-    connectClusters,
-    activeCluster,
-    onActiveClusterChange,
-    selectedPlugin,
-    onPluginSelectionChange,
-}: ConnectorTypeProps) => {
-    return (<>
-        <h2>Installation Target</h2>
-        <Select<string> style={{ minWidth: '300px' }}
-            placeholder="Choose Connect Cluster…"
-            onChange={(clusterName) => {
-                api.refreshClusterAdditionalInfo(clusterName, true);
-                onActiveClusterChange(clusterName);
-            }}
-            defaultValue={activeCluster || undefined}
-        >
-            {connectClusters.map(({ clusterName }) => <Option key={clusterName} value={clusterName}>{clusterName}</Option>)}
-        </Select>
-        {activeCluster == null ? null : (<>
-            <h2>Connector Type</h2>
+                                  connectClusters,
+                                  activeCluster,
+                                  onActiveClusterChange,
+                                  selectedPlugin,
+                                  onPluginSelectionChange,
+                                }: ConnectorTypeProps) => {
+  return (<>
+    <h2>Installation Target</h2>
+    <Select<string> style={{minWidth: '300px'}}
+                    placeholder="Choose Connect Cluster…"
+                    onChange={(clusterName) => {
+                      api.refreshClusterAdditionalInfo(clusterName, true);
+                      onActiveClusterChange(clusterName);
+                    }}
+                    defaultValue={activeCluster || undefined}
+    >
+      {connectClusters.map(({clusterName}) => <Option key={clusterName} value={clusterName}>{clusterName}</Option>)}
+    </Select>
+    {activeCluster == null ? null : (<>
+      <h2>Connector Type</h2>
 
-            <HiddenRadioList<ConnectorPlugin>
-                name={'connector-type'}
-                onChange={onPluginSelectionChange}
-                value={selectedPlugin ?? undefined}
-                options={api.connectAdditionalClusterInfo.get(activeCluster)?.plugins.map(plugin => ({
-                    value: plugin,
-                    render: (card) => <ConnectorBoxCard {...card} connectorPlugin={plugin} />,
-                })) || []} />
-        </>)}
-    </>);
+      <HiddenRadioList<ConnectorPlugin>
+          name={'connector-type'}
+          onChange={onPluginSelectionChange}
+          value={selectedPlugin ?? undefined}
+          options={api.connectAdditionalClusterInfo.get(activeCluster)?.plugins.map(plugin => ({
+            value: plugin,
+            render: (card) => <ConnectorBoxCard {...card} connectorPlugin={plugin}/>,
+          })) || []}/>
+    </>)}
+  </>);
 });
 
 @observer
 class CreateConnector extends PageComponent {
-    initPage(p: PageInitHelper) {
-        p.title = 'Create Connector';
-        p.addBreadcrumb('Create Connector', '/create-connector');
+  initPage(p: PageInitHelper) {
+    p.title = 'Create Connector';
+    p.addBreadcrumb('Create Connector', '/create-connector');
 
-        this.refreshData(false);
-        appGlobal.onRefresh = () => this.refreshData(true);
-    }
+    this.refreshData(false);
+    appGlobal.onRefresh = () => this.refreshData(true);
+  }
 
-    refreshData(force: boolean) {
-        api.refreshConnectClusters(force);
-    }
+  refreshData(force: boolean) {
+    api.refreshConnectClusters(force);
+  }
 
-    render() {
-        const clusters = api.connectConnectors?.clusters;
-        if (clusters == null) return null;
+  render() {
+    const clusters = api.connectConnectors?.clusters;
+    if (clusters == null) return null;
 
-        return (
-            <motion.div {...animProps} className={styles.motionContainer}>
-                <Card className={styles.wizardView}>
-                    <ConnectorWizard connectClusters={clusters} />
-                </Card>
-            </motion.div>
-        );
-    }
+    return (
+        <motion.div {...animProps} className={styles.motionContainer}>
+          <Card className={styles.wizardView}>
+            <ConnectorWizard connectClusters={clusters}/>
+          </Card>
+        </motion.div>
+    );
+  }
 }
 
 interface ConnectorWizardProps {
-    connectClusters: Array<ClusterConnectors>;
+  connectClusters: Array<ClusterConnectors>;
 }
 
 function ConnectorWizard({connectClusters}: ConnectorWizardProps) {
@@ -112,6 +104,12 @@ function ConnectorWizard({connectClusters}: ConnectorWizardProps) {
   const [invalidValidationResult, setInvalidValidationResult] = useState<ConnectorValidationResult | null>(null);
   const [validationFailure, setValidationFailure] = useState<unknown>(null);
   const [creationFailure, setCreationFailure] = useState<unknown>(null);
+
+  const clearErrors = () => {
+    setCreationFailure(null);
+    setValidationFailure(null);
+    setInvalidValidationResult(null);
+  };
 
   const steps: Array<WizardStep> = [
     {
@@ -142,7 +140,8 @@ function ConnectorWizard({connectClusters}: ConnectorWizardProps) {
             </div>
             : null}
         {(activeCluster && selectedPlugin)
-            ? <ConfigPage clusterName={activeCluster!} pluginClassName={selectedPlugin!.class} onChange={setProperties} />
+            ? <ConfigPage clusterName={activeCluster!} pluginClassName={selectedPlugin!.class}
+                          onChange={setProperties}/>
             : <div>no cluster or plugin selected</div>
         }
       </>,
@@ -169,13 +168,10 @@ function ConnectorWizard({connectClusters}: ConnectorWizardProps) {
       />,
       postConditionMet: () => true,
       async transitionConditionMet(): Promise<{ conditionMet: boolean }> {
+        clearErrors();
         try {
-          const validationResult = await Promise.resolve<ConnectorValidationResult>({
-            name: 'foo',
-            error_count: 0,
-            groups: [],
-            configs: [],
-          });
+          const validationResult = await api.validateConnectorConfig(activeCluster!, selectedPlugin!.class,
+              JSON.parse(properties));
 
           if (validationResult.error_count > 0) {
             setInvalidValidationResult(validationResult);
@@ -258,12 +254,12 @@ function Review({
         : null
     }
 
-    {validationFailure ? <Alert type="error" message={<>
+    {validationFailure ? <Alert style={{marginTop: '2rem'}} type="error" message={<>
       <strong>Validation Attempt Failed</strong>
       <p>{String(validationFailure)}</p>
     </>}/> : null}
 
-    {creationFailure ? <Alert type="error" message={<>
+    {creationFailure ? <Alert style={{marginTop: '2rem'}} type="error" message={<>
       <strong>Creation Attempt Failed</strong>
       <p>{String(creationFailure)}</p>
     </>}/> : null}
@@ -281,19 +277,26 @@ function Review({
 }
 
 function ValidationDisplay({validationResult}: { validationResult: ConnectorValidationResult }) {
-  return <Alert type="warning" message={<>
-    <strong>Submitted Configuration is Invalid</strong>
-    <ul style={{listStyle: 'none', padding: 0}}>
-      {validationResult.configs.map((connectorProperty) => {
-        return (<li>
-          <p>
-            <span><strong>Property name:</strong> {connectorProperty.value.name}</span><br/>
-            <span><strong>Property value:</strong> {connectorProperty.value.value}</span><br/>
-            <span><strong>Property errors:</strong> {connectorProperty.value.errors.join(', ')}</span>
-          </p>
-        </li>);
-      })}
-    </ul>
+  return <Alert style={{marginTop: '2rem'}} type="warning" message={<>
+    <h3>Submitted configuration is invalid</h3>
+
+    <Table pagination={false} size={'small'}
+           dataSource={validationResult.configs.filter(connectorProperty => connectorProperty.value.errors.length > 0).
+               map(cp => cp.value)} columns={[
+      {
+        title: 'Property Name',
+        dataIndex: 'name',
+        key: 'name',
+      }, {
+        title: 'Current Value',
+        dataIndex: 'value',
+        key: 'value',
+      }, {
+        title: 'Validation Errors',
+        dataIndex: 'errors',
+        key: 'errors',
+      },
+    ]} rowKey={record => record.name}/>
   </>}/>;
 }
 
