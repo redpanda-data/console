e2f6aeffd1ce510fbbe98a58edba443ae21132d7
David Pratt <davidpratt512@gmail.com>
2022-03-03 00:11:15 -0700

    topic docs: avoid double "pre" tags on code blocks

--- frontend/src/components/pages/topics/Tab.Docu.tsx
+++ frontend/src/components/pages/topics/Tab.Docu.tsx
@@ -56,6 +56,7 @@ export class TopicDocumentation extends Component<{ topic: Topic }> {
                 margin: null,
                 padding: null,
             }}
+            PreTag="div"
             language={match[1]}
             children={String(children).replace(/\n$/, '')} {...props} />
           ) : (
