d1e36c5a9b6d6c71533528d9b6f12e26e90bab03
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2022-06-15 15:05:47 +0200

    update  older npm packages;

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -9,13 +9,17 @@
  * by the Apache License, Version 2.0
  */
 
-const { override, addBabelPlugins } = require('customize-cra');
+const { override, addBabelPlugins, addBabelPreset } = require('customize-cra');
 
 
 module.exports = override(
+
+    addBabelPreset("@babel/preset-env"),
+
     addBabelPlugins(
         "@babel/plugin-proposal-nullish-coalescing-operator",
-        "@babel/plugin-proposal-logical-assignment-operators"
+        "@babel/plugin-proposal-logical-assignment-operators",
+        "@babel/plugin-proposal-numeric-separator",
     ),
 
     (webpackConfig) => {
