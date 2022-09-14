0c842ca4e642c1014f6191ba7eaee8b25cd18a0b
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2022-09-14 20:56:03 +0200

    use new react 18 "createRoot" function

--- frontend/src/bootstrap.tsx
+++ frontend/src/bootstrap.tsx
@@ -8,13 +8,9 @@
  * the Business Source License, use of this software will be governed
  * by the Apache License, Version 2.0
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import EmbeddedApp from './EmbeddedApp';
 
-
-const root = document.getElementById('root');
-ReactDOM.render(
-    <EmbeddedApp />,
-    root
-);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+root.render(<EmbeddedApp />);
