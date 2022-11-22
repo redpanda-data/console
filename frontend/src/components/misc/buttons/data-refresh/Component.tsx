4a977e6f8b242ca42c982bf8ba1b709c34ea598b
victorgawk <victor.gawk@gmail.com>
2022-11-22 09:13:54 -0300

    Auto Refresh button (#536)
    * Add Auto Refresh Button
    
    - Added a countdown span showing when the next refresh will happen.
    - To avoid auto refresh triggering while another refresh is still ongoing, the auto refresh now will only force-refresh when no other api requests are ongoing.
    - Updated auto refresh interval values:
      - default: 5 -> 10 seconds
      - minimum: 1 -> 5 seconds
      - maximum: 60 -> 300 seconds

--- frontend/src/components/misc/buttons/data-refresh/Component.tsx
+++ frontend/src/components/misc/buttons/data-refresh/Component.tsx
@@ -8,14 +8,19 @@
  * the Business Source License, use of this software will be governed
  * by the Apache License, Version 2.0
  */
-import { SyncIcon } from '@primer/octicons-react';
+import { PlayIcon, SyncIcon } from '@primer/octicons-react';
 import { Button, Popover } from 'antd';
 import { observer } from 'mobx-react';
 import { ReactNode, useEffect, useState } from 'react';
 import { appGlobal } from '../../../../state/appGlobal';
 import { api, REST_CACHE_DURATION_SEC } from '../../../../state/backendApi';
+import { uiSettings } from '../../../../state/ui';
 import { prettyMilliseconds } from '../../../../utils/utils';
 import styles from '../buttons.module.scss';
+
+let autoRefreshActive = false;
+let autoRefreshRefreshing = false;
+let autoRefreshTimer: NodeJS.Timeout;
 
 export const DataRefreshButton = observer(() => {
     const [lastRequestCount, setLastRequestCount] = useState(0);
@@ -34,6 +39,41 @@ export const DataRefreshButton = observer(() => {
         // TODO: small table that shows what cached data we have and how old it is
     };
 
+    const autoRefreshTextFunc = (): ReactNode => {
+        return <div style={{ maxWidth: '350px' }}>
+            Enable or disable automatic refresh every <span className="codeBox">{uiSettings.autoRefreshIntervalSecs}s</span>.
+        </div>;
+    };
+    const [autoRefreshCountdown, setAutoRefreshCountdown] = useState(uiSettings.autoRefreshIntervalSecs);
+    const autoRefreshFunc = () => {
+        if (autoRefreshActive) {
+            if (autoRefreshTimer) {
+                clearInterval(autoRefreshTimer);
+            }
+            autoRefreshActive = false;
+            appGlobal.onRefresh();
+        } else {
+            autoRefreshActive = true;
+            autoRefreshRefreshing = false;
+            appGlobal.onRefresh();
+            let nextRefreshTime = new Date().getTime() + (uiSettings.autoRefreshIntervalSecs * 1000);
+            autoRefreshTimer = setInterval(() => {
+                const currentTime = new Date().getTime();
+                if (nextRefreshTime < currentTime && api.activeRequests.length == 0) {
+                    if (!autoRefreshRefreshing) {
+                        autoRefreshRefreshing = true;
+                        appGlobal.onRefresh();
+                    } else {
+                        nextRefreshTime = currentTime + (uiSettings.autoRefreshIntervalSecs * 1000);
+                        setAutoRefreshCountdown(Math.ceil((nextRefreshTime - currentTime) / 1000));
+                        autoRefreshRefreshing = false;
+                    }
+                } else {
+                    setAutoRefreshCountdown(Math.ceil((nextRefreshTime - currentTime) / 1000));
+                }
+            }, 250);
+        }
+    };
 
     // Track how many requests we've sent in total
        const countStr = lastRequestCount > 1
@@ -42,12 +82,24 @@ export const DataRefreshButton = observer(() => {
 
     // maybe we need to use the same 'no vertical expansion' trick:
     return <div className={styles.dataRefreshButton}>
+        <Popover title="Auto Refresh" content={autoRefreshTextFunc} placement="rightTop" overlayClassName="popoverSmall" >
+            <Button icon={< PlayIcon size={16} />} shape="circle" className={`${styles.hoverButton} ${autoRefreshActive ? styles.pulsating : ''}`} onClick={autoRefreshFunc} />
+        </Popover>
         {
-        api.activeRequests.length == 0
+        api.activeRequests.length == 0 && !autoRefreshRefreshing
             ?
             <>
+                {
+                autoRefreshActive ?
+                <>
+                    <span style={{ padding: '0 10px', fontSize: '80%', userSelect: 'none' }}>Refreshing in {autoRefreshCountdown} secs</span>
+                </>
+                :
+                <>
+                </>
+                }
                 <Popover title="Force Refresh" content={refreshTextFunc} placement="rightTop" overlayClassName="popoverSmall" >
-                    <Button icon={< SyncIcon size={16} />} shape="circle" className={styles.hoverButton} onClick={() => appGlobal.onRefresh()} />
+                    <Button icon={< SyncIcon size={16} />} shape="circle" className={`${styles.hoverButton} ${autoRefreshActive ? styles.rotation : ''}`} onClick={() => appGlobal.onRefresh()} />
                 </Popover>
                 {/* <span style={{ paddingLeft: '.2em', fontSize: '80%' }}>fetched <b>1 min</b> ago</span> */}
             </>
--- frontend/src/state/ui.ts
+++ frontend/src/state/ui.ts
@@ -141,6 +141,7 @@ const defaultUiSettings = {
     topicDetailsActiveTabKey: undefined as TopicTabId | undefined,
 
     topicDetailsShowStatisticsBar: true, // for now: global for all topic details
+    autoRefreshIntervalSecs: 10,
     jsonViewer: {
         fontSize: '12px',
         lineHeight: '1em',
