8b6f8b3bcb86531b9f85f72949c938a9e781e9eb
Santiago Jimenez Giraldo <santiago@redpanda.com>
2023-03-21 19:51:01 +0100

    feat(frontend): validation for empty fields
    add validation for empty fields when the field is required

--- frontend/src/components/pages/connect/dynamic-ui/forms/ErrorWrapper.tsx
+++ frontend/src/components/pages/connect/dynamic-ui/forms/ErrorWrapper.tsx
@@ -1,9 +1,14 @@
+import { PropsWithoutRef } from 'react';
 import { FormField } from '@redpanda-data/ui';
 import { observer } from 'mobx-react';
 import { Property } from '../../../../../state/connect/state';
 
-export const ErrorWrapper = observer(function(props: { property: Property; input: JSX.Element }) {
+const isRequiredError = (name: string) => `Required configuration "${name}" must be provided`;
+const isEmpty = (property: Property) => property.value == '' || property.value == null;
+
+export const ErrorWrapper = observer(function(props: PropsWithoutRef<{ property: Property; input: JSX.Element }>) {
     const { property, input } = props;
+    const isRequired = property.entry.definition.required;
     const showErrors = property.errors.length > 0;
 
     const errors = showErrors ? property.errors : property.lastErrors;
@@ -15,10 +20,10 @@ export const ErrorWrapper = observer(function(props: { property: Property; input
     return (
         <div>
             <FormField
-                isInvalid={!!errorToShow}
-                isRequired={property.entry.definition.required}
+                isInvalid={!!errorToShow || (isEmpty(property) && isRequired)}
+                isRequired={isRequired}
                 label={property.entry.definition.display_name}
-                errorText={errorToShow}
+                errorText={isEmpty(property) && isRequired ? errorToShow || isRequiredError(property.name) : errorToShow}
                 description={property.entry.definition.documentation}
                 onClick={cycleError}
             >
