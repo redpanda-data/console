51b4f5e9025b645ff1dbea89840bea9c6e2fd47d
Tomasz Sadura <tomasz@redpanda.com>
2023-03-21 13:44:01 +0100

    backend: Rename S3 and GCS sink connectors files

--- backend/pkg/connector/patch/redpanda_gcs_sink.go
+++ backend/pkg/connector/patch/redpanda_gcs_sink.go
@@ -15,44 +15,43 @@ import (
 	"github.com/redpanda-data/console/backend/pkg/connector/model"
 )
 
-// ConfigPatchRedpandaS3 is a config patch that includes changes that shall be applied to the
-// Redpanda S3 Source and Sink connectors.
-type ConfigPatchRedpandaS3 struct {
+// ConfigPatchRedpandaGCS is a config patch that includes changes that shall be applied to the
+// Redpanda GCS Sink connector.
+type ConfigPatchRedpandaGCS struct {
 	ConfigurationKeySelector IncludeExcludeSelector
 	ConnectorClassSelector   IncludeExcludeSelector
 }
 
-var _ ConfigPatch = (*ConfigPatchRedpandaS3)(nil)
+var _ ConfigPatch = (*ConfigPatchRedpandaGCS)(nil)
 
-// NewConfigPatchRedpandaS3 returns a new Patch for the Redpanda S3 connectors.
-func NewConfigPatchRedpandaS3() *ConfigPatchRedpandaS3 {
-	return &ConfigPatchRedpandaS3{
+// NewConfigPatchRedpandaGCS returns a new Patch for the Redpanda GCS connector.
+func NewConfigPatchRedpandaGCS() *ConfigPatchRedpandaGCS {
+	return &ConfigPatchRedpandaGCS{
 		ConfigurationKeySelector: IncludeExcludeSelector{
 			Include: regexp.MustCompile(`.*`),
 			Exclude: nil,
 		},
 		ConnectorClassSelector: IncludeExcludeSelector{
-			Include: regexp.MustCompile(`com.redpanda.kafka.connect.s3\..*`),
+			Include: regexp.MustCompile(`com.redpanda.kafka.connect.gcs\..*`),
 			Exclude: nil,
 		},
 	}
 }
 
 // IsMatch implements the ConfigPatch.IsMatch interface.
-func (c *ConfigPatchRedpandaS3) IsMatch(configKey, connectorClass string) bool {
+func (c *ConfigPatchRedpandaGCS) IsMatch(configKey, connectorClass string) bool {
 	return c.ConfigurationKeySelector.IsMatch(configKey) && c.ConnectorClassSelector.IsMatch(connectorClass)
 }
 
 // PatchDefinition implements the ConfigPatch.PatchDefinition interface.
 //
 //nolint:cyclop // This function defines/patches a lot of things, but it's easy to comprehend.
-func (*ConfigPatchRedpandaS3) PatchDefinition(d model.ConfigDefinition) model.ConfigDefinition {
+func (*ConfigPatchRedpandaGCS) PatchDefinition(d model.ConfigDefinition) model.ConfigDefinition {
 	// Misc patches
 	switch d.Definition.Name {
 	case "format.output.type":
-		d.SetDisplayName("S3 file format").
-			SetDocumentation("Format of the key coming from the Kafka topic. A valid schema must be available.").
-			SetComponentType(model.ComponentRadioGroup)
+		d.SetDisplayName("GCS file format").
+			SetDocumentation("Format of the key coming from the Kafka topic. A valid schema must be available.")
 	case "file.compression.type":
 		d.SetDisplayName("Output file compression")
 	case "format.output.fields":
@@ -63,14 +62,8 @@ func (*ConfigPatchRedpandaS3) PatchDefinition(d model.ConfigDefinition) model.Co
 		d.SetDisplayName("Envelope for primitives")
 	case "file.max.records":
 		d.SetDisplayName("Max records per file")
-	case "aws.access.key.id":
-		d.SetRequired(true)
-	case "aws.secret.access.key":
-		d.SetRequired(true)
-	case "aws.s3.region":
-		d.SetRecommendedValues(awsRegions)
 	case "name":
-		d.SetValue("s3-connector")
+		d.SetValue("gcs-connector")
 
 	// Below properties will be grouped into "Error Handling"
 	case "errors.retry.timeout":
@@ -79,28 +72,13 @@ func (*ConfigPatchRedpandaS3) PatchDefinition(d model.ConfigDefinition) model.Co
 		d.SetDisplayName("Retry back-off").
 			SetDocumentation("Retry backoff in milliseconds. Useful for performing recovery in case " +
 				"of transient exceptions. Maximum value is 86400000 (24 hours).")
-	case "aws.s3.backoff.max.delay.ms":
-		d.SetDisplayName("S3 maximum back-off")
-	case "aws.s3.backoff.max.retries":
-		d.SetDisplayName("S3 max retries")
-	case "aws.s3.backoff.delay.ms":
-		d.SetDisplayName("S3 retry back-off")
 	}
 
 	// Importance Patches
 	switch d.Definition.Name {
-	case "aws.access.key.id",
-		"aws.secret.access.key",
-		"aws.s3.bucket.name",
-		"aws.s3.region",
-		"format.output.type":
+	case "format.output.type":
 		d.SetImportance(model.ConfigDefinitionImportanceHigh)
-	case "aws.sts.role.arn",
-		"aws.sts.role.session.name",
-		"aws.sts.role.external.id",
-		"aws.sts.role.session.duration",
-		"aws.sts.config.endpoint",
-		"config.action.reload":
+	case "config.action.reload":
 		d.SetImportance(model.ConfigDefinitionImportanceLow)
 	}
 
