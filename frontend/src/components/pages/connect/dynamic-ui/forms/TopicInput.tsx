81377b74a33b8c1bfa3ab28acf43d879786c4ea5
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2023-03-27 15:39:35 +0200

    frontend: update "TopicInput" to select the initial state of the checkbox (whether topics or topics.regex is used) based on the current property values

--- frontend/src/components/pages/connect/dynamic-ui/forms/TopicInput.tsx
+++ frontend/src/components/pages/connect/dynamic-ui/forms/TopicInput.tsx
@@ -4,43 +4,43 @@ import { PropsWithoutRef } from 'react';
 import { Property } from '../../../../../state/connect/state';
 
 export const TopicInput = observer(({ properties }: PropsWithoutRef<{ properties: Property[] }>) => {
-    const $state = useLocalObservable(() => ({
-        properties: new Map(properties.map((p) => [p.name, p])),
-        _selected: 'topics',
-        get property() {
-            return this.properties.get(this._selected)!;
-        },
-        set selected(input: string) {
-            this.property.value = '';
-            this._selected = input;
-        },
-    }));
+    const state = useLocalObservable(() => {
+        const props = new Map(properties.map((p) => [p.name, p]));
+        const topicsRegex = properties.find(x => x.name == 'topics.regex');
+        const initialSelection = topicsRegex?.value ? 'topics.regex' : 'topics';
+        return {
+            properties: props,
+            _selected: initialSelection,
+            get property() {
+                return this.properties.get(this._selected)!;
+            },
+            setSelectedProp(input: string) {
+                this.property.value = '';
+                this._selected = input;
+            },
+        };
+    });
 
-    const showErrors = $state?.property?.errors.length > 0;
+    if (!state.property) return null;
 
-    const errors = showErrors ? $state?.property?.errors : $state?.property?.lastErrors;
-
-    const errorToShow = showErrors ? errors[$state?.property?.currentErrorIndex % errors.length] : undefined;
-
-    const cycleError = showErrors ? () => $state.property.currentErrorIndex++ : undefined;
-
-    if (!$state.property) return null;
+    const showErrors = state.property.errors.length > 0;
+    const errors = showErrors ? state.property.errors : state.property.lastErrors;
+    const errorToShow = showErrors ? errors[state.property.currentErrorIndex % errors.length] : undefined;
+    const cycleError = showErrors ? () => state.property.currentErrorIndex++ : undefined;
 
     return (
         <FormControl>
-            {$state.properties.has('topics.regex') && (
+            {state.properties.has('topics.regex') && (
                 <Checkbox
-                    onChange={(e) => {
-                        $state.selected = e.target.checked ? 'topics.regex' : 'topics';
-                    }}
-                >
+                    isChecked={state._selected == 'topics.regex'}
+                    onChange={(e) => state.setSelectedProp(e.target.checked ? 'topics.regex' : 'topics')}>
                     Use regular expresions
                 </Checkbox>
             )}
 
-            <FormHelperText mb={15}>{$state.property.entry.definition.documentation}</FormHelperText>
+            <FormHelperText mb={15}>{state.property.entry.definition.documentation}</FormHelperText>
 
-            <Input value={String($state.property.value)} onChange={(e) => ($state.property.value = e.target.value)} spellCheck={false} />
+            <Input value={String(state.property.value)} onChange={(e) => (state.property.value = e.target.value)} spellCheck={false} />
 
             {showErrors && <FormErrorMessage onClick={cycleError}>{errorToShow}</FormErrorMessage>}
         </FormControl>
