e58f562f418e61b0d82151b3ae7c3c598cac9743
Martin Bochenek <bochenek.martin@gmail.com>
2023-03-29 15:41:58 +0200

    connectors: http source guide

--- backend/pkg/connector/interceptor/http_source_hook.go
+++ backend/pkg/connector/interceptor/http_source_hook.go
@@ -163,5 +163,165 @@ func KafkaConnectToConsoleHttpSourceHook(response model.ValidationResponse, conf
 		},
 	})
 
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.response.list.pointer",
+			Type:          "STRING",
+			DefaultValue:  "/",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "HTTP response list pointer",
+			Documentation: "JsonPointer to the property in the response body containing an array of records. Example: '/items'",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.response.list.pointer",
+			Value:             "/",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.response.record.offset.pointer",
+			Type:          "STRING",
+			DefaultValue:  "",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "HTTP response record offset pointer",
+			Documentation: "Comma separated list of key=/value pairs where the key is the name of the property in the offset, and the value is the JsonPointer to the value being used as offset for future requests.",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.response.record.offset.pointer",
+			Value:             "",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.timer.interval.millis",
+			Type:          "LONG",
+			DefaultValue:  "60000",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "HTTP timer interval in millis",
+			Documentation: "Interval in between requests when up-to-date (milliseconds)",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.timer.interval.millis",
+			Value:             "60000",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.timer.catchup.interval.millis",
+			Type:          "LONG",
+			DefaultValue:  "60000",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "HTTP timer catchup interval in millis",
+			Documentation: "Interval in between requests when catching up (milliseconds)",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.timer.catchup.interval.millis",
+			Value:             "30000",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.client.connection.timeout.millis",
+			Type:          "LONG",
+			DefaultValue:  "2000",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "HTTP client connection timeout in millis",
+			Documentation: "Timeout for opening a connection (milliseconds)",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.client.connection.timeout.millis",
+			Value:             "2000",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.client.read.timeout.millis",
+			Type:          "LONG",
+			DefaultValue:  "2000",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "Read timeout in millis",
+			Documentation: "Timeout for reading a response (milliseconds)",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.client.read.timeout.millis",
+			Value:             "2000",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.client.connection.ttl.millis",
+			Type:          "LONG",
+			DefaultValue:  "300000",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "Time to live for the connection in millis",
+			Documentation: "",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.client.connection.ttl.millis",
+			Value:             "300000",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
+	response.Configs = append(response.Configs, model.ConfigDefinition{
+		Definition: model.ConfigDefinitionKey{
+			Name:          "http.response.policy.codes.skip",
+			Type:          "STRING",
+			DefaultValue:  "300..399",
+			Importance:    model.ConfigDefinitionImportanceMedium,
+			Required:      false,
+			DisplayName:   "Skip HTTP response codes policy",
+			Documentation: "Comma separated list of code ranges that will result in the parser skipping the response. Example: '300..305, 307..310'",
+			Dependents:    []string{},
+		},
+		Value: model.ConfigDefinitionValue{
+			Name:              "http.response.policy.codes.skip",
+			Value:             "300..399",
+			RecommendedValues: []string{},
+			Visible:           true,
+			Errors:            []string{},
+		},
+	})
+
 	return KafkaConnectToConsoleTopicCreationHook(response, config)
 }
