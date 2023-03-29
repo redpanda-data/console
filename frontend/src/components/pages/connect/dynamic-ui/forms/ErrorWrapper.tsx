c7f566ac74dd99b45ec0e5b40668b62a6cf67403
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2023-03-29 11:56:50 +0200

    frontend: make use of "ExpandableText" and change the input box of the `topics` property to a proper dropdown select box

--- frontend/src/components/pages/connect/dynamic-ui/forms/ErrorWrapper.tsx
+++ frontend/src/components/pages/connect/dynamic-ui/forms/ErrorWrapper.tsx
@@ -2,11 +2,12 @@ import { PropsWithoutRef } from 'react';
 import { FormField } from '@redpanda-data/ui';
 import { observer } from 'mobx-react';
 import { Property } from '../../../../../state/connect/state';
+import { ExpandableText } from '../../../../misc/ExpandableText';
 
 const isRequiredError = (name: string) => `Required configuration "${name}" must be provided`;
 const isEmpty = (property: Property) => property.value == '' || property.value == null;
 
-export const ErrorWrapper = observer(function(props: PropsWithoutRef<{ property: Property; input: JSX.Element }>) {
+export const ErrorWrapper = observer(function (props: PropsWithoutRef<{ property: Property; input: JSX.Element }>) {
     const { property, input } = props;
     const isRequired = property.entry.definition.required;
     const showErrors = property.errors.length > 0;
@@ -24,7 +25,9 @@ export const ErrorWrapper = observer(function(props: PropsWithoutRef<{ property:
                 isRequired={isRequired}
                 label={property.entry.definition.display_name}
                 errorText={isEmpty(property) && isRequired ? errorToShow || isRequiredError(property.name) : errorToShow}
-                description={property.entry.definition.documentation}
+                description={
+                    <ExpandableText maxChars={60}>{property.entry.definition.documentation}</ExpandableText>
+                }
                 onClick={cycleError}
             >
                 {input}
