8065726db9a9be9683567f0fa41c23370c619578
Martin Bochenek <bochenek.martin@gmail.com>
2023-04-03 17:44:36 +0200

    connectors: fixing linter issues

--- backend/pkg/random/string.go
+++ backend/pkg/random/string.go
@@ -7,6 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
+// Package random containing utils for random values generation
 package random
 
 import (
