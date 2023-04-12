743e46c4a33ea2f644783ef32848cb5be2c8120f
Tomasz Sadura <tomasz@redpanda.com>
2023-04-12 16:00:14 +0200

    backend: Add docs for BigQuery connector

--- backend/pkg/connector/patch/bigquery_sink.go
+++ backend/pkg/connector/patch/bigquery_sink.go
@@ -71,7 +71,7 @@ func (*ConfigPatchBigQuery) PatchDefinition(d model.ConfigDefinition, _ string) 
 		d.SetDisplayName("Default dataset")
 	case "autoCreateTables":
 		d.SetDisplayName("Auto create tables").
-			SetDocumentation("Automatically create BigQuery tables if they don't already exist. If a table does not exist your messages need to have schema")
+			SetDocumentation("Automatically create BigQuery tables if they don't already exist. If a table does not exist your messages need to have a schema")
 	case "topic2TableMap":
 		d.SetDisplayName("Topic to table map")
 	case "allowNewBigQueryFields":
