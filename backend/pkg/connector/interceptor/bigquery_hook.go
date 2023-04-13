a85b350a63dd898de8ca22ab43e54aceab25339d
Tomasz Sadura <tomasz@redpanda.com>
2023-04-13 11:59:33 +0200

    backend: Add more comments to BigQuery hooks

--- backend/pkg/connector/interceptor/bigquery_hook.go
+++ backend/pkg/connector/interceptor/bigquery_hook.go
@@ -1,6 +1,8 @@
 package interceptor
 
-// ConsoleToKafkaConnectBigQueryHook adds keySource property explicitly in Console to Kafka Connect request
+// ConsoleToKafkaConnectBigQueryHook sets the keySource property to JSON explicitly, making JSON the only way of
+// providing the Google key in the wizard, and sets bigQueryPartitionDecorator explicitly to false, to allow using
+// various table partitioning types on the BigQuery side
 func ConsoleToKafkaConnectBigQueryHook(config map[string]any) map[string]any {
 	_, exists := config["keySource"]
 	if !exists {
