8b4caf3d23a6848fc0cb1937c8b096c7f05984b3
rikimaru0345 <rikimaru0345@users.noreply.github.com>
2023-04-14 14:15:37 +0200

    frontend: remove unnecessary information from the title of the connector details page and add placeholders for health and config overview

--- frontend/src/components/pages/connect/Connector.Details.tsx
+++ frontend/src/components/pages/connect/Connector.Details.tsx
@@ -16,7 +16,7 @@ import { observer, useLocalObservable } from 'mobx-react';
 import { comparer } from 'mobx';
 import { appGlobal } from '../../../state/appGlobal';
 import { api } from '../../../state/backendApi';
-import { ClusterConnectorInfo } from '../../../state/restInterfaces';
+import { ClusterConnectorInfo, PropertyImportance } from '../../../state/restInterfaces';
 import { uiSettings } from '../../../state/ui';
 import { Code, findPopupContainer } from '../../../utils/tsxUtils';
 import { sortField } from '../../misc/common';
@@ -24,17 +24,13 @@ import { KowlTable } from '../../misc/KowlTable';
 import { PageComponent, PageInitHelper } from '../Page';
 import { ConnectClusterStore } from '../../../state/connect/state';
 import { ConfigPage } from './dynamic-ui/components';
-
 import './helper';
-
-// React Editor
-
-// Monaco Type
-import { ConfirmModal, ConnectorStatisticsCard, NotConfigured, okIcon, TaskState, warnIcon } from './helper';
+import { ConfirmModal, NotConfigured, TaskState } from './helper';
 import PageContent from '../../misc/PageContent';
-import { isEmbedded } from '../../../config';
 import { delay } from '../../../utils/utils';
-import { Box, Flex, Tabs } from '@redpanda-data/ui';
+import { Box, CodeBlock, Flex, Grid, Heading, Tabs, Text } from '@redpanda-data/ui';
+import Section from '../../misc/Section';
+import React from 'react';
 
 export type UpdatingConnectorData = { clusterName: string; connectorName: string };
 export type RestartingTaskData = { clusterName: string; connectorName: string; taskId: number };
@@ -88,20 +84,18 @@ const KafkaConnectorMain = observer(
         return <>
             {/* Title */}
             <Flex flexDirection="row" alignItems="center" gap="1em">
-                <span style={{ fontSize: '26px', display: 'inline-block' }}>
-                    {connectClusterStore.validateConnectorState(connectorName, ['RUNNING']) ? okIcon : warnIcon}
-                </span>
                 <span style={{ fontSize: 'x-large', fontWeight: 600 }}>
                     {connectorName}
-                </span>
-                <span style={{ fontSize: 'medium', opacity: 0.5 }}>
-                    ({connectClusterStore.getConnectorState(connectorName)?.toLowerCase() ?? '<empty>'})
                 </span>
             </Flex>
 
             {/* [Pause] [Restart] [Delete] */}
             <Flex flexDirection="row" alignItems="center" gap="0.5em">
+
+                {/* [View JSON Config] */}
                 <ViewConfigModalButton connector={connector} />
+
+                {/* [Pause/Resume]  [Restart] */}
                 {connectClusterStore.validateConnectorState(connectorName, ['FAILED', 'UNASSIGNED']) ? (
                     <TaskState observable={connector} />
                 ) : (
@@ -130,6 +124,8 @@ const KafkaConnectorMain = observer(
                         </Tooltip>
                     </>
                 )}
+
+                {/* [Delete] */}
                 <Tooltip
                     placement="top"
                     trigger={!canEdit ? 'hover' : 'none'}
@@ -162,7 +158,9 @@ const KafkaConnectorMain = observer(
                         key: 'configuration',
                         name: 'Configuration',
                         component: <Box mt="8">
-                            <ConfigPage connectorStore={connectorStore} />
+                            <Box>
+                                <ConfigPage connectorStore={connectorStore} />
+                            </Box>
 
                             {/* Update Config Button */}
                             <div style={{ marginTop: '1em' }}>
@@ -319,45 +317,87 @@ const ConfigOverviewTab = observer((p: {
     const connectorName = connector.name;
 
     return <>
-        {/* Task List */}
-        <KowlTable
-            key="taskList"
-            dataSource={connectClusterStore.getConnectorTasks(connectorName)}
-            columns={[
-                {
-                    title: 'Task',
-                    dataIndex: 'taskId',
-                    width: 200,
-                    sorter: sortField('taskId'),
-                    defaultSortOrder: 'ascend',
-                    render: (v) => <Code nowrap>Task-{v}</Code>,
-                },
-                {
-                    title: 'Status',
-                    dataIndex: 'state',
-                    sorter: sortField('state'),
-                    render: (_, r) => <TaskState observable={r} />,
-                    filterType: { type: 'enum', optionClassName: 'capitalize', toDisplay: (x) => String(x).toLowerCase() },
-                },
-                {
-                    title: 'Worker',
-                    dataIndex: 'workerId',
-                    sorter: sortField('workerId'),
-                    render: (_, r) => <Code nowrap>{r.workerId}</Code>,
-                    filterType: { type: 'enum' },
-                }
-            ]}
-            rowKey="taskId"
-            search={{
-                searchColumnIndex: 0,
-                isRowMatch: (row, regex) =>
-                    regex.test(String(row.taskId)) || regex.test(row.state) || regex.test(row.workerId),
-            }}
-            observableSettings={uiSettings.kafkaConnect.connectorDetails}
-            pagination={{
-                defaultPageSize: 10,
-            }}
-        />
+        <Grid
+            templateAreas={`"health details"
+                        "tasks details"`}
+            gridTemplateRows="auto"
+            alignItems="start"
+            gap="6"
+        >
+            <Section gridArea="health">
+                <Flex flexDirection="row" gap="4" m="1">
+                    <Box width="5px" borderRadius="3px" background="green" />
+
+                    <Flex flexDirection="column">
+                        <Text fontWeight="semibold" fontSize="3xl">Running</Text>
+                        <Text opacity=".5">Status</Text>
+                    </Flex>
+                </Flex>
+            </Section>
+
+            <Section py={4} gridArea="tasks">
+                <Flex alignItems="center" mt="2" mb="6" gap="2">
+                    <Heading as="h3" fontSize="1rem" fontWeight="semibold" textTransform="uppercase" color="blackAlpha.800">
+                        Tasks
+                    </Heading>
+                    <Text opacity=".5" fontWeight="normal">({connectClusterStore.getConnectorTasks(connectorName)?.length || 0})</Text>
+                </Flex>
+                <KowlTable
+                    key="taskList"
+                    dataSource={connectClusterStore.getConnectorTasks(connectorName)}
+                    columns={[
+                        {
+                            title: 'Task',
+                            dataIndex: 'taskId',
+                            width: 200,
+                            sorter: sortField('taskId'),
+                            defaultSortOrder: 'ascend',
+                            render: (v) => <Code nowrap>Task-{v}</Code>,
+                        },
+                        {
+                            title: 'Status',
+                            dataIndex: 'state',
+                            sorter: sortField('state'),
+                            render: (_, r) => <TaskState observable={r} />,
+                            filterType: { type: 'enum', optionClassName: 'capitalize', toDisplay: (x) => String(x).toLowerCase() },
+                        },
+                        {
+                            title: 'Worker',
+                            dataIndex: 'workerId',
+                            sorter: sortField('workerId'),
+                            render: (_, r) => <Code nowrap>{r.workerId}</Code>,
+                            filterType: { type: 'enum' },
+                        }
+                    ]}
+                    rowKey="taskId"
+                    search={{
+                        searchColumnIndex: 0,
+                        isRowMatch: (row, regex) =>
+                            regex.test(String(row.taskId)) || regex.test(row.state) || regex.test(row.workerId),
+                    }}
+                    observableSettings={uiSettings.kafkaConnect.connectorDetails}
+                    pagination={{
+                        defaultPageSize: 10,
+                    }}
+                />
+            </Section>
+
+            <Section py={4} gridArea="details">
+                <Heading as="h3" mb="6" mt="2" fontSize="1rem" fontWeight="semibold" textTransform="uppercase" color="blackAlpha.800">
+                    Connector Details
+                </Heading>
+
+                <ConnectorDetails config={[
+                    { name: 'Type', value: 'Name of type', importance: PropertyImportance.High, isSensitive: false },
+                    { name: 'Topics', value: 'a,b,c,d,e', importance: PropertyImportance.High, isSensitive: false },
+                    { name: 'Bucket name', value: 'example!', importance: PropertyImportance.High, isSensitive: false },
+                    { name: 'Bucket region', value: 'us-east-1', importance: PropertyImportance.High, isSensitive: false },
+                    { name: 'Key format', value: 'AVRO', importance: PropertyImportance.High, isSensitive: false },
+                    { name: 'Max tasks', value: '5', importance: PropertyImportance.High, isSensitive: false },
+                ]} />
+            </Section>
+        </Grid>
+
     </>
 });
 
@@ -386,9 +426,6 @@ class KafkaConnectorDetails extends PageComponent<{ clusterName: string; connect
 
         return (
             <PageContent>
-                {isEmbedded() ? <></> : <ConnectorStatisticsCard clusterName={clusterName} connectorName={connectorName} />}
-
-                {/* Main Card */}
                 <KafkaConnectorMain clusterName={clusterName} connectorName={connectorName} refreshData={this.refreshData} />
             </PageContent>
         );
@@ -411,18 +448,53 @@ const ViewConfigModalButton = (p: { connector: ClusterConnectorInfo }) => {
             <Flex alignItems="center" mb="8px">
                 <Box fontSize="medium" fontWeight={500}>Connector Config (JSON)</Box>
 
-                <Button style={{ marginLeft: '16px', paddingInline: '8px' }} onClick={() => {
+                <Button style={{ marginLeft: '16px', paddingInline: '24px' }} onClick={() => {
                     navigator.clipboard.writeText(p.connector.jsonConfig);
                 }}>Copy</Button>
             </Flex>
-            <div className="codeBox" style={{ whiteSpace: 'pre', overflow: 'scroll', width: '100%', padding: '10px 8px' }}>
-                {p.connector.jsonConfig}
-            </div>
+
+            <CodeBlock codeString={p.connector.jsonConfig} language="json" showCopyButton />
         </>
     </Modal>;
 
     return <>
-        <Button onClick={() => setShowConfig(true)}>Show Config</Button>
+        <Button onClick={() => setShowConfig(true)}>View Json Config</Button>
         {viewConfigModal}
     </>
 };
+
+interface PlaceholderConfigEntry {
+    name: string;
+    value: string;
+    importance: PropertyImportance;
+    isSensitive: boolean;
+}
+
+function ConnectorDetails(p: { config: PlaceholderConfigEntry[] }) {
+    const { config } = p;
+
+    const items = config
+        .filter(x => !x.isSensitive && x.importance == PropertyImportance.High)
+        .orderBy(x => {
+            const name = x.name;
+            switch (name) {
+                case 'type':
+                    return -2;
+                case 'topics':
+                case 'topics.regex':
+                    return -1;
+
+                default:
+                    return 100;
+            }
+        });
+
+    return <Grid templateColumns="auto 1fr" rowGap="3" columnGap="10">
+        {items.map(x =>
+            <React.Fragment key={x.name}>
+                <Text fontWeight="semibold" whiteSpace="nowrap">{x.name}</Text>
+                <Text>{x.value}</Text>
+            </React.Fragment>
+        )}
+    </Grid>
+}
