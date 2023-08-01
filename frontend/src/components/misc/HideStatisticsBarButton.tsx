061541d62dd04db932c18347ae4c0c2c6a039bc8
Frank Manno <frank@redpanda.com>
2023-08-01 15:40:10 -0400

    ui: Updated Tooltip component to use the one from Redpanda UI Library

--- frontend/src/components/misc/HideStatisticsBarButton.tsx
+++ frontend/src/components/misc/HideStatisticsBarButton.tsx
@@ -9,33 +9,27 @@
  * by the Apache License, Version 2.0
  */
 
-import { Component } from 'react'
-import React from 'react'
-import { Tooltip, message } from 'antd'
-import { findPopupContainer } from '../../utils/tsxUtils'
-import { EyeClosedIcon } from '@primer/octicons-react'
-
-
-
+import { Component } from 'react';
+import React from 'react';
+import { message } from 'antd';
+import { findPopupContainer } from '../../utils/tsxUtils';
+import { EyeClosedIcon } from '@primer/octicons-react';
+import { Tooltip } from '@redpanda-data/ui';
 export class HideStatisticsBarButton extends Component<{ onClick: () => void }> {
-
     handleClick = () => {
         this.props.onClick();
         message.info('Statistics bar hidden! You can enable it again in the preferences.', 8);
-    }
+    };
 
     render() {
-        return <Tooltip
-            title={<span style={{ whiteSpace: 'nowrap' }}>Hide statistics bar</span>}
-            getPopupContainer={findPopupContainer}
-            arrowPointAtCenter={true}
-            placement="right"
-        >
-            <div className="hideStatsBarButton" onClick={this.handleClick}>
-                <div style={{ display: 'flex', width: '100%' }}>
-                    <EyeClosedIcon size="medium" />
+        return (
+            <Tooltip label={<span style={{ whiteSpace: 'nowrap' }}>Hide statistics bar</span>} placement="right" hasArrow>
+                <div className="hideStatsBarButton" onClick={this.handleClick}>
+                    <div style={{ display: 'flex', width: '100%' }}>
+                        <EyeClosedIcon size="medium" />
+                    </div>
                 </div>
-            </div>
-        </Tooltip>
+            </Tooltip>
+        );
     }
 }
