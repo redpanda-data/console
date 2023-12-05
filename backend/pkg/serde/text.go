2fb88164c6ca6459e56aa77f5a5c3eec75f76357
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2023-12-05 22:25:05 +0000

    backend: return empty byte array for empty strings

--- backend/pkg/serde/text.go
+++ backend/pkg/serde/text.go
@@ -84,5 +84,11 @@ func (TextSerde) SerializeObject(_ context.Context, obj any, _ PayloadType, opts
 		return nil, fmt.Errorf("payload contains UTF8 control characters therefore not plain text format")
 	}
 
+	// If message encoding text is used and the byte array is empty, the user
+	// probably wants to write an empty string, rather than null.
+	if byteData == nil {
+		byteData = []byte("")
+	}
+
 	return byteData, nil
 }
