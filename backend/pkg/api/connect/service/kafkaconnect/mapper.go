a83974e023e1f2625366a8a2ea82c1fa29009a0e
Santiago Jimenez Giraldo <santiago@redpanda.com>
2024-01-03 15:26:07 +0100

    feat(backend): implement connect handlers for the kafkaconnect
    Implement put and get config endpoint for the kafkaconnect api in the
    grpc implementation
    
    Signed-off-by: Santiago Jimenez Giraldo <santiago@redpanda.com>

--- backend/pkg/api/connect/service/kafkaconnect/mapper.go
+++ backend/pkg/api/connect/service/kafkaconnect/mapper.go
@@ -212,6 +212,23 @@ func (m mapper) connectorInfoListToProto(connectorInfoList []kafkaconnect.Cluste
 	return clusters, errs
 }
 
+// ConnectorSpecToProto converts the http response to proto message
+func (mapper) ConnectorSpecToProto(connector con.ConnectorInfo) *dataplanev1alpha1.ConnectorSpec {
+	tasks := make([]*dataplanev1alpha1.TaskInfo, len(connector.Tasks))
+
+	for i, task := range connector.Tasks {
+		tasks[i] = &dataplanev1alpha1.TaskInfo{
+			Connector: task.Connector,
+			Task:      int32(task.Task),
+		}
+	}
+	return &dataplanev1alpha1.ConnectorSpec{
+		Name:   connector.Name,
+		Config: connector.Config,
+		Tasks:  tasks,
+	}
+}
+
 // convertStringMapToInterfaceMap converts interface map to string map
 func convertStringMapToInterfaceMap(stringMap map[string]string) map[string]any {
 	interfaceMap := make(map[string]any, len(stringMap))
