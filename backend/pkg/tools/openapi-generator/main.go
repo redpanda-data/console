e5d8a6d8c9925aaf5337a70aa8999f46313cca0d
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2024-01-05 13:36:58 +0100

    backend: update openapi client calls

--- backend/pkg/tools/openapi-generator/main.go
+++ backend/pkg/tools/openapi-generator/main.go
@@ -382,7 +382,7 @@ func updateUsers(doc3 *openapi3.T) {
 		}
 		response := &v1alpha1.ListUsersResponse{Users: users}
 		responseExample := toExample(response, "List Users", "List users", true)
-		doc3.Paths["/v1alpha1/users"].Get.Responses.Get(http.StatusOK).Value.Content.Get("application/json").Example = responseExample.Value
+		doc3.Paths.Value("/v1alpha1/users").Get.Responses.Status(http.StatusOK).Value.Content.Get("application/json").Example = responseExample.Value
 	}
 
 	// POST /users
@@ -393,7 +393,7 @@ func updateUsers(doc3 *openapi3.T) {
 			Password:  "secure-password",
 			Mechanism: v1alpha1.SASLMechanism_SASL_MECHANISM_SCRAM_SHA_256,
 		}}
-		doc3.Paths["/v1alpha1/users"].Post.RequestBody.Value.Content["application/json"].Example = toExample(createUserReq, "Create User", "Create user", false).Value
+		doc3.Paths.Value("/v1alpha1/users").Post.RequestBody.Value.Content["application/json"].Example = toExample(createUserReq, "Create User", "Create user", false).Value
 
 		// Responses
 		response := &v1alpha1.CreateUserResponse{User: &v1alpha1.CreateUserResponse_User{
@@ -401,7 +401,7 @@ func updateUsers(doc3 *openapi3.T) {
 			Mechanism: v1alpha1.SASLMechanism_SASL_MECHANISM_SCRAM_SHA_256.Enum(),
 		}}
 		responseExample := toExample(response, "Create User", "Create user", true)
-		doc3.Paths["/v1alpha1/users"].Post.Responses.Get(http.StatusCreated).Value.Content.Get("application/json").Example = responseExample.Value
+		doc3.Paths.Value("/v1alpha1/users").Post.Responses.Status(http.StatusCreated).Value.Content.Get("application/json").Example = responseExample.Value
 
 		badRequestExample := toExample(
 			newBadRequestError(
@@ -417,7 +417,7 @@ func updateUsers(doc3 *openapi3.T) {
 			"Bad Request",
 			"Bad Request",
 			true)
-		doc3.Paths["/v1alpha1/users"].Post.Responses.Get(http.StatusBadRequest).Value.Content.Get("application/json").Example = badRequestExample.Value
+		doc3.Paths.Value("/v1alpha1/users").Post.Responses.Status(http.StatusBadRequest).Value.Content.Get("application/json").Example = badRequestExample.Value
 	}
 
 	// PUT /users/{user.name}
@@ -430,7 +430,7 @@ func updateUsers(doc3 *openapi3.T) {
 				Mechanism: v1alpha1.SASLMechanism_SASL_MECHANISM_SCRAM_SHA_256,
 			},
 		}
-		doc3.Paths["/v1alpha1/users/{user.name}"].Put.RequestBody.Value.Content["application/json"].Example = toExample(updateUserReq, "Update User", "Update user", false).Value
+		doc3.Paths.Value("/v1alpha1/users/{user.name}").Put.RequestBody.Value.Content["application/json"].Example = toExample(updateUserReq, "Update User", "Update user", false).Value
 
 		// Responses
 		response := &v1alpha1.UpdateUserResponse{
@@ -440,7 +440,7 @@ func updateUsers(doc3 *openapi3.T) {
 			},
 		}
 		responseExample := toExample(response, "Update User", "Update user", true)
-		doc3.Paths["/v1alpha1/users/{user.name}"].Put.Responses.Get(http.StatusOK).Value.Content.Get("application/json").Example = responseExample.Value
+		doc3.Paths.Value("/v1alpha1/users/{user.name}").Put.Responses.Status(http.StatusOK).Value.Content.Get("application/json").Example = responseExample.Value
 
 		badRequestExample := toExample(
 			newBadRequestError(
@@ -456,7 +456,7 @@ func updateUsers(doc3 *openapi3.T) {
 			"Bad Request",
 			"Bad Request",
 			true)
-		doc3.Paths["/v1alpha1/users/{user.name}"].Put.Responses.Get(http.StatusBadRequest).Value.Content.Get("application/json").Example = badRequestExample.Value
+		doc3.Paths.Value("/v1alpha1/users/{user.name}").Put.Responses.Status(http.StatusBadRequest).Value.Content.Get("application/json").Example = badRequestExample.Value
 	}
 
 	// DELETE /users/{name}
@@ -464,7 +464,7 @@ func updateUsers(doc3 *openapi3.T) {
 		// Response
 		response := &v1alpha1.DeleteUserResponse{}
 		responseExample := toExample(response, "Delete User", "Delete user", true)
-		doc3.Paths["/v1alpha1/users/{name}"].Delete.Responses.Get(http.StatusNoContent).Value.Content.Get("application/json").Example = responseExample.Value
+		doc3.Paths.Value("/v1alpha1/users/{name}").Delete.Responses.Status(http.StatusNoContent).Value.Content.Get("application/json").Example = responseExample.Value
 
 		notFoundExample := toExample(
 			connectErrorToErrorStatus(
@@ -477,7 +477,7 @@ func updateUsers(doc3 *openapi3.T) {
 			"Bad Request",
 			"Bad Request",
 			true)
-		doc3.Paths["/v1alpha1/users/{name}"].Delete.Responses.Get(http.StatusNotFound).Value.Content.Get("application/json").Example = notFoundExample.Value
+		doc3.Paths.Value("/v1alpha1/users/{name}").Delete.Responses.Status(http.StatusNotFound).Value.Content.Get("application/json").Example = notFoundExample.Value
 	}
 }
 
