f4ba06bf4cddf600af0858801776d4168ed6e47d
Jan Vorcak <jan.vorcak@redpanda.com>
2024-01-25 13:41:03 +0100

    Improved formatting

--- frontend/src/utils/pagination.ts
+++ frontend/src/utils/pagination.ts
@@ -1,4 +1,4 @@
-import { PaginationState, Updater } from "@redpanda-data/ui";
+import { PaginationState, Updater } from '@redpanda-data/ui';
 
 export const onPaginationChange = (state: PaginationState, callBack?: (args: { pageSize: number; pageIndex: number }) => void) => (x: Updater<PaginationState>) => {
     const newState = typeof x === 'function' ? x(state) : x
