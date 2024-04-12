27eb7e2015952080ac69c8aeda8402da2f0283e9
Bojan Djurkovic <bojan@redpanda.com>
2024-04-11 23:50:01 -0300

    backend: add role authz hooks

--- backend/pkg/api/hooks/hooks.go
+++ backend/pkg/api/hooks/hooks.go
@@ -130,6 +130,11 @@ type AuthorizationHooks interface {
 	CanCreateSchemas(ctx context.Context) (bool, *rest.Error)
 	CanDeleteSchemas(ctx context.Context) (bool, *rest.Error)
 	CanManageSchemaRegistry(ctx context.Context) (bool, *rest.Error)
+
+	// Kafka Role Hooks
+	CanListKafkaRoles(ctx context.Context) (bool, *rest.Error)
+	CanCreateKafkaRoles(ctx context.Context) (bool, *rest.Error)
+	CanDeleteKafkaRoles(ctx context.Context) (bool, *rest.Error)
 }
 
 // ConsoleHooks are hooks for providing additional context to the Frontend where needed.
