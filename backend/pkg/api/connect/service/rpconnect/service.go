e39a132522aba2990dea559eee6ad17a600919f5
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2024-05-22 18:37:23 +0100

    backend: add auth check for linting endpoint

--- backend/pkg/api/connect/service/rpconnect/service.go
+++ backend/pkg/api/connect/service/rpconnect/service.go
@@ -19,6 +19,7 @@ import (
 	"go.uber.org/zap"
 
 	apierrors "github.com/redpanda-data/console/backend/pkg/api/connect/errors"
+	"github.com/redpanda-data/console/backend/pkg/api/hooks"
 	v1alpha1 "github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/console/v1alpha1"
 	"github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/console/v1alpha1/consolev1alpha1connect"
 	"github.com/redpanda-data/console/backend/pkg/rpconnect"
@@ -30,11 +31,12 @@ var _ consolev1alpha1connect.RedpandaConnectServiceHandler = (*Service)(nil)
 type Service struct {
 	logger *zap.Logger
 	linter *rpconnect.Linter
+	hooks  hooks.AuthorizationHooks
 	mapper *mapper
 }
 
 // NewService initializes a new RedpandaConnect service.
-func NewService(logger *zap.Logger) (*Service, error) {
+func NewService(logger *zap.Logger, authHooks hooks.AuthorizationHooks) (*Service, error) {
 	linter, err := rpconnect.NewLinter()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create linter: %w", err)
@@ -43,12 +45,23 @@ func NewService(logger *zap.Logger) (*Service, error) {
 	return &Service{
 		logger: logger,
 		linter: linter,
+		hooks:  authHooks,
 		mapper: &mapper{},
 	}, nil
 }
 
 // LintConfig lints a redpanda connect config.
-func (s *Service) LintConfig(_ context.Context, req *connect.Request[v1alpha1.LintConfigRequest]) (*connect.Response[v1alpha1.LintConfigResponse], error) {
+func (s *Service) LintConfig(ctx context.Context, req *connect.Request[v1alpha1.LintConfigRequest]) (*connect.Response[v1alpha1.LintConfigResponse], error) {
+	// This is a dummy check, we only want to ensure that the requester is authenticated.
+	canView, restErr := s.hooks.CanViewConnectCluster(ctx, "redpanda")
+	if !canView || restErr != nil {
+		return nil, apierrors.NewConnectError(
+			connect.CodePermissionDenied,
+			fmt.Errorf("you are not allowed to call this endpoint"),
+			apierrors.NewErrorInfo(commonv1alpha1.Reason_REASON_PERMISSION_DENIED.String()),
+		)
+	}
+
 	yamlCfg := []byte(req.Msg.YamlConfig)
 	lints, err := s.linter.LintYAMLConfig(yamlCfg)
 	if err != nil {
