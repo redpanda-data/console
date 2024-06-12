0ea05ffb8189438011e50463ccf5264e9e3d53f5
Jan Vorcak <jan.vorcak@redpanda.com>
2024-06-12 09:29:41 +0200

    Information about estimation of the message count

--- frontend/src/components/misc/Statistic.tsx
+++ frontend/src/components/misc/Statistic.tsx
@@ -1,19 +1,30 @@
-
-import { SpaceProps, Stat, StatLabel, StatNumber } from '@redpanda-data/ui';
+import { Box, Flex, SpaceProps, Stat, StatLabel, StatNumber, Tooltip } from '@redpanda-data/ui';
+import { MdInfoOutline } from 'react-icons/md';
 
 export function Statistic(p: {
     key?: string | number,
     title: React.ReactNode,
     value: React.ReactNode,
-
+    hint?: string,
     className?: string,
 } & SpaceProps) {
 
-    const { key, title, value, className, ...rest } = p;
+    const { key, title, value, className, hint, ...rest } = p;
 
-    return <Stat key={key} className={className} flexBasis="auto" flexGrow={0} marginRight="2rem" {...rest}>
-        <StatNumber>{value}</StatNumber>
-        <StatLabel>{title}</StatLabel>
-    </Stat>
+    return (
+        <Stat key={key} className={className} flexBasis="auto" flexGrow={0} marginRight="2rem" {...rest}>
+            <Flex gap={1}>
+                <StatNumber>{value}</StatNumber>
+                {hint && (
+                    <Tooltip label={hint} hasArrow placement="right">
+                        <Box alignSelf="start">
+                            <MdInfoOutline />
+                        </Box>
+                    </Tooltip>
+                )}
+            </Flex>
+            <StatLabel>{title}</StatLabel>
+        </Stat>
+    );
 
 }
