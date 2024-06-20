5091e96de5b4c6736187e909e2fc379895d9eb61
Jan Vorcak <jan.vorcak@redpanda.com>
2024-06-20 08:29:46 +0200

    Allow parallel run in CI

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false, // TODO enable this in the future
+  fullyParallel: !!process.env.CI,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
