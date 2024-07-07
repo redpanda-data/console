4720621211866ee3243f48e9bd0101bb9ab762d7
Bojan Djurkovic <bojan@redpanda.com>
2024-07-06 22:57:51 -0300

    backend: add v1alpha2 topic service proxy

--- backend/pkg/api/connect/service/topic/v1alpha1/mapper.go
+++ backend/pkg/api/connect/service/topic/v1alpha1/mapper.go
@@ -10,212 +10,202 @@
 package topic
 
 import (
-	"fmt"
-
-	"github.com/twmb/franz-go/pkg/kmsg"
-
-	common "github.com/redpanda-data/console/backend/pkg/api/connect/service/common/v1alpha1"
 	v1alpha1 "github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/dataplane/v1alpha1"
+	v1alpha2 "github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/dataplane/v1alpha2"
 )
 
-type kafkaClientMapper struct {
-	commonKafkaClientMapper common.KafkaClientMapper
-}
+type kafkaClientMapper struct{}
 
-// createTopicRequestToKafka maps the proto request to create a topic into a kmsg.CreateTopicsRequestTopic.
-func (k *kafkaClientMapper) createTopicRequestToKafka(req *v1alpha1.CreateTopicRequest) *kmsg.CreateTopicsRequest {
-	kafkaReq := kmsg.NewCreateTopicsRequest()
-	kafkaReq.ValidateOnly = req.ValidateOnly
-	kafkaReq.Topics = []kmsg.CreateTopicsRequestTopic{k.createTopicRequestTopicToKafka(req.Topic)}
-	return &kafkaReq
-}
-
-// createTopicRequestToKafka maps the proto message for creating a topic to kmsg.CreateTopicsRequestTopic.
-func (*kafkaClientMapper) createTopicRequestTopicToKafka(topicReq *v1alpha1.CreateTopicRequest_Topic) kmsg.CreateTopicsRequestTopic {
-	partitionCount := int32(-1)
-	if topicReq.PartitionCount != nil {
-		partitionCount = *topicReq.PartitionCount
-	}
-
-	replicationFactor := int16(-1)
-	if topicReq.ReplicationFactor != nil {
-		replicationFactor = int16(*topicReq.ReplicationFactor)
-	}
-
-	configs := make([]kmsg.CreateTopicsRequestTopicConfig, len(topicReq.Configs))
-	for i, cfg := range topicReq.Configs {
-		c := kmsg.NewCreateTopicsRequestTopicConfig()
-		c.Name = cfg.Name
-		c.Value = cfg.Value
-		configs[i] = c
-	}
-
-	replicaAssignments := make([]kmsg.CreateTopicsRequestTopicReplicaAssignment, len(topicReq.ReplicaAssignments))
-	for i, assignment := range topicReq.ReplicaAssignments {
-		a := kmsg.NewCreateTopicsRequestTopicReplicaAssignment()
-		a.Partition = assignment.PartitionId
-		a.Replicas = assignment.ReplicaIds
-		replicaAssignments[i] = a
-	}
-
-	req := kmsg.NewCreateTopicsRequestTopic()
-	req.Topic = topicReq.Name
-	req.NumPartitions = partitionCount
-	req.ReplicationFactor = replicationFactor
-	req.ReplicaAssignment = replicaAssignments
-	req.Configs = configs
-
-	return req
-}
-
-func (*kafkaClientMapper) createTopicResponseTopicToProto(topic kmsg.CreateTopicsResponseTopic) *v1alpha1.CreateTopicResponse {
-	return &v1alpha1.CreateTopicResponse{
-		Name:              topic.Topic,
-		PartitionCount:    topic.NumPartitions,
-		ReplicationFactor: int32(topic.ReplicationFactor),
-	}
-}
-
-func (*kafkaClientMapper) describeTopicConfigsToKafka(req *v1alpha1.GetTopicConfigurationsRequest) kmsg.DescribeConfigsRequest {
-	configResource := kmsg.NewDescribeConfigsRequestResource()
-	configResource.ResourceType = kmsg.ConfigResourceTypeTopic
-	configResource.ResourceName = req.TopicName
-	configResource.ConfigNames = nil // Requests all configs
-
-	kafkaReq := kmsg.NewDescribeConfigsRequest()
-	kafkaReq.IncludeDocumentation = true
-	kafkaReq.IncludeSynonyms = true
-	kafkaReq.Resources = []kmsg.DescribeConfigsRequestResource{configResource}
-
-	return kafkaReq
-}
-
-func (k *kafkaClientMapper) describeTopicConfigsToProto(resources []kmsg.DescribeConfigsResponseResourceConfig) ([]*v1alpha1.Topic_Configuration, error) {
-	mappedResources := make([]*v1alpha1.Topic_Configuration, len(resources))
-	for i, resource := range resources {
-		configType, err := k.commonKafkaClientMapper.ConfigTypeToProto(resource.ConfigType)
-		if err != nil {
-			return nil, fmt.Errorf("failed to map config type for resource %q: %w", resource.Name, err)
-		}
-
-		configSource, err := k.commonKafkaClientMapper.ConfigSourceToProto(resource.Source)
-		if err != nil {
-			return nil, fmt.Errorf("failed to map config source for resource %q: %w", resource.Name, err)
-		}
-
-		synonyms := make([]*v1alpha1.ConfigSynonym, len(resource.ConfigSynonyms))
-		for i, synonym := range resource.ConfigSynonyms {
-			mappedSynonym, err := k.commonKafkaClientMapper.ConfigSynonymToProto(synonym)
-			if err != nil {
-				return nil, fmt.Errorf("failed to map config synonym for resource %q: %w", resource.Name, err)
-			}
-			synonyms[i] = mappedSynonym
-		}
-
-		mappedResources[i] = &v1alpha1.Topic_Configuration{
-			Name:           resource.Name,
-			Type:           configType,
-			Value:          resource.Value,
-			Source:         configSource,
-			ReadOnly:       resource.ReadOnly,
-			Sensitive:      resource.IsSensitive,
-			ConfigSynonyms: synonyms,
-			Documentation:  resource.Documentation,
-		}
-	}
-
-	return mappedResources, nil
-}
-
-func (*kafkaClientMapper) deleteTopicToKmsg(req *v1alpha1.DeleteTopicRequest) kmsg.DeleteTopicsRequest {
-	kafkaReq := kmsg.NewDeleteTopicsRequest()
-	kafkaReq.TopicNames = []string{req.Name}
-	kafkaReq.Topics = []kmsg.DeleteTopicsRequestTopic{
-		{
-			Topic: kmsg.StringPtr(req.Name),
+func (*kafkaClientMapper) v1alpha1ToListTopicsv1alpha2(r *v1alpha1.ListTopicsRequest) *v1alpha2.ListTopicsRequest {
+	return &v1alpha2.ListTopicsRequest{
+		Filter: &v1alpha2.ListTopicsRequest_Filter{
+			NameContains: r.GetFilter().GetNameContains(),
 		},
+		PageSize:  r.GetPageSize(),
+		PageToken: r.GetPageToken(),
 	}
-
-	return kafkaReq
 }
 
-func (k *kafkaClientMapper) updateTopicConfigsToKafka(req *v1alpha1.UpdateTopicConfigurationsRequest) (*kmsg.IncrementalAlterConfigsRequest, error) {
-	// We only have one resource (a single topic) whose configs we want to update incrementally
-	// The API allows to add many, independent resources of different types. Because we always only
-	// want to patch configs for a single Kafka topic, we can simplify the mapping here by hardcoding
-	// a couple properties of the request.
-	alterTopicResource := kmsg.NewIncrementalAlterConfigsRequestResource()
-	alterTopicResource.ResourceType = kmsg.ConfigResourceTypeTopic
-	alterTopicResource.ResourceName = req.TopicName
+func (*kafkaClientMapper) v1alpha2ListTopicsResponseTov1alpha1(topics []*v1alpha2.ListTopicsResponse_Topic) []*v1alpha1.ListTopicsResponse_Topic {
+	out := make([]*v1alpha1.ListTopicsResponse_Topic, 0, len(topics))
 
-	topicConfigUpdates := make([]kmsg.IncrementalAlterConfigsRequestResourceConfig, len(req.Configurations))
-	for i, requestedConfigUpdate := range req.Configurations {
-		topicConfigUpdate := kmsg.NewIncrementalAlterConfigsRequestResourceConfig()
-		topicConfigUpdate.Name = requestedConfigUpdate.Name
-		topicConfigUpdate.Value = requestedConfigUpdate.Value
-		kafkaOp, err := k.commonKafkaClientMapper.AlterConfigOperationToKafka(requestedConfigUpdate.Operation)
-		if err != nil {
-			return nil, err
+	for _, topic := range topics {
+		out = append(out, &v1alpha1.ListTopicsResponse_Topic{
+			Name:              topic.GetName(),
+			Internal:          topic.GetInternal(),
+			PartitionCount:    topic.GetPartitionCount(),
+			ReplicationFactor: topic.GetReplicationFactor(),
+		})
+	}
+	return out
+}
+
+func (*kafkaClientMapper) v1alpha1ToDeleteTopicv1alpha2(r *v1alpha1.DeleteTopicRequest) *v1alpha2.DeleteTopicRequest {
+	return &v1alpha2.DeleteTopicRequest{
+		Name: r.GetName(),
+	}
+}
+
+func (*kafkaClientMapper) v1alpha1GetTopicConfigurationv1alpha2(r *v1alpha1.GetTopicConfigurationsRequest) *v1alpha2.GetTopicConfigurationsRequest {
+	return &v1alpha2.GetTopicConfigurationsRequest{
+		TopicName: r.GetTopicName(),
+	}
+}
+
+func (*kafkaClientMapper) v1alpha2TopicConfigsv1alpha1(configs []*v1alpha2.Topic_Configuration) []*v1alpha1.Topic_Configuration {
+	out := make([]*v1alpha1.Topic_Configuration, 0, len(configs))
+
+	for _, c := range configs {
+		synonyms := make([]*v1alpha1.ConfigSynonym, 0, len(c.GetConfigSynonyms()))
+
+		for _, s := range c.GetConfigSynonyms() {
+			ns := &v1alpha1.ConfigSynonym{
+				Name:   s.GetName(),
+				Value:  s.Value,
+				Source: mapv1alpha2ConfigSourcev1alpha1(s.GetSource()),
+			}
+			synonyms = append(synonyms, ns)
 		}
-		topicConfigUpdate.Op = kafkaOp
 
-		topicConfigUpdates[i] = topicConfigUpdate
+		out = append(out, &v1alpha1.Topic_Configuration{
+			Name:           c.GetName(),
+			Type:           mapv1alpha2ConfigTypev1alpha1(c.GetType()),
+			Value:          c.Value,
+			Source:         mapv1alpha2ConfigSourcev1alpha1(c.GetSource()),
+			ReadOnly:       c.GetReadOnly(),
+			Sensitive:      c.GetSensitive(),
+			ConfigSynonyms: synonyms,
+			Documentation:  c.Documentation,
+		})
 	}
-	alterTopicResource.Configs = topicConfigUpdates
 
-	// Construct kafka request for altering configs
-	kafkaReq := kmsg.NewIncrementalAlterConfigsRequest()
-	kafkaReq.Resources = []kmsg.IncrementalAlterConfigsRequestResource{alterTopicResource}
-
-	return &kafkaReq, nil
+	return out
 }
 
-func (k *kafkaClientMapper) kafkaMetadataToProto(metadata *kmsg.MetadataResponse) []*v1alpha1.ListTopicsResponse_Topic {
-	topics := make([]*v1alpha1.ListTopicsResponse_Topic, len(metadata.Topics))
-	for i, topicMetadata := range metadata.Topics {
-		topics[i] = k.kafkaTopicMetadataToProto(topicMetadata)
+func (*kafkaClientMapper) v1alpha1UpdateTopicConfigurationv1alpha2(r *v1alpha1.UpdateTopicConfigurationsRequest) *v1alpha2.UpdateTopicConfigurationsRequest {
+	configs := make([]*v1alpha2.UpdateTopicConfigurationsRequest_UpdateConfiguration, 0, len(r.GetConfigurations()))
+	for _, c := range r.GetConfigurations() {
+		configs = append(configs, &v1alpha2.UpdateTopicConfigurationsRequest_UpdateConfiguration{
+			Name:      c.GetName(),
+			Value:     c.Value,
+			Operation: mapV1alpha1ConfigAlterOperationToV1alpha2(c.GetOperation()),
+		})
 	}
-
-	return topics
-}
-
-func (*kafkaClientMapper) kafkaTopicMetadataToProto(topicMetadata kmsg.MetadataResponseTopic) *v1alpha1.ListTopicsResponse_Topic {
-	// We iterate through all partitions to figure out the replication factor,
-	// in case we get an error for the first partitions
-	replicationFactor := -1
-	for _, partition := range topicMetadata.Partitions {
-		if len(partition.Replicas) > replicationFactor {
-			replicationFactor = len(partition.Replicas)
-		}
-	}
-
-	return &v1alpha1.ListTopicsResponse_Topic{
-		Name:              *topicMetadata.Topic,
-		Internal:          topicMetadata.IsInternal,
-		PartitionCount:    int32(len(topicMetadata.Partitions)),
-		ReplicationFactor: int32(replicationFactor),
+	return &v1alpha2.UpdateTopicConfigurationsRequest{
+		TopicName:      r.GetTopicName(),
+		Configurations: configs,
 	}
 }
 
-func (k *kafkaClientMapper) setTopicConfigurationsToKafka(req *v1alpha1.SetTopicConfigurationsRequest) *kmsg.AlterConfigsRequest {
-	alterConfigResource := kmsg.NewAlterConfigsRequestResource()
-	alterConfigResource.ResourceType = kmsg.ConfigResourceTypeTopic
-	alterConfigResource.ResourceName = req.TopicName
+func (*kafkaClientMapper) v1alpha1SetTopicConfigurationv1alpha2(r *v1alpha1.SetTopicConfigurationsRequest) *v1alpha2.SetTopicConfigurationsRequest {
+	configs := make([]*v1alpha2.SetTopicConfigurationsRequest_SetConfiguration, 0, len(r.GetConfigurations()))
 
-	for _, config := range req.Configurations {
-		alterConfigResource.Configs = append(alterConfigResource.Configs, k.setTopicConfigurationsResourceToKafka(config))
+	for _, c := range r.GetConfigurations() {
+		configs = append(configs, &v1alpha2.SetTopicConfigurationsRequest_SetConfiguration{
+			Name:  c.GetName(),
+			Value: c.Value,
+		})
 	}
 
-	kafkaReq := kmsg.NewAlterConfigsRequest()
-	kafkaReq.Resources = []kmsg.AlterConfigsRequestResource{alterConfigResource}
-
-	return &kafkaReq
+	return &v1alpha2.SetTopicConfigurationsRequest{
+		TopicName:      r.GetTopicName(),
+		Configurations: configs,
+	}
 }
 
-func (*kafkaClientMapper) setTopicConfigurationsResourceToKafka(req *v1alpha1.SetTopicConfigurationsRequest_SetConfiguration) kmsg.AlterConfigsRequestResourceConfig {
-	kafkaReq := kmsg.NewAlterConfigsRequestResourceConfig()
-	kafkaReq.Name = req.Name
-	kafkaReq.Value = req.Value
+func (*kafkaClientMapper) v1alpha1CreateTopicv1alpha2(r *v1alpha1.CreateTopicRequest) *v1alpha2.CreateTopicRequest {
+	ras := make([]*v1alpha2.CreateTopicRequest_Topic_ReplicaAssignment, 0, len(r.GetTopic().GetReplicaAssignments()))
 
-	return kafkaReq
+	for _, ra := range r.GetTopic().GetReplicaAssignments() {
+		ras = append(ras, &v1alpha2.CreateTopicRequest_Topic_ReplicaAssignment{
+			PartitionId: ra.GetPartitionId(),
+			ReplicaIds:  ra.GetReplicaIds(),
+		})
+	}
+
+	configs := make([]*v1alpha2.CreateTopicRequest_Topic_Config, 0, len(r.GetTopic().GetConfigs()))
+	for _, c := range r.GetTopic().GetConfigs() {
+		configs = append(configs, &v1alpha2.CreateTopicRequest_Topic_Config{
+			Name:  c.GetName(),
+			Value: c.Value,
+		})
+	}
+
+	return &v1alpha2.CreateTopicRequest{
+		Topic: &v1alpha2.CreateTopicRequest_Topic{
+			Name:               r.GetTopic().GetName(),
+			PartitionCount:     r.GetTopic().PartitionCount,
+			ReplicationFactor:  r.GetTopic().ReplicationFactor,
+			ReplicaAssignments: ras,
+			Configs:            configs,
+		},
+		ValidateOnly: r.GetValidateOnly(),
+	}
+}
+
+func (*kafkaClientMapper) v1alpha2CreateTopicResponsev1alpha1(msg *v1alpha2.CreateTopicResponse) *v1alpha1.CreateTopicResponse {
+	return &v1alpha1.CreateTopicResponse{
+		Name:              msg.GetName(),
+		PartitionCount:    msg.GetPartitionCount(),
+		ReplicationFactor: msg.GetReplicationFactor(),
+	}
+}
+
+func mapv1alpha2ConfigTypev1alpha1(t v1alpha2.ConfigType) v1alpha1.ConfigType {
+	switch t {
+	case v1alpha2.ConfigType_CONFIG_TYPE_BOOLEAN:
+		return v1alpha1.ConfigType_CONFIG_TYPE_BOOLEAN
+	case v1alpha2.ConfigType_CONFIG_TYPE_STRING:
+		return v1alpha1.ConfigType_CONFIG_TYPE_STRING
+	case v1alpha2.ConfigType_CONFIG_TYPE_INT:
+		return v1alpha1.ConfigType_CONFIG_TYPE_INT
+	case v1alpha2.ConfigType_CONFIG_TYPE_SHORT:
+		return v1alpha1.ConfigType_CONFIG_TYPE_SHORT
+	case v1alpha2.ConfigType_CONFIG_TYPE_LONG:
+		return v1alpha1.ConfigType_CONFIG_TYPE_LONG
+	case v1alpha2.ConfigType_CONFIG_TYPE_DOUBLE:
+		return v1alpha1.ConfigType_CONFIG_TYPE_DOUBLE
+	case v1alpha2.ConfigType_CONFIG_TYPE_LIST:
+		return v1alpha1.ConfigType_CONFIG_TYPE_LIST
+	case v1alpha2.ConfigType_CONFIG_TYPE_CLASS:
+		return v1alpha1.ConfigType_CONFIG_TYPE_CLASS
+	case v1alpha2.ConfigType_CONFIG_TYPE_PASSWORD:
+		return v1alpha1.ConfigType_CONFIG_TYPE_PASSWORD
+	default:
+		return v1alpha1.ConfigType_CONFIG_TYPE_UNSPECIFIED
+	}
+}
+
+func mapv1alpha2ConfigSourcev1alpha1(t v1alpha2.ConfigSource) v1alpha1.ConfigSource {
+	switch t {
+	case v1alpha2.ConfigSource_CONFIG_SOURCE_DYNAMIC_TOPIC_CONFIG:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_DYNAMIC_TOPIC_CONFIG
+	case v1alpha2.ConfigSource_CONFIG_SOURCE_DYNAMIC_BROKER_CONFIG:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_DYNAMIC_BROKER_CONFIG
+	case v1alpha2.ConfigSource_CONFIG_SOURCE_DYNAMIC_DEFAULT_BROKER_CONFIG:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_DYNAMIC_DEFAULT_BROKER_CONFIG
+	case v1alpha2.ConfigSource_CONFIG_SOURCE_STATIC_BROKER_CONFIG:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_STATIC_BROKER_CONFIG
+	case v1alpha2.ConfigSource_CONFIG_SOURCE_DEFAULT_CONFIG:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_DEFAULT_CONFIG
+	case v1alpha2.ConfigSource_CONFIG_SOURCE_DYNAMIC_BROKER_LOGGER_CONFIG:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_DYNAMIC_BROKER_LOGGER_CONFIG
+	default:
+		return v1alpha1.ConfigSource_CONFIG_SOURCE_UNSPECIFIED
+	}
+}
+
+func mapV1alpha1ConfigAlterOperationToV1alpha2(t v1alpha1.ConfigAlterOperation) v1alpha2.ConfigAlterOperation {
+	switch t {
+	case v1alpha1.ConfigAlterOperation_CONFIG_ALTER_OPERATION_SET:
+		return v1alpha2.ConfigAlterOperation_CONFIG_ALTER_OPERATION_SET
+	case v1alpha1.ConfigAlterOperation_CONFIG_ALTER_OPERATION_DELETE:
+		return v1alpha2.ConfigAlterOperation_CONFIG_ALTER_OPERATION_DELETE
+	case v1alpha1.ConfigAlterOperation_CONFIG_ALTER_OPERATION_APPEND:
+		return v1alpha2.ConfigAlterOperation_CONFIG_ALTER_OPERATION_APPEND
+	case v1alpha1.ConfigAlterOperation_CONFIG_ALTER_OPERATION_SUBTRACT:
+		return v1alpha2.ConfigAlterOperation_CONFIG_ALTER_OPERATION_SUBTRACT
+	default:
+		return v1alpha2.ConfigAlterOperation_CONFIG_ALTER_OPERATION_UNSPECIFIED
+	}
 }
