e17501adc018d13df1449b8ad3e4b7cdf1edb1cb
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2024-08-05 11:23:10 +0100

    backend: expect protobuf encoding if specifically requested

--- backend/pkg/serde/service_integration_test.go
+++ backend/pkg/serde/service_integration_test.go
@@ -503,7 +503,7 @@ func (s *SerdeIntegrationTestSuite) TestDeserializeRecord() {
 		require.NotNil(dr)
 
 		// check value properties
-		assert.Equal(PayloadEncoding(""), dr.Value.Encoding)
+		assert.Equal(PayloadEncodingProtobuf, dr.Value.Encoding)
 		assert.Equal(false, dr.Value.IsPayloadNull)
 		assert.Equal(false, dr.Value.IsPayloadTooLarge)
 		assert.Equal(serializedOrder, dr.Value.OriginalPayload)
