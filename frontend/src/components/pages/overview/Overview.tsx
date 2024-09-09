51a33af6bed878f7dbc05c2b99c4fdcb05972733
Jan Vorcak <jan.vorcak@redpanda.com>
2024-09-09 10:08:53 +0200

    Empty debuginfo in the overview page

--- frontend/src/components/pages/overview/Overview.tsx
+++ frontend/src/components/pages/overview/Overview.tsx
@@ -21,7 +21,7 @@ import { DefaultSkeleton } from '../../../utils/tsxUtils';
 import Section from '../../misc/Section';
 import PageContent from '../../misc/PageContent';
 import './Overview.scss';
-import { Button, DataTable, Flex, Heading, Icon, Link, Skeleton, Tooltip, Grid, GridItem } from '@redpanda-data/ui';
+import { Button, DataTable, Flex, Heading, Icon, Link, Skeleton, Tooltip, Grid, GridItem, List, ListItem, Box } from '@redpanda-data/ui';
 import { CheckIcon } from '@primer/octicons-react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import React, { FC } from 'react';
@@ -127,6 +127,57 @@ class Overview extends PageComponent {
                             <Statistic title="Topics" value={overview.kafka.topicsCount} />
                             <Statistic title="Replicas" value={overview.kafka.replicasCount} />
                         </Flex>
+                    </Section>
+
+                    <Section py={4} gridArea="debugInfo">
+                        <Heading as="h3" >Cluster Health Debug</Heading>
+                        <List spacing={3}>
+                            <ListItem>
+                                <Grid
+                                    templateColumns={{sm: '1fr', md: '1fr 1fr'}} // Single column on mobile, two columns on larger screens
+                                    gap={4}
+                                >
+                                    <Box fontWeight="bold">Reason</Box>
+                                    <Box>Under-replicated partitions</Box>
+                                </Grid>
+                            </ListItem>
+                            <ListItem>
+                                <Grid
+                                    templateColumns={{sm: '1fr', md: '1fr 1fr'}}
+                                    gap={4}
+                                >
+                                    <Box fontWeight="bold">Down nodes</Box>
+                                    <Box>0</Box>
+                                </Grid>
+                            </ListItem>
+                            <ListItem>
+                                <Grid
+                                    templateColumns={{sm: '1fr', md: '1fr 1fr'}}
+                                    gap={4}
+                                >
+                                    <Box fontWeight="bold">Leaderless partitions</Box>
+                                    <Box>0</Box>
+                                </Grid>
+                            </ListItem>
+                            <ListItem>
+                                <Grid
+                                    templateColumns={{sm: '1fr', md: '1fr 1fr'}}
+                                    gap={4}
+                                >
+                                    <Box fontWeight="bold">Under-replicated partitions</Box>
+                                    <Box><Link color="blue.500" href="#">Go to Topics</Link></Box>
+                                </Grid>
+                            </ListItem>
+                            <ListItem>
+                                <Grid
+                                    templateColumns={{sm: '1fr', md: '1fr 1fr'}}
+                                    gap={4}
+                                >
+                                    <Box fontWeight="bold">Debug bundle</Box>
+                                    <Box><Link color="blue.500" href="#">Generate</Link></Box>
+                                </Grid>
+                            </ListItem>
+                        </List>
                     </Section>
 
                     <Section py={4} gridArea="broker">
