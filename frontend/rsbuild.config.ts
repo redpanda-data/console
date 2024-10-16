a4ddeee722a1711e5a0768a93c45aee3b6144253
Santiago Jimenez Giraldo <santiago@redpanda.com>
2024-10-16 13:53:50 +0200

    fix(frontend): add all proxied routes
    make sure all proxied routes are taken into account in the proxy
    configuration
    
    Signed-off-by: Santiago Jimenez Giraldo <santiago@redpanda.com>

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -12,78 +12,80 @@ const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
 
 export default defineConfig({
     plugins: [
-      pluginReact({
-        reactRefreshOptions: {
-          forceEnable: true,
-        },
-      }),
-      pluginSvgr({ mixedImport: true }),
-      pluginSass(),
+        pluginReact({
+            reactRefreshOptions: {
+                forceEnable: true,
+            },
+        }),
+        pluginSvgr({ mixedImport: true }),
+        pluginSass(),
     ],
     moduleFederation: {
-      options: moduleFederationConfig,
+        options: moduleFederationConfig,
     },
     dev: {
-      hmr: true,
+        hmr: true,
     },
     html: {
-      template: './public/index.html',
-      templateParameters: {
-        REACT_APP_ENABLED_FEATURES: process.env.REACT_APP_ENABLED_FEATURES,
-        REACT_APP_CONSOLE_GIT_SHA: process.env.REACT_APP_CONSOLE_GIT_SHA,
-        REACT_APP_CONSOLE_PLATFORM_VERSION: process.env.REACT_APP_CONSOLE_PLATFORM_VERSION,
-        REACT_APP_CONSOLE_GIT_REF: process.env.REACT_APP_CONSOLE_GIT_REF,
-        REACT_APP_BUSINESS: process.env.REACT_APP_BUSINESS,
-        REACT_APP_BUILD_TIMESTAMP: process.env.REACT_APP_BUILD_TIMESTAMP,
-        REACT_APP_DEV_HINT: process.env.REACT_APP_DEV_HINT,
-      },
+        template: './public/index.html',
+        templateParameters: {
+            REACT_APP_ENABLED_FEATURES: process.env.REACT_APP_ENABLED_FEATURES,
+            REACT_APP_CONSOLE_GIT_SHA: process.env.REACT_APP_CONSOLE_GIT_SHA,
+            REACT_APP_CONSOLE_PLATFORM_VERSION: process.env.REACT_APP_CONSOLE_PLATFORM_VERSION,
+            REACT_APP_CONSOLE_GIT_REF: process.env.REACT_APP_CONSOLE_GIT_REF,
+            REACT_APP_BUSINESS: process.env.REACT_APP_BUSINESS,
+            REACT_APP_BUILD_TIMESTAMP: process.env.REACT_APP_BUILD_TIMESTAMP,
+            REACT_APP_DEV_HINT: process.env.REACT_APP_DEV_HINT,
+        },
     },
     server: {
-      htmlFallback: 'index',
+        htmlFallback: 'index',
         proxy: {
-          context: ['/api', '/redpanda.api'],
-          target: 'http://localhost:9090',
-       },
+            context: ['/api', '/redpanda.api', '/auth', '/logout'],
+            target: 'http://localhost:9090',
+        },
     },
     source: {
-      define: publicVars,
-      decorators: {
-        version: 'legacy',
-      }
+        define: publicVars,
+        decorators: {
+            version: 'legacy',
+        },
     },
     output: {
-      distPath: {
-        root: 'build',
-      },
+        distPath: {
+            root: 'build',
+        },
     },
     tools: {
-      rspack: (config, { appendPlugins }) => {
-        config.resolve ||= {};
-        config.resolve.alias ||= {};
-        config.output ||= {};
-        /* resolve symlinks so the proto generate code can be built. */
-        config.resolve.symlinks = false;
+        rspack: (config, { appendPlugins }) => {
+            config.resolve ||= {};
+            config.resolve.alias ||= {};
+            config.output ||= {};
+            /* resolve symlinks so the proto generate code can be built. */
+            config.resolve.symlinks = false;
 
-        config.output.publicPath = 'auto';
+            config.output.publicPath = 'auto';
 
-        const plugins = [
-          new NodePolyfillPlugin({
-            additionalAliases: ['process'],
-          }),
-        ]
+            const plugins = [
+                new NodePolyfillPlugin({
+                    additionalAliases: ['process'],
+                }),
+            ];
 
-        if (process.env.RSDOCTOR) {
-          plugins.push(new RsdoctorRspackPlugin({
-            supports: {
-              /**
-               * @see https://rsdoctor.dev/config/options/options#generatetilegraph
-               */
-              generateTileGraph: true,
+            if (process.env.RSDOCTOR) {
+                plugins.push(
+                    new RsdoctorRspackPlugin({
+                        supports: {
+                            /**
+                             * @see https://rsdoctor.dev/config/options/options#generatetilegraph
+                             */
+                            generateTileGraph: true,
+                        },
+                    })
+                );
             }
-          }));
-        }
 
-        appendPlugins(plugins);
-      },
+            appendPlugins(plugins);
+        },
     },
 });
