7a313b13a1d3c4c48b778063bbedb4f30a229798
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2024-12-20 13:16:14 +0100

    frontend: fix typo causing syntax error (#1568)

--- frontend/src/components/pages/transforms/Transforms.Setup.tsx
+++ frontend/src/components/pages/transforms/Transforms.Setup.tsx
@@ -36,7 +36,7 @@ export class TransformsSetup extends PageComponent<{}> {
               within Redpanda.{' '}
               <ChakraLink
                 isExternal
-                href="https://docs.redpanda.com/current/develop/data-transforms/build/
+                href="https://docs.redpanda.com/current/develop/data-transforms/build"
                 style={{ textDecoration: 'underline solid 1px' }}
               >
                 Learn more
