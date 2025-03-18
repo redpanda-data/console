eb2b3a75605cb0c73ee97e1c0115952b54bb913a
Beniamin Malinski <malinskibeniamin@gmail.com>
2025-03-18 14:24:45 +0000

    frontend: Add pipeline API wrapper

---
+++ frontend/src/react-query/api/pipeline.tsx
@@ -0,0 +1,171 @@
+import type { PartialMessage } from '@bufbuild/protobuf';
+import { useMutation, useQuery } from '@connectrpc/connect-query';
+import { useQueryClient } from '@tanstack/react-query';
+import {
+  createPipeline,
+  deletePipeline,
+  getPipeline,
+  getPipelinesForSecret,
+  listPipelines,
+  startPipeline,
+  stopPipeline,
+} from 'protogen/redpanda/api/console/v1alpha1/pipeline-PipelineService_connectquery';
+import {
+  GetPipelineRequest,
+  GetPipelinesForSecretRequest,
+  ListPipelinesRequest,
+  type ListPipelinesResponse,
+} from 'protogen/redpanda/api/console/v1alpha1/pipeline_pb';
+import {
+  GetPipelineRequest as GetPipelineRequestDataPlane,
+  GetPipelinesForSecretRequest as GetPipelinesForSecretRequestDataPlane,
+  ListPipelinesRequest as ListPipelinesRequestDataPlane,
+  type Pipeline,
+} from 'protogen/redpanda/api/dataplane/v1/pipeline_pb';
+import type { Secret } from 'protogen/redpanda/api/dataplane/v1/secret_pb';
+import { MAX_PAGE_SIZE, type QueryOptions } from 'react-query/react-query.utils';
+import { useInfiniteQueryWithAllPages } from 'react-query/use-infinite-query-with-all-pages';
+import { TOASTS, formatToastErrorMessageGRPC, showToast } from 'utils/toast.utils';
+
+export const REDPANDA_AI_AGENT_PIPELINE_PREFIX = '_redpanda-agent';
+
+export const useGetPipelineQuery = ({ id }: { id: Pipeline['id'] }) => {
+  const getPipelineRequestDataPlane = new GetPipelineRequestDataPlane({ id });
+  const getPipelineRequest = new GetPipelineRequest({ request: getPipelineRequestDataPlane });
+  return useQuery(getPipeline, getPipelineRequest);
+};
+
+export const useListPipelinesAndAgentsQuery = (
+  input?: PartialMessage<ListPipelinesRequestDataPlane>,
+  options?: QueryOptions<ListPipelinesRequestDataPlane, ListPipelinesResponse, ListPipelinesResponse>,
+) => {
+  const listPipelinesRequestDataPlane = new ListPipelinesRequestDataPlane({
+    pageSize: MAX_PAGE_SIZE,
+    pageToken: '',
+    ...input,
+  });
+
+  const listPipelinesRequest = new ListPipelinesRequest({
+    request: listPipelinesRequestDataPlane,
+  }) as PartialMessage<ListPipelinesRequest> & Required<Pick<PartialMessage<ListPipelinesRequest>, 'request'>>;
+
+  const listPipelinesResult = useInfiniteQueryWithAllPages(listPipelines, listPipelinesRequest, {
+    pageParamKey: 'request',
+    enabled: options?.enabled,
+  });
+
+  const allRecords = listPipelinesResult?.data?.pages?.flatMap(({ response }) => response?.pipelines);
+
+  const pipelines = allRecords?.filter(
+    (pipeline) => !pipeline?.displayName.startsWith(REDPANDA_AI_AGENT_PIPELINE_PREFIX),
+  );
+  const agents = allRecords?.filter((pipeline) => pipeline?.displayName.startsWith(REDPANDA_AI_AGENT_PIPELINE_PREFIX));
+
+  return {
+    ...listPipelinesResult,
+    data: {
+      pipelines,
+      agents,
+    },
+  };
+};
+
+export const useCreatePipelineMutationWithToast = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(createPipeline, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [listPipelines.service.typeName] });
+      showToast({
+        id: TOASTS.PIPELINE.CREATE.SUCCESS,
+        title: 'Pipeline created successfully',
+        status: 'success',
+      });
+    },
+    onError: (error) => {
+      showToast({
+        id: TOASTS.PIPELINE.CREATE.ERROR,
+        title: formatToastErrorMessageGRPC({ error, action: 'create', entity: 'pipeline' }),
+        status: 'error',
+      });
+    },
+  });
+};
+
+export const useStartPipelineMutationWithToast = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(startPipeline, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [listPipelines.service.typeName] });
+      showToast({
+        id: TOASTS.PIPELINE.START.SUCCESS,
+        title: 'Pipeline started successfully',
+        status: 'success',
+      });
+    },
+    onError: (error) => {
+      showToast({
+        id: TOASTS.PIPELINE.START.ERROR,
+        title: formatToastErrorMessageGRPC({ error, action: 'start', entity: 'pipeline' }),
+        status: 'error',
+      });
+    },
+  });
+};
+
+export const useStopPipelineMutationWithToast = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(stopPipeline, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [listPipelines.service.typeName] });
+      showToast({
+        id: TOASTS.PIPELINE.STOP.SUCCESS,
+        title: 'Pipeline stopped successfully',
+        status: 'success',
+      });
+    },
+    onError: (error) => {
+      showToast({
+        id: TOASTS.PIPELINE.STOP.ERROR,
+        title: formatToastErrorMessageGRPC({ error, action: 'stop', entity: 'pipeline' }),
+        status: 'error',
+      });
+    },
+  });
+};
+
+export const useDeletePipelineMutationWithToast = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(deletePipeline, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [listPipelines.service.typeName] });
+      showToast({
+        id: TOASTS.PIPELINE.DELETE.SUCCESS,
+        title: 'Pipeline deleted successfully',
+        status: 'success',
+      });
+    },
+    onError: (error) => {
+      showToast({
+        id: TOASTS.PIPELINE.DELETE.ERROR,
+        title: formatToastErrorMessageGRPC({ error, action: 'delete', entity: 'pipeline' }),
+        status: 'error',
+      });
+    },
+  });
+};
+
+export const useGetPipelinesForSecretQuery = ({ secretId }: { secretId: Secret['id'] }) => {
+  const getPipelinesForSecretRequestDataPlane = new GetPipelinesForSecretRequestDataPlane({ secretId });
+
+  const getPipelinesForSecretRequest = new GetPipelinesForSecretRequest({
+    request: getPipelinesForSecretRequestDataPlane,
+  });
+
+  return useQuery(getPipelinesForSecret, getPipelinesForSecretRequest, {
+    enabled: secretId !== undefined,
+  });
+};
