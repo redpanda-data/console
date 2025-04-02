17b3cad0bd8fea55121b2e43af5f9de851858ce7
Beniamin Malinski <malinskibeniamin@gmail.com>
2025-04-02 20:02:15 +0200

    frontend: Adjust form field spacing

--- frontend/src/components/form/checkbox/checkbox-field.tsx
+++ frontend/src/components/form/checkbox/checkbox-field.tsx
@@ -1,12 +1,14 @@
-import { Checkbox, type CheckboxProps, FormControl, FormLabel, HStack } from '@redpanda-data/ui';
+import { Checkbox, type CheckboxProps, FormControl, FormHelperText, FormLabel, HStack } from '@redpanda-data/ui';
+import type { ReactNode } from 'react';
 import { ErrorInfoField } from '../error-info/error-info-field';
 import { useFieldContext } from '../form-hook-contexts';
 
 interface CheckboxFieldProps extends CheckboxProps {
-  label?: string;
+  label?: ReactNode;
+  helperText?: ReactNode;
 }
 
-export const CheckboxField = ({ label, ...rest }: CheckboxFieldProps) => {
+export const CheckboxField = ({ label, helperText, ...rest }: CheckboxFieldProps) => {
   const field = useFieldContext<boolean>();
 
   return (
@@ -18,7 +20,13 @@ export const CheckboxField = ({ label, ...rest }: CheckboxFieldProps) => {
             {label}
           </FormLabel>
         )}
+        {helperText && (
+          <FormHelperText mt={0} mb={1}>
+            {helperText}
+          </FormHelperText>
+        )}
       </HStack>
+
       <ErrorInfoField field={field} />
     </FormControl>
   );
