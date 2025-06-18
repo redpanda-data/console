redpanda-data/console

commit 59d0780e43e6f3e676432cdb53dd7073024ddfb5

Console V3 refactorings
- refactoring auth hooks for enterprise versions
- flatten package structure
- breaking config changes
- serdes load schemas lazily from schema registry
- move logging package from cloudhut to this repo
- use client factories for constructing api clients dynamically

backend/pkg/api/handle_kafka_connect.go
@@ -18,8 +18,6 @@ import (
 
 	"github.com/cloudhut/common/rest"
 	con "github.com/cloudhut/connect-client"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 
 	"github.com/redpanda-data/console/backend/pkg/connect"
 )
@@ -50,18 +48,6 @@ func (api *API) handleGetConnectors() http.HandlerFunc {
 			return
 		}
 
-		for _, shard := range connectors {
-			clusterName := shard.ClusterName
-
-			// Attach allowed actions for each cluster
-			var restErr *rest.Error
-			shard.AllowedActions, restErr = api.Hooks.Authorization.AllowedConnectClusterActions(r.Context(), clusterName)
-			if restErr != nil {
-				api.Logger.Error("failed to check view connect cluster permissions", zap.Error(restErr.Err))
-				continue
-			}
-		}
-
 		res := response{
 			Clusters:     connectors,
 			IsConfigured: true,
@@ -77,22 +63,6 @@ func (api *API) handleGetClusterConnectors() http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
 
-		canSee, restErr := api.Hooks.Authorization.CanViewConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canSee {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to view this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to view connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
 		connectors, restErr := api.ConnectSvc.GetClusterConnectors(ctx, clusterName)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
@@ -106,22 +76,6 @@ func (api *API) handleGetClusterConnectors() http.HandlerFunc {
 func (api *API) handleGetClusterInfo() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		clusterName := rest.GetURLParam(r, "clusterName")
-
-		canSee, restErr := api.Hooks.Authorization.CanViewConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canSee {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to view this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to view connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
 
 		clusterInfo, restErr := api.ConnectSvc.GetClusterInfo(r.Context(), clusterName)
 		if restErr != nil {
@@ -143,22 +97,6 @@ func (api *API) handleGetConnector() http.HandlerFunc {
 
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
-
-		canSee, restErr := api.Hooks.Authorization.CanViewConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canSee {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to view this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to view connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
 
 		connectorInfo, restErr := api.ConnectSvc.GetConnector(ctx, clusterName, connector)
 		if restErr != nil {
@@ -192,28 +130,7 @@ func (api *API) handlePutConnectorConfig() http.HandlerFunc {
 		clusterName := rest.GetURLParam(r, "clusterName")
 		connectorName := rest.GetURLParam(r, "connector")
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to create connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
 		var req putConnectorConfigRequest
-		restErr = rest.Decode(w, r, &req)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
 
 		cInfo, restErr := api.ConnectSvc.PutConnectorConfig(r.Context(), clusterName, connectorName, req.ToClientRequest())
 		if restErr != nil {
@@ -237,24 +154,8 @@ func (api *API) handlePutValidateConnectorConfig() http.HandlerFunc {
 		clusterName := rest.GetURLParam(r, "clusterName")
 		pluginClassName := rest.GetURLParam(r, "pluginClassName")
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to create connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
 		var req map[string]any
-		restErr = rest.Decode(w, r, &req)
+		restErr := rest.Decode(w, r, &req)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return
@@ -290,24 +191,8 @@ func (api *API) handleCreateConnector() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		clusterName := rest.GetURLParam(r, "clusterName")
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to create connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
 		var req createConnectorRequest
-		restErr = rest.Decode(w, r, &req)
+		restErr := rest.Decode(w, r, &req)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return
@@ -331,23 +216,7 @@ func (api *API) handleDeleteConnector() http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
 
-		canDelete, restErr := api.Hooks.Authorization.CanDeleteConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canDelete {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to delete in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to delete connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
-		restErr = api.ConnectSvc.DeleteConnector(ctx, clusterName, connector)
+		restErr := api.ConnectSvc.DeleteConnector(ctx, clusterName, connector)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return
@@ -365,23 +234,7 @@ func (api *API) handlePauseConnector() http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to edit connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
-		restErr = api.ConnectSvc.PauseConnector(ctx, clusterName, connector)
+		restErr := api.ConnectSvc.PauseConnector(ctx, clusterName, connector)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return
@@ -399,23 +252,7 @@ func (api *API) handleResumeConnector() http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to edit connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
-		restErr = api.ConnectSvc.ResumeConnector(ctx, clusterName, connector)
+		restErr := api.ConnectSvc.ResumeConnector(ctx, clusterName, connector)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return
@@ -433,23 +270,7 @@ func (api *API) handleRestartConnector() http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to edit connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
-		restErr = api.ConnectSvc.RestartConnector(ctx, clusterName, connector, true, false)
+		restErr := api.ConnectSvc.RestartConnector(ctx, clusterName, connector, true, false)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return
@@ -478,23 +299,7 @@ func (api *API) handleRestartConnectorTask() http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), api.ConnectSvc.Cfg.RequestTimeout)
 		defer cancel()
 
-		canEdit, restErr := api.Hooks.Authorization.CanEditConnectCluster(r.Context(), clusterName)
-		if restErr != nil {
-			rest.SendRESTError(w, r, api.Logger, restErr)
-			return
-		}
-		if !canEdit {
-			rest.SendRESTError(w, r, api.Logger, &rest.Error{
-				Err:          fmt.Errorf("requester has no permissions to edit in this connect cluster"),
-				Status:       http.StatusForbidden,
-				Message:      "You don't have permissions to edit connectors in this Kafka connect cluster",
-				InternalLogs: []zapcore.Field{zap.String("cluster_name", clusterName)},
-				IsSilent:     false,
-			})
-			return
-		}
-
-		restErr = api.ConnectSvc.RestartConnectorTask(ctx, clusterName, connector, taskID)
+		restErr := api.ConnectSvc.RestartConnectorTask(ctx, clusterName, connector, taskID)
 		if restErr != nil {
 			rest.SendRESTError(w, r, api.Logger, restErr)
 			return

commit 1bc442c36f90b3abebf86d3193ba048abcb10c86

fix: restore missing request body parsing in connector config update
The handlePutConnectorConfig endpoint was missing the rest.Decode() call,
causing all connector config updates to fail with "Connector class is not set"
regardless of the actual config content.

This regression was introduced in commit 59d0780e during V3 refactoring
when authorization code was removed but the request decoding was accidentally
removed as well.

Changes:
- Add missing rest.Decode() call to parse JSON request body
- Add regression test to prevent this issue from recurring

Fixes connector config updates that were failing since March 2025.

backend/pkg/api/handle_kafka_connect.go
@@ -131,6 +131,11 @@ func (api *API) handlePutConnectorConfig() http.HandlerFunc {
 		connectorName := rest.GetURLParam(r, "connector")
 
 		var req putConnectorConfigRequest
+		restErr := rest.Decode(w, r, &req)
+		if restErr != nil {
+			rest.SendRESTError(w, r, api.Logger, restErr)
+			return
+		}
 
 		cInfo, restErr := api.ConnectSvc.PutConnectorConfig(r.Context(), clusterName, connectorName, req.ToClientRequest())
 		if restErr != nil {

backend/pkg/api/handle_kafka_connect_test.go
@@ -1,0 +1,77 @@
+// Copyright 2025 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file https://github.com/redpanda-data/redpanda/blob/dev/licenses/bsl.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package api
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+// TestHandlePutConnectorConfigParsesRequestBody ensures the endpoint properly parses the request body
+// This is a regression test for the bug where the endpoint forgot to call rest.Decode(),
+// causing all connector configs to be treated as empty and fail with "Connector class is not set"
+func TestHandlePutConnectorConfigParsesRequestBody(t *testing.T) {
+	// Test that malformed JSON fails at parsing stage (not at service stage)
+	t.Run("malformed_json_returns_bad_request", func(t *testing.T) {
+		req := httptest.NewRequest("PUT", "/kafka-connect/clusters/test-cluster/connectors/test-connector", bytes.NewReader([]byte(`{"config": invalid}`)))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Setup chi URL params
+		rctx := chi.NewRouteContext()
+		rctx.URLParams.Add("clusterName", "test-cluster")
+		rctx.URLParams.Add("connector", "test-connector")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+		api := &API{
+			ConnectSvc: nil, // Should not be reached due to parsing failure
+			Logger:     zap.NewNop(),
+		}
+
+		w := httptest.NewRecorder()
+		handler := api.handlePutConnectorConfig()
+		handler.ServeHTTP(w, req)
+
+		// Should return 400 Bad Request due to JSON parsing error
+		// If rest.Decode() was missing, this would cause different behavior
+		assert.Equal(t, http.StatusBadRequest, w.Code, "Malformed JSON should return 400 Bad Request")
+	})
+
+	// Test that empty request body (no JSON) properly fails at parsing stage
+	t.Run("empty_request_body_fails_at_parsing", func(t *testing.T) {
+		req := httptest.NewRequest("PUT", "/kafka-connect/clusters/test-cluster/connectors/test-connector", bytes.NewReader([]byte("")))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Setup chi URL params
+		rctx := chi.NewRouteContext()
+		rctx.URLParams.Add("clusterName", "test-cluster")
+		rctx.URLParams.Add("connector", "test-connector")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+		api := &API{
+			ConnectSvc: nil, // Should not be reached due to parsing failure
+			Logger:     zap.NewNop(),
+		}
+
+		w := httptest.NewRecorder()
+		handler := api.handlePutConnectorConfig()
+		handler.ServeHTTP(w, req)
+
+		// Should return 400 Bad Request due to empty/invalid JSON
+		// This ensures rest.Decode() is being called
+		assert.Equal(t, http.StatusBadRequest, w.Code, "Empty request body should return 400 Bad Request")
+	})
+}