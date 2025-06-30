8168ace52620f72a2686123f945db8bb63d30271
Beniamin Malinski <malinskibeniamin@gmail.com>
2025-06-30 11:58:03 +0100

    frontend: remove requiredVersion for react
    Required for React 19 upgrade to have smooth transition

--- frontend/module-federation.js
+++ frontend/module-federation.js
@@ -8,22 +8,18 @@ module.exports = {
     './EmbeddedApp': './src/EmbeddedApp.tsx',
     './injectApp': './src/injectApp.tsx',
     './config': './src/config.ts',
-    './newReact': require.resolve('react'),
-    './newReactDOM': require.resolve('react-dom'),
   },
 
-  shared: [
-    'react-dom',
-    {
-      react: {
-        import: 'react', // the "react" package will be used a provided and fallback module
-        shareKey: 'newReact', // under this name the shared module will be placed in the share scope
-        shareScope: 'modern', // share scope with this name will be used
-        singleton: true, // only a single version of the shared module is allowed
-      },
-      '@redpanda-data/ui': {
-        import: '@redpanda-data/ui',
-      },
+  shared: {
+    react: {
+      singleton: true,
     },
-  ],
+    'react-dom': {
+      singleton: true,
+    },
+    '@redpanda-data/ui': {
+      singleton: true,
+      requiredVersion: deps['@redpanda-data/ui'],
+    },
+  },
 };
