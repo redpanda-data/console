9350cd3032061f071c88f2e807cf6ca92eca9920
Julin <142230457+c-julin@users.noreply.github.com>
2025-08-07 11:31:29 +0100

    backend: add set quota name validation and update service errors to match style of other services

--- backend/pkg/api/connect/service/quota/v1/service.go
+++ backend/pkg/api/connect/service/quota/v1/service.go
@@ -57,7 +57,11 @@ func (s *Service) ListQuotas(ctx context.Context, req *connect.Request[v1.ListQu
 
 	resp, err := s.consoleSvc.DescribeClientQuotas(ctx, kafkaReq)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to describe quotas: %w", err))
+		return nil, apierrors.NewConnectError(
+			connect.CodeInternal,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_KAFKA_API_ERROR.String(), apierrors.KeyValsFromKafkaError(err)...),
+		)
 	}
 
 	// Handle errors from the console service
@@ -68,7 +72,11 @@ func (s *Service) ListQuotas(ctx context.Context, req *connect.Request[v1.ListQu
 	// Map console response to protobuf response
 	quotaEntries, err := s.kafkaClientMapper.quotaItemsToProto(resp.Entries)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to map quota items to proto: %w", err))
+		return nil, apierrors.NewConnectError(
+			connect.CodeInternal,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_CONSOLE_ERROR.String()),
+		)
 	}
 
 	// Add pagination
@@ -106,12 +114,20 @@ func (s *Service) ListQuotas(ctx context.Context, req *connect.Request[v1.ListQu
 func (s *Service) BatchSetQuota(ctx context.Context, req *connect.Request[v1.BatchSetQuotaRequest]) (*connect.Response[v1.BatchSetQuotaResponse], error) {
 	kafkaReq, err := s.kafkaClientMapper.batchSetQuotaRequestToKafka(req.Msg)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		return nil, apierrors.NewConnectError(
+			connect.CodeInvalidArgument,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_CONSOLE_ERROR.String()),
+		)
 	}
 
 	resp, err := s.consoleSvc.AlterClientQuotas(ctx, kafkaReq)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to create quota: %w", err))
+		return nil, apierrors.NewConnectError(
+			connect.CodeInternal,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_KAFKA_API_ERROR.String(), apierrors.KeyValsFromKafkaError(err)...),
+		)
 	}
 
 	successfulEntities, failedEntities := s.kafkaClientMapper.mapAlterClientQuotasResponse(resp.Entries)
@@ -128,12 +144,20 @@ func (s *Service) BatchSetQuota(ctx context.Context, req *connect.Request[v1.Bat
 func (s *Service) DeleteQuota(ctx context.Context, req *connect.Request[v1.DeleteQuotaRequest]) (*connect.Response[v1.DeleteQuotaResponse], error) {
 	kafkaReq, err := s.kafkaClientMapper.deleteQuotaRequestToKafka(req.Msg)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		return nil, apierrors.NewConnectError(
+			connect.CodeInvalidArgument,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_CONSOLE_ERROR.String()),
+		)
 	}
 
 	resp, err := s.consoleSvc.AlterClientQuotas(ctx, kafkaReq)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to delete quota: %w", err))
+		return nil, apierrors.NewConnectError(
+			connect.CodeInternal,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_KAFKA_API_ERROR.String(), apierrors.KeyValsFromKafkaError(err)...),
+		)
 	}
 	// For single delete quota requests, we expect a single entry in the response
 	// loop through the entries and return if any error occurs
@@ -150,12 +174,20 @@ func (s *Service) DeleteQuota(ctx context.Context, req *connect.Request[v1.Delet
 func (s *Service) SetQuota(ctx context.Context, req *connect.Request[v1.SetQuotaRequest]) (*connect.Response[v1.SetQuotaResponse], error) {
 	kafkaReq, err := s.kafkaClientMapper.setQuotaRequestToKafka(req.Msg)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		return nil, apierrors.NewConnectError(
+			connect.CodeInvalidArgument,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_CONSOLE_ERROR.String()),
+		)
 	}
 
 	resp, err := s.consoleSvc.AlterClientQuotas(ctx, kafkaReq)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to set quota: %w", err))
+		return nil, apierrors.NewConnectError(
+			connect.CodeInternal,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_KAFKA_API_ERROR.String(), apierrors.KeyValsFromKafkaError(err)...),
+		)
 	}
 	// For single set quota requests, we expect a single entry in the response
 	// loop through the entries and return if any error occurs
@@ -174,12 +206,20 @@ func (s *Service) SetQuota(ctx context.Context, req *connect.Request[v1.SetQuota
 func (s *Service) BatchDeleteQuota(ctx context.Context, req *connect.Request[v1.BatchDeleteQuotaRequest]) (*connect.Response[v1.BatchDeleteQuotaResponse], error) {
 	kafkaReq, err := s.kafkaClientMapper.batchDeleteQuotaRequestToKafka(req.Msg)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		return nil, apierrors.NewConnectError(
+			connect.CodeInvalidArgument,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_CONSOLE_ERROR.String()),
+		)
 	}
 
 	resp, err := s.consoleSvc.AlterClientQuotas(ctx, kafkaReq)
 	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("failed to batch delete quota: %w", err))
+		return nil, apierrors.NewConnectError(
+			connect.CodeInternal,
+			err,
+			apierrors.NewErrorInfo(v1.Reason_REASON_KAFKA_API_ERROR.String(), apierrors.KeyValsFromKafkaError(err)...),
+		)
 	}
 
 	successfulEntities, failedEntities := s.kafkaClientMapper.mapAlterClientQuotasResponse(resp.Entries)
