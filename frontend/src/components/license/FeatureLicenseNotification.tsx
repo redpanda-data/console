bbeef1222d3491176dfb633ff95bf336fa44044d
Ján Vorčák <jan.vorcak@redpanda.com>
2025-08-12 11:01:17 +0200

    Feature banner notification for extended trial

--- frontend/src/components/license/FeatureLicenseNotification.tsx
+++ frontend/src/components/license/FeatureLicenseNotification.tsx
@@ -26,16 +26,15 @@ const getLicenseAlertContentForFeature = (
   _featureName: 'rbac' | 'reassignPartitions',
   license: License | undefined,
   enterpriseFeaturesUsed: ListEnterpriseFeaturesResponse_Feature[],
+  bakedInTrial: boolean,
 ): { message: ReactElement; status: 'warning' | 'info' } | null => {
   if (license === undefined) {
     return null;
   }
 
   const msToExpiration = getMillisecondsToExpiration(license);
-  const bakedInTrial = isBakedInTrial(license);
 
-
-  if(license.type === License_Type.TRIAL && api.isRedpanda) {
+  if (license.type === License_Type.TRIAL && api.isRedpanda) {
     if (bakedInTrial) {
       return {
         message: (
@@ -47,7 +46,16 @@ const getLicenseAlertContentForFeature = (
           </Box>
         ),
         status: msToExpiration > 5 * MS_IN_DAY ? 'info' : 'warning',
-      }; 
+      };
+    } else {
+      return {
+        message: (
+          <Box>
+            <Text>This is an enterprise feature.</Text>
+          </Box>
+        ),
+        status: msToExpiration > 5 * MS_IN_DAY ? 'info' : 'warning',
+      };
     }
   }
 
@@ -163,13 +171,24 @@ export const FeatureLicenseNotification: FC<{ featureName: 'reassignPartitions' 
       void api.listLicenses();
     }, []);
 
-    const license = api.licenses
+    const licenses = api.licenses
       .filter((license) => license.type === License_Type.TRIAL || license.type === License_Type.COMMUNITY)
       .sort((a, b) => LICENSE_WEIGHT[a.type] - LICENSE_WEIGHT[b.type]) // Sort by priority
-      .first();
+
+    // Choose the license with the latest expiration time
+    const license = licenses.reduce((latest, current) => {
+      const latestExpiration = Number(latest.expiresAt);
+      const currentExpiration = Number(current.expiresAt);
+      return currentExpiration > latestExpiration ? current : latest;
+    });
+
+    // Trial is either baked-in or extended. We need to check if any of the licenses are baked-in.
+    // We say the trial is baked-in if and only if all the licenses are baked-in. There can be a situation where, 
+    // use has registered a license, it's updated in the brokers, but the console doesn't have the license re-loaded yet.
+    const bakedInTrial = licenses.every(license => isBakedInTrial(license));
 
     const enterpriseFeaturesUsed = api.enterpriseFeaturesUsed;
-    const alertContent = getLicenseAlertContentForFeature(featureName, license, enterpriseFeaturesUsed);
+    const alertContent = getLicenseAlertContentForFeature(featureName, license, enterpriseFeaturesUsed, bakedInTrial);
 
     // This component needs info about whether we're using Redpanda or Kafka, without fetching clusterOverview first, we might get a malformed result
     if (api.clusterOverview === null) {
