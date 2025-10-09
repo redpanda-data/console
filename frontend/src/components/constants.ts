1bfa0d086f280bf7423ccc6b81986ca72cfa5ac8
Beniamin Malinski <malinskibeniamin@gmail.com>
2025-10-09 08:31:21 +0100

    frontend: add new AI agents feature flags

--- frontend/src/components/constants.ts
+++ frontend/src/components/constants.ts
@@ -6,11 +6,11 @@ export const BUILDER_API_KEY = '4abd0efa0759420b88149ada5c1eb216';
 
 // By default, most feature flags will be false when there's no embedded mode on.
 export const FEATURE_FLAGS = {
-  enableAiAgentsInConsoleUi: false,
-  enableAiAgentsInConsoleUiPreview: false,
   enableKnowledgeBaseInConsoleUi: false,
   enableRemoteMcpInConsole: false,
   enableRemoteMcpInspectorInConsole: false,
   enableRemoteMcpConnectClientInConsoleServerless: false,
   enableRpcnTiles: false,
+  enableAiAgentsInConsole: false,
+  enableAiAgentsInspectorInConsole: false,
 };
