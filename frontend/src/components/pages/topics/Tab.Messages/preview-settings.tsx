cf111d729b2aa0752e9d103e237513e491e5aff3
Ján Vorčák <jan.vorcak@redpanda.com>
2025-10-20 13:57:19 +0200

    Move pattern help drawer to a separate file

--- frontend/src/components/pages/topics/Tab.Messages/preview-settings.tsx
+++ frontend/src/components/pages/topics/Tab.Messages/preview-settings.tsx
@@ -9,23 +9,8 @@
  * by the Apache License, Version 2.0
  */
 
-import { GearIcon, InfoIcon, ThreeBarsIcon, XIcon } from '@primer/octicons-react';
-import {
-  Box,
-  Button,
-  Checkbox,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  Flex,
-  Input,
-  Popover,
-  useDisclosure,
-} from '@redpanda-data/ui';
+import { GearIcon, ThreeBarsIcon, XIcon } from '@primer/octicons-react';
+import { Box, Button, Checkbox, Flex, Input, Popover } from '@redpanda-data/ui';
 import { arrayMoveMutable } from 'array-move';
 import { observer } from 'mobx-react';
 import React from 'react';
@@ -38,151 +23,14 @@ import {
   type ResponderProvided,
 } from 'react-beautiful-dnd';
 
-import globExampleImg from '../../../../assets/globExample.png';
+import { globHelp, PatternHelpDrawer } from './preview-settings/pattern-help-drawer';
 import type { TopicMessage } from '../../../../state/rest-interfaces';
 import type { PreviewTagV2 } from '../../../../state/ui';
 import { uiState } from '../../../../state/ui-state';
 import { IsDev } from '../../../../utils/env';
-import { Code, Label, OptionGroup, toSafeString } from '../../../../utils/tsx-utils';
+import { Label, OptionGroup, toSafeString } from '../../../../utils/tsx-utils';
 import { type CollectedProperty, collectElements2, getAllMessageKeys, randomId } from '../../../../utils/utils';
 import { SingleSelect } from '../../../misc/select';
-
-const PatternHelpDrawer = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  return (
-    <>
-      <button
-        onClick={onOpen}
-        style={{
-          margin: '0 2px',
-          color: 'hsl(205deg, 100%, 50%)',
-          textDecoration: 'underline dotted',
-        }}
-        type="button"
-      >
-        <InfoIcon size={15} />
-        &nbsp;glob patterns
-      </button>
-      <Drawer isOpen={isOpen} onClose={onClose} placement="right" size="xl">
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Glob Pattern Examples</DrawerHeader>
-
-          <DrawerBody>{globHelp}</DrawerBody>
-
-          <DrawerFooter>
-            <Button mr={3} onClick={onClose} variant="outline">
-              Close
-            </Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </>
-  );
-};
-
-const globHelp = (
-  <div>
-    {/* Examples + Image */}
-    <Flex gap={2}>
-      <Flex grow={1}>
-        <div className="globHelpGrid">
-          <div className="h">Pattern</div>
-          <div className="h">Result</div>
-          <div className="h">Reason / Explanation</div>
-
-          <div className="titleRowSeparator" />
-
-          {/* Example */}
-          <div className="c1">
-            <Code>id</Code>
-          </div>
-          <div className="c2">id: 1111</div>
-          <div className="c3">There is only one 'id' property at the root of the object</div>
-          <div className="rowSeparator" />
-
-          {/* Example */}
-          <div className="c1">
-            <Code>*.id</Code>
-          </div>
-          <div className="c2">
-            <div>customer.id: 2222</div>
-            <div>key.with.dots.id: 3333</div>
-          </div>
-          <div className="c3">Star only seraches in direct children. Here, only 2 children contain an 'id' prop</div>
-          <div className="rowSeparator" />
-
-          {/* Example */}
-          <div className="c1">
-            <Code>**.id</Code>
-          </div>
-          <div className="c2">(all ID properties)</div>
-          <div className="c3">Double-star searches everywhere</div>
-          <div className="rowSeparator" />
-
-          {/* Example */}
-          <div className="c1">
-            <Code>customer.*Na*</Code>
-          </div>
-          <div className="c2">
-            <div>customer.firstName: John</div>
-            <div>customer.lastName: Example</div>
-          </div>
-          <div className="c3">In the direct child named 'customer', find all properties that contain 'Na'</div>
-          <div className="rowSeparator" />
-
-          {/* Example */}
-          <div className="c1">
-            <Code>key.with.dots.id</Code>
-          </div>
-          <div className="c2">(no results!)</div>
-          <div className="c3">There is no property named 'key'!</div>
-          <div className="rowSeparator" />
-
-          {/* Example */}
-          <div className="c1">
-            <Code>"key.with.dots".id</Code>
-          </div>
-          <div className="c2">key.with.dots.id: 3333</div>
-          <div className="c3">
-            To find properties with special characters in their name, use single or double-quotes
-          </div>
-          <div className="rowSeparator" />
-        </div>
-      </Flex>
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-        <div style={{ opacity: 0.5, fontSize: 'smaller', textAlign: 'center' }}>Example Data</div>
-        <img alt="Examples for glob patterns" src={globExampleImg} />
-      </div>
-    </Flex>
-
-    {/* Details */}
-    <div>
-      <h3>Details</h3>
-      <div>
-        A glob pattern is just a list of property names seperated by dots. In addition to simple property names you can
-        use:
-      </div>
-      <ul style={{ paddingLeft: '2em', marginTop: '.5em' }}>
-        <li>
-          <Code>*</Code> Star to match all current properties
-        </li>
-        <li>
-          <Code>**</Code> Double-Star to matches all current and nested properties
-        </li>
-        <li>
-          <Code>"</Code>/<Code>'</Code> Quotes for when a property-name contains dots
-        </li>
-        <li>
-          <Code>abc*</Code> One or more stars within a name. Depending on where you place the star, you can check if a
-          name starts with, ends with, or contains some string.
-        </li>
-      </ul>
-    </div>
-  </div>
-);
 
 export const PreviewSettings = observer(({ messages }: { messages: TopicMessage[] }) => {
   const allCurrentKeys = getAllMessageKeys(messages)
@@ -264,8 +112,6 @@ export const PreviewSettings = observer(({ messages }: { messages: TopicMessage[
             )}
           </Droppable>
         </DragDropContext>
-
-        {/* <CustomTagList tags={uiState.topicSettings.previewTags} allCurrentKeys={this.props.allCurrentKeys} /> */}
       </div>
 
       <div style={{ marginTop: '1em' }}>
