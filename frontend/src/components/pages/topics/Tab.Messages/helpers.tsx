b24d75d0745c883e4775331c8afbaf62868c6c8a
Ján Vorčák <jan.vorcak@redpanda.com>
2025-10-23 17:24:35 +0200

    Dedup getControlCharacterName

--- frontend/src/components/pages/topics/Tab.Messages/helpers.tsx
+++ frontend/src/components/pages/topics/Tab.Messages/helpers.tsx
@@ -45,3 +45,77 @@ export function getDeleteErrorText(
   }
   return undefined;
 }
+
+/**
+ * Gets the name of a control character by its ASCII code
+ */
+export function getControlCharacterName(code: number): string {
+  switch (code) {
+    case 0:
+      return 'NUL';
+    case 1:
+      return 'SOH';
+    case 2:
+      return 'STX';
+    case 3:
+      return 'ETX';
+    case 4:
+      return 'EOT';
+    case 5:
+      return 'ENQ';
+    case 6:
+      return 'ACK';
+    case 7:
+      return 'BEL';
+    case 8:
+      return 'BS';
+    case 9:
+      return 'HT';
+    case 10:
+      return 'LF';
+    case 11:
+      return 'VT';
+    case 12:
+      return 'FF';
+    case 13:
+      return 'CR';
+    case 14:
+      return 'SO';
+    case 15:
+      return 'SI';
+    case 16:
+      return 'DLE';
+    case 17:
+      return 'DC1';
+    case 18:
+      return 'DC2';
+    case 19:
+      return 'DC3';
+    case 20:
+      return 'DC4';
+    case 21:
+      return 'NAK';
+    case 22:
+      return 'SYN';
+    case 23:
+      return 'ETB';
+    case 24:
+      return 'CAN';
+    case 25:
+      return 'EM';
+    case 26:
+      return 'SUB';
+    case 27:
+      return 'ESC';
+    case 28:
+      return 'FS';
+    case 29:
+      return 'GS';
+    case 30:
+      return 'RS';
+    case 31:
+      return 'US';
+    default:
+      return '';
+  }
+}
--- frontend/src/components/pages/topics/Tab.Messages/message-display/payload-component.tsx
+++ frontend/src/components/pages/topics/Tab.Messages/message-display/payload-component.tsx
@@ -16,6 +16,7 @@ import { useState } from 'react';
 
 import type { Payload } from '../../../../../state/rest-interfaces';
 import { KowlJsonView } from '../../../../misc/kowl-json-view';
+import { getControlCharacterName } from '../helpers';
 
 // Regex for checking printable ASCII characters
 const PRINTABLE_CHAR_REGEX = /[\x20-\x7E]/;
@@ -55,76 +56,6 @@ function highlightControlChars(str: string, maxLength?: number): ReactNode[] {
   return elements;
 }
 
-function getControlCharacterName(code: number): string {
-  switch (code) {
-    case 0:
-      return 'NUL';
-    case 1:
-      return 'SOH';
-    case 2:
-      return 'STX';
-    case 3:
-      return 'ETX';
-    case 4:
-      return 'EOT';
-    case 5:
-      return 'ENQ';
-    case 6:
-      return 'ACK';
-    case 7:
-      return 'BEL';
-    case 8:
-      return 'BS';
-    case 9:
-      return 'HT';
-    case 10:
-      return 'LF';
-    case 11:
-      return 'VT';
-    case 12:
-      return 'FF';
-    case 13:
-      return 'CR';
-    case 14:
-      return 'SO';
-    case 15:
-      return 'SI';
-    case 16:
-      return 'DLE';
-    case 17:
-      return 'DC1';
-    case 18:
-      return 'DC2';
-    case 19:
-      return 'DC3';
-    case 20:
-      return 'DC4';
-    case 21:
-      return 'NAK';
-    case 22:
-      return 'SYN';
-    case 23:
-      return 'ETB';
-    case 24:
-      return 'CAN';
-    case 25:
-      return 'EM';
-    case 26:
-      return 'SUB';
-    case 27:
-      return 'ESC';
-    case 28:
-      return 'FS';
-    case 29:
-      return 'GS';
-    case 30:
-      return 'RS';
-    case 31:
-      return 'US';
-    default:
-      return '';
-  }
-}
 
 export const PayloadComponent = observer((p: { payload: Payload; loadLargeMessage: () => Promise<void> }) => {
   const { payload, loadLargeMessage } = p;
