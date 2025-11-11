a5a9b3e98129f440b7205b18b9f610ff5350e40e
Andres Aristizabal <andres@redpanda.com>
2025-11-11 08:36:48 -0500

    refactor(routes): hide shadowlink for cloud

--- frontend/src/components/routes.tsx
+++ frontend/src/components/routes.tsx
@@ -594,7 +594,7 @@ export const APP_ROUTES: IRouteEntry[] = [
     'Shadow Links',
     LinkIcon,
     true,
-    routeVisibility(true, [Feature.ShadowLinkService])
+    routeVisibility(() => !isEmbedded(), [Feature.ShadowLinkService])
   ),
   MakeRoute<{}>('/shadowlinks/create', ShadowLinkCreatePage, 'Create Shadow Link'),
   MakeRoute<{ name: string }>('/shadowlinks/:name', ShadowLinkDetailsPage, 'Shadow Link Details'),
