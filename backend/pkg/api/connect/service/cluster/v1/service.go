f653d22c8acc3bd7242d46499fa525bcb707e2f7
Graham Smith <graham.smith@redpanda.com>
2025-12-08 13:51:09 -0500

    backend implementation

--- backend/pkg/api/connect/service/cluster/v1/service.go
+++ backend/pkg/api/connect/service/cluster/v1/service.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Redpanda Data, Inc.
+// Copyright 2025 Redpanda Data, Inc.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.md
@@ -16,10 +16,13 @@ import (
 
 	"connectrpc.com/connect"
 
+	"github.com/redpanda-data/console/backend/pkg/api/connect/service/common/v1"
 	"github.com/redpanda-data/console/backend/pkg/config"
 	"github.com/redpanda-data/console/backend/pkg/console"
+	dataplanev1 "github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/dataplane/v1"
 	v1 "github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/private/v1"
 	"github.com/redpanda-data/console/backend/pkg/protogen/redpanda/api/private/v1/privatev1connect"
+	"github.com/twmb/franz-go/pkg/kmsg"
 )
 
 var _ privatev1connect.ClusterServiceHandler = (*Service)(nil)
@@ -31,11 +34,80 @@ type Service struct {
 	consoleSvc console.Servicer
 }
 
+func configSourceStringToProto(s string) dataplanev1.ConfigSource {
+	mapper := &common.KafkaClientMapper{}
+	if ksource, err := kmsg.ParseConfigSource(s); err == nil {
+		if dsource, err := mapper.ConfigSourceToProto(ksource); err == nil {
+			return dsource
+		}
+	}
+	return dataplanev1.ConfigSource_CONFIG_SOURCE_UNSPECIFIED
+}
+
+func configTypeStringToProto(s string) dataplanev1.ConfigType {
+	mapper := &common.KafkaClientMapper{}
+	if ktype, err := kmsg.ParseConfigType(s); err == nil {
+		if dtype, err := mapper.ConfigTypeToProto(ktype); err == nil {
+			return dtype
+		}
+	}
+	return dataplanev1.ConfigType_CONFIG_TYPE_UNSPECIFIED
+}
+
 // GetCluster returns cluster information.
 func (s *Service) GetCluster(ctx context.Context, req *connect.Request[v1.GetClusterRequest]) (*connect.Response[v1.GetClusterResponse], error) {
-	// TODO: Implement cluster information retrieval
+	resp, err := s.consoleSvc.GetClusterInfo(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var brokers []*v1.Broker
+	for _, broker := range resp.Brokers {
+		var configEntries []*v1.BrokerConfigEntry
+		for _, cfg := range broker.Config.Configs {
+			synonyms := make([]*dataplanev1.ConfigSynonym, len(cfg.Synonyms))
+			for i, syn := range cfg.Synonyms {
+				synonyms[i] = &dataplanev1.ConfigSynonym{
+					Name:   syn.Name,
+					Value:  syn.Value,
+					Source: configSourceStringToProto(syn.Source),
+				}
+			}
+
+			configEntries = append(configEntries, &v1.BrokerConfigEntry{
+				Name:  cfg.Name,
+				Value: cfg.Value,
+
+				// TODO: after REST deprecation, stop converting config source/type to strings in GetClusterInfo
+				Source:          configSourceStringToProto(cfg.Source),
+				Type:            configTypeStringToProto(cfg.Type),
+				IsExplicitlySet: cfg.IsExplicitlySet,
+				IsDefaultValue:  cfg.IsDefaultValue,
+				IsReadOnly:      cfg.IsReadOnly,
+				IsSensitive:     cfg.IsSensitive,
+				Documentation:   *cfg.Documentation,
+				Synonyms:        synonyms,
+			})
+		}
+
+		brokers = append(brokers, &v1.Broker{
+			Id:         broker.BrokerID,
+			LogDirSize: broker.LogDirSize,
+			Address:    broker.Address,
+			Rack:       broker.Rack,
+			Config: &v1.BrokerConfig{
+				Error:  broker.Config.Error,
+				Values: configEntries,
+			},
+		})
+	}
+
 	return connect.NewResponse(&v1.GetClusterResponse{
-		Cluster: &v1.Cluster{},
+		Cluster: &v1.Cluster{
+			ControllerId: resp.ControllerID,
+			KafkaVersion: resp.KafkaVersion,
+			Brokers:      brokers,
+		},
 	}), nil
 }
 
