4683c6a5832ac4b158c3f33b684a42584a240f44
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2026-01-29 15:07:55 +0000

    proto(tracing): regenerate code

--- backend/pkg/protogen/redpanda/api/dataplane/v1alpha3/tracing.pb.gw.go
+++ backend/pkg/protogen/redpanda/api/dataplane/v1alpha3/tracing.pb.gw.go
@@ -109,6 +109,41 @@ func local_request_TracingService_GetTrace_0(ctx context.Context, marshaler runt
 	return msg, metadata, err
 }
 
+var filter_TracingService_GetTraceHistogram_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+
+func request_TracingService_GetTraceHistogram_0(ctx context.Context, marshaler runtime.Marshaler, client TracingServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq GetTraceHistogramRequest
+		metadata runtime.ServerMetadata
+	)
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_TracingService_GetTraceHistogram_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	msg, err := client.GetTraceHistogram(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+}
+
+func local_request_TracingService_GetTraceHistogram_0(ctx context.Context, marshaler runtime.Marshaler, server TracingServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq GetTraceHistogramRequest
+		metadata runtime.ServerMetadata
+	)
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_TracingService_GetTraceHistogram_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	msg, err := server.GetTraceHistogram(ctx, &protoReq)
+	return msg, metadata, err
+}
+
 // RegisterTracingServiceHandlerServer registers the http handlers for service TracingService to "mux".
 // UnaryRPC     :call TracingServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -154,6 +189,26 @@ func RegisterTracingServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 			return
 		}
 		forward_TracingService_GetTrace_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
+	mux.Handle(http.MethodGet, pattern_TracingService_GetTraceHistogram_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/redpanda.api.dataplane.v1alpha3.TracingService/GetTraceHistogram", runtime.WithHTTPPathPattern("/v1alpha3/traces:histogram"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TracingService_GetTraceHistogram_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_TracingService_GetTraceHistogram_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -229,15 +284,34 @@ func RegisterTracingServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 		}
 		forward_TracingService_GetTrace_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodGet, pattern_TracingService_GetTraceHistogram_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/redpanda.api.dataplane.v1alpha3.TracingService/GetTraceHistogram", runtime.WithHTTPPathPattern("/v1alpha3/traces:histogram"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TracingService_GetTraceHistogram_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_TracingService_GetTraceHistogram_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	return nil
 }
 
 var (
-	pattern_TracingService_ListTraces_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1alpha3", "traces"}, ""))
-	pattern_TracingService_GetTrace_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1alpha3", "traces", "trace_id"}, ""))
+	pattern_TracingService_ListTraces_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1alpha3", "traces"}, ""))
+	pattern_TracingService_GetTrace_0          = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1alpha3", "traces", "trace_id"}, ""))
+	pattern_TracingService_GetTraceHistogram_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1alpha3", "traces"}, "histogram"))
 )
 
 var (
-	forward_TracingService_ListTraces_0 = runtime.ForwardResponseMessage
-	forward_TracingService_GetTrace_0   = runtime.ForwardResponseMessage
+	forward_TracingService_ListTraces_0        = runtime.ForwardResponseMessage
+	forward_TracingService_GetTrace_0          = runtime.ForwardResponseMessage
+	forward_TracingService_GetTraceHistogram_0 = runtime.ForwardResponseMessage
 )
