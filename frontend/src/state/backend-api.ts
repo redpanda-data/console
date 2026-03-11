8b94a9fccd0ec2c511244090127ab5ad8aec0052
Blair McKee <23467862+eblairmckee@users.noreply.github.com>
2026-03-10 21:51:59 -0700

    fix: type

--- frontend/src/state/backend-api.ts
+++ frontend/src/state/backend-api.ts
@@ -23,10 +23,6 @@ import {
   isBakedInTrial,
   prettyLicenseType,
 } from 'components/license/license-utils';
-import JSONBigIntFactory from 'json-bigint';
-
-const JSONBigInt = JSONBigIntFactory({ storeAsString: true });
-
 import { ListMessagesRequestSchema } from 'protogen/redpanda/api/console/v1alpha1/list_messages_pb';
 import type { TransformMetadata } from 'protogen/redpanda/api/dataplane/v1/transform_pb';
 import { createStore as zustandCreate } from 'zustand/vanilla';
