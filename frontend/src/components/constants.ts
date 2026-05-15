18ab2fd953c01e21dc5aa8b926a4b7cad08eec33
Ján Vorčák <jan.vorcak@redpanda.com>
2026-05-15 19:03:44 +0200

    enableNewSecurityPage: true for non-embedded mode

--- frontend/src/components/constants.ts
+++ frontend/src/components/constants.ts
@@ -16,7 +16,7 @@ export const FEATURE_FLAGS = {
   enableNewPipelineLogs: false,
   enablePipelineDiagrams: false,
   enableConnectSlashMenu: false,
-  enableNewSecurityPage: false,
+  enableNewSecurityPage: true,
   enableTeamsBridge: false,
 };
 
