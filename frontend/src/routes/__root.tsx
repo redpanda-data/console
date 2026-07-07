578eac3eb8a3c1d28c2a8185650520091d7f2fd0
Peter Zenger <peter.zenger@redpanda.com>
2026-07-07 12:23:18 -0700

    registry updates and misc for the RPCN branch use

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -32,6 +32,7 @@ import { RouterSync } from '../components/misc/router-sync';
 import { SidebarInset } from '../components/redpanda-ui/components/sidebar';
 import RequireAuth from '../components/require-auth';
 import { useIsDarkMode } from '../hooks/use-is-dark-mode';
+import { IsDev } from '../utils/env';
 import { isFullscreenPath } from '../utils/fullscreen-routes';
 import { ModalContainer } from '../utils/modal-container';
 
@@ -56,7 +57,7 @@ function RootLayout() {
         {process.env.NODE_ENV === 'development' && <DebugHelper />}
       </NuqsAdapter>
 
-      {process.env.NODE_ENV === 'development' && <TanStackRouterDevtools position="bottom-right" />}
+      {IsDev && <TanStackRouterDevtools position="bottom-right" />}
     </>
   );
 }
